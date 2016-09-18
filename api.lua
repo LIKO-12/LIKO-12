dd7e64f3fbc808ac803db5a167739d5876f6889a
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2016-09-18 10:26:49 +0300

    Patched SpriteGroup to allow scaling

--- api.lua
+++ api.lua
@@ -217,7 +217,12 @@ function SpriteSheet:draw(id,x,y,r,sx,sy) self.img:draw(x,y,r,sx,sy,self.quads[i
 function SpriteSheet:extract(id) return ImageData(8,8):paste(self:data(),1,1,self:rect(id)) end
 
 function Sprite(id,x,y,r,sx,sy,sheet) (sheet or SpriteMap):draw(id,x,y,r,sx,sy) end
-function SpriteGroup(id,x,y,w,h,sheet) for spry = 1, h or 1 do for sprx = 1, w or 1 do (sheet or SpriteMap):draw((id-1)+sprx+(spry*24-24),x+(sprx*8-8),y+(spry*8-8)) end end end
+function SpriteGroup(id,x,y,w,h,sx,sy,sheet)
+  local sx,sy = floor(sx or 1), floor(sy or 1)
+  for spry = 1, h or 1 do for sprx = 1, w or 1 do
+    (sheet or SpriteMap):draw((id-1)+sprx+(spry*24-24),x+(sprx*sx*8-sx*8),y+(spry*sy*8-sy*8),0,sx,sy)
+  end end
+end
 
 EditorSheet = SpriteSheet(Image("/editorsheet.png"),24,12)
 
