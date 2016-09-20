72bc6e2ede7d2dea173b8bde478e23fe8eed5837
Rami Sabbagh <ramilego4game@gmail.com>
2016-09-20 19:40:48 +0300

    Something nice for the code editor

--- editor/code.lua
+++ editor/code.lua
@@ -10,18 +10,26 @@ comment = _GetColor(14),
 str = _GetColor(13),
 }
 
-cedit.codebuffer = {""}
-cedit.lineLimit = 14
-cedit.cursorX, cedit.cursorY = 1,1
-cedit.topLine = 0
-
-local blinktime = 0.5
-local blinktimer = 0
-local blinkstate = false
+function cedit:resetBuffer()
+  self.buffer = TextBuffer(1,2,45,14,0,0,0)
+  function self.buffer:_redraw() --To add syntax highlighting
+    api.rect(1,9,192,128-16,6) api.color(7)
+    local dbuff, gx,gy, sr = self:getLinesBuffer()
+    local cbuff = colorize(dbuff,cedit.colors)
+    api.color(8)
+    for line, text in ipairs(cbuff) do
+      api.print(text, (gx*8-6)-sr*4,(gy+line-1)*8-6)
+    end
+    api.rect(1,128-7,192,8,10)
+    api.color(5)
+    api.print("LINE "..self.cursorY.."/"..#self.buffer,2,128-5)
+  end
+end
 
 function cedit:export()
-  local code = ""
-  for line,text in ipairs(self.codebuffer) do
+  if not self.buffer then self:resetBuffer() end
+  local code, buff = "", self.buffer:getBuffer()
+  for line,text in ipairs(buff) do
     code = code..text.."\n"
   end
   return code
@@ -33,22 +41,24 @@ local function magiclines(s)
 end
 
 function cedit:load(code)
-  self.codebuffer, self.topLine, self.cursorX, self.cursorY = {}, 0, 1, 1
-  if not code then self.codebuffer[1] = "" return self end
+  --self.codebuffer, self.topLine, self.cursorX, self.cursorY = {}, 0, 1, 1
+  self:resetBuffer()
+  if not code then return self end
   local code = code
   for line in magiclines(code) do
-    table.insert(self.codebuffer,line)
+    table.insert(self.buffer.buffer,line)
   end
   return self
 end
 
 function cedit:_switch()
-  
+  self:resetBuffer()
 end
 
 function cedit:_redraw()
-  api.rect(1,9,192,128-16,6) api.color(7)
-  local tocolor = {}
+  self.buffer:_redraw()
+  --api.rect(1,9,192,128-16,6) api.color(7)
+  --[[local tocolor = {}
   for i=self.topLine+1,self.topLine+self.lineLimit do
     if self.codebuffer[i] then table.insert(tocolor,self.codebuffer[i]) end
   end
@@ -60,7 +70,7 @@ function cedit:_redraw()
   
   api.rect(1,128-7,192,8,10)
   api.color(5)
-  api.print("LINE "..self.topLine+self.cursorY.."/"..#self.codebuffer,2,128-5)
+  api.print("LINE "..self.topLine+self.cursorY.."/"..#self.codebuffer,2,128-5)]]
   
   --[[for i=self.topLine+1,self.topLine+self.lineLimit do
     if self.codebuffer[i] then api.print_grid(self.codebuffer[i],1,(i-self.topLine)+1) end
@@ -68,147 +78,36 @@ function cedit:_redraw()
 end
 
 function cedit:_update(dt)
-  blinktimer = blinktimer+dt if blinktimer > blinktime then blinktimer = blinktimer - blinktime  blinkstate = not blinkstate end
-  local curlen = self.codebuffer[self.topLine+self.cursorY]:len()
-  if blinkstate then api.rect((self.cursorX-1)*4+2,(self.cursorY)*8+2,4,5,5) else self:_redraw() end
+  self.buffer:_update(dt)
 end
 
 function cedit:_mmove(x,y,dx,dy,it,iw)
-  if math.abs(y) > 5 then return end --Dead mouse wheen strike
-  if math.abs(x) > 5 then return end --Dead mouse wheen strike
+  if math.abs(y) > 5 then return end --Dead mouse wheel strike
+  if math.abs(x) > 5 then return end --Dead mouse wheel strike
   if y > 0 then
-    self:_kpress("up",0,false)
+    self.buffer:_kpress("up",0,false)
   elseif y < 0 then
-    self:_kpress("down",0,false)
+    self.buffer:_kpress("down",0,false)
   end
   
   if x > 0 then
-    self:_kpress("right",0,false) --Maybe ? or inverted..
+    self.buffer:_kpress("right",0,false) --Maybe ? or inverted..
   elseif x < 0 then
-    self:_kpress("left",0,false)
+    self.buffer:_kpress("left",0,false)
   end
 end
 
-cedit.keymap = {
-  ["return"] = function(self)
-    -- check if the cursor is at the last line
-    if self.cursorY == self.lineLimit then
-      self.topLine = self.topLine+1
-    else
-      self.cursorY = self.cursorY+1
-    end
-
-    for i=#self.codebuffer, self.cursorY+self.topLine, -1 do
-      self.codebuffer[i+1] = self.codebuffer[i]
-    end--Shift down the code
-
-    local offset = self.cursorY+self.topLine
-    self.codebuffer[offset] = self.codebuffer[offset-1]:sub(self.cursorX,-1)
-    self.codebuffer[offset-1] = self.codebuffer[offset-1]:sub(0,self.cursorX-1)
-
-    -- self.codebuffer[self.cursorY+self.topLine] = "" --Insert a new empty line
-    -- Set the cursorX to 1 (since it's an empty line), also reset the
-    -- blinkstate&timer to force the cursor to blink on
-    self.cursorX, blinktimer, blinkstate = 1, 0, true
-  end,
-
-  backspace = function(self)
-    local offset = self.cursorY+self.topLine
-    if self.codebuffer[offset] == "" then
-      if self.cursorY ~= 1 then
-         --Remove the line so the table size stays correct
-        table.remove(self.codebuffer,offset)
-        if self.cursorY == 1 then
-         self.topLine = self.topLine-1
-         if self.topLine < 1 then self.topline=0 end
-        else
-         self.cursorY = self.cursorY-1
-        end
-        self.cursorX = self.codebuffer[self.cursorY+self.topLine]:len()+1
-      end
-    else
-      if self.cursorX > 1 then
-        self.codebuffer[offset] =
-          self.codebuffer[offset]:sub(0,self.cursorX-2) ..
-          self.codebuffer[offset]:sub(self.cursorX,-1)
-        self.cursorX = self.cursorX-1
-      --[[else
-        self.codebuffer[offset-1] = self.codebuffer[offset-1]..(self.codebuffer[offset] or "")
-        for i=offset, #self.codebuffer do self.codebuffer[i] = self.codebuffer[i+1] end--Shift up the code
-        table.remove(self.codebuffer,#self.codebuffer) --Drop The last line (because it got duplicated)
-        self.cursorY = self.cursorY-1
-        if self.cursorY < 1 then if self.topLine > 0 then self.topLine = self.topLine -1 end self.cursorY = 1 end
-        self.cursorX = self.codebuffer[offset-1]:len()+1  MUST SETUP LINE LENGTH SYSTEM FIRST]]
-      end
-    end
-    blinktimer, blinkstate = 0, true
-  end,
-
-  up = function(self)
-    self.cursorY = self.cursorY-1
-    if self.cursorY < 1 then if self.topLine > 0 then self.topLine = self.topLine -1 end self.cursorY = 1 end
-    if self.cursorX > self.codebuffer[self.cursorY+self.topLine]:len()+1 then self.cursorX = self.codebuffer[self.cursorY+self.topLine]:len()+1 end
-    blinktimer, blinkstate = 0, true
-  end,
-
-  down = function(self)
-    blinktimer, blinkstate = 0, true
-    if self.cursorY+self.topLine == #self.codebuffer then return end
-    if self.cursorY == self.lineLimit then
-      self.topLine = self.topLine+1
-    else
-      self.cursorY = self.cursorY+1
-    end
-    if self.cursorX > self.codebuffer[self.cursorY+self.topLine]:len()+1 then self.cursorX = self.codebuffer[self.cursorY+self.topLine]:len()+1 end
-  end,
-
-  left = function(self)
-    local lineNum = self.cursorY+self.topLine
-    self.cursorX = self.cursorX - 1
-    if self.cursorX < 1 and lineNum > 1 then
-      self.keymap.up(self)
-      self.keymap["end"](self)
-    elseif self.cursorX < 1 then
-      self.cursorX = 1
-    end
-    blinktimer, blinkstate = 0, true
-  end,
-
-  right = function(self)
-    local lineNum = self.cursorY+self.topLine
-    self.cursorX = self.cursorX + 1
-    if self.cursorX > #self.codebuffer[lineNum] then
-      if lineNum < #self.codebuffer then
-        self.keymap.down(self)
-        self.keymap.home(self)
-      else
-        self.cursorX = #self.codebuffer[lineNum] + 1
-      end
-    end
-    blinktimer, blinkstate = 0, true
-  end,
-
-  home = function(self)
-   self.cursorX = 1
-  end,
-
-  ["end"] = function(self)
-    self.cursorX = #self.codebuffer[self.topLine+self.cursorY] + 1
-  end
-}
+function cedit:_kpress(k,sc,ir)
+  self.buffer:_kpress(k,sc,ir)
+end
 
 function cedit:_tinput(t)
-  if t == "\\" then return end --This thing is so bad, so GO AWAY
-  blinktimer, blinkstate = 0, true
-  if self.codebuffer[self.cursorY+self.topLine]:len() == 46 then return end
-  self.codebuffer[self.cursorY+self.topLine] = self.codebuffer[self.cursorY+self.topLine]:sub(0,self.cursorX-1)..t..self.codebuffer[self.cursorY+self.topLine]:sub(self.cursorX,-1)--self.codebuffer[self.cursorY+self.topLine]..t
-  self.cursorX = self.cursorX+1
-  self:_redraw()
+  self.buffer:_tinput(t)
 end
 
 function cedit:_tpress()
   --This means the user is using a touch device
-  self.lineLimit = 7
+  --self.lineLimit = 7
   api.showkeyboard(true)
 end
 
---
+++ libraries/textbuffer.lua
@@ -0,0 +1,194 @@
+--This library is create for programming code editors in liko-12 more easly by using premade text buffer with horizental and vertical scrolling support--
+
+local tb = _Class("liko12.textBuffer")
+
+--Args: grid x, grid Y, draw width in grid, draw height in grid, lines Limit, length Limit, min length, cursor color, blinktime--
+--0 blinktime disables cursor, 0 lines limit, 0 length disables all that..--
+--TODO: add real lines & length & mlength limits.--
+function tb:initialize(gx,gy,gw,gh,linel,lengthl,mlength,curcol,blinktime)
+  self.gx, self.gy, self.linel, self.lenl, self.minl, self.curcol, self.blinktime = gx or 1, gy or 1, linel or 16, lengthl or 45, mlength or 0, curcol or 5, blinktime or 0.5
+  self.gw, self.gh = gw or self.lenl, gh or self.linel
+  self.buffer = {""} --Start with an empty line
+  self.blinktimer = 0
+  self.blinkstate = 0
+  
+  self.cursorX, self.cursorY = 1,1
+  self.lastCX = 1 --Last cursor x when scrolling up or down
+  
+  self.shiftRight = 0
+  self.shiftTop = 0
+  
+  self.keymap = {
+    ["return"] = function(self,ir)
+      self:shiftDown(self.cursorY+1)
+      self.buffer[self.cursorY+1] = self.buffer[self.cursorY]:sub(self.cursorX,-1)
+      self.buffer[self.cursorY] = self.buffer[self.cursorY]:sub(0,self.cursorX-1)
+      self.cursorY = self.cursorY+1
+      self.cursorX = 1
+    end,
+    
+    ["backspace"] = function(self,ir)
+      if self.cursorX == 1 then --If it's at the start of the line
+        if self.buffer[self.cursorY-1] then
+          self.buffer[self.cursorY-1] = self.buffer[self.cursorY-1]..self.buffer[self.cursorY]
+          self:shiftUp(self.cursorY+1)
+          self.cursorY = self.cursorY-1
+          self.cursorX = self.buffer[self.cursorY]:len()+1
+        end
+      else
+        self.buffer[self.cursorY] = self.buffer[self.cursorY]:sub(0,self.cursorX-2)..self.buffer[self.cursorY]:sub(self.cursorX,-1)
+        self.cursorX = self.cursorX-1
+      end
+    end,
+    
+    ["left"] = function(self,ir)
+      self.cursorX = self.cursorX-1
+      if self.cursorX < 1 then
+        if self.buffer[self.cursorY-1] then
+          self.cursorX = self.buffer[self.cursorY-1]:len()+1
+          self.keymap["up"](self,false)
+        else
+          self.cursorX = 1
+        end
+      end
+    end,
+    
+    ["right"] = function(self,ir)
+      self.cursorX = self.cursorX+1
+      if self.cursorX > self.buffer[self.cursorY]:len()+1 then
+        if self.buffer[self.cursorY+1] then
+          self.cursorX = 1
+          self.keymap["down"](self,false)
+        else
+          self.cursorX = self.buffer[self.cursorY]:len()+1
+        end
+      end
+    end,
+    
+    ["up"] = function(self,ir)
+      if self.buffer[self.cursorY-1] then
+        self.cursorY = self.cursorY-1
+        if not ir then self.lastCX = self.cursorX else self.cursorX = self.lastCX end
+        if self.cursorX > self.buffer[self.cursorY]:len()+1 then self.cursorX = self.buffer[self.cursorY]:len()+1 end
+      end
+    end,
+    
+    ["down"] = function(self,ir)
+      if self.buffer[self.cursorY+1] then
+        self.cursorY = self.cursorY+1
+        if not ir then self.lastCX = self.cursorX else self.cursorX = self.lastCX end
+        if self.cursorX > self.buffer[self.cursorY]:len()+1 then self.cursorX = self.buffer[self.cursorY]:len()+1 end
+      end
+    end,
+    
+    ["home"] = function(self,ir)
+      self.cursorX = 1
+    end,
+    
+    ["end"] = function(self,ir)
+      self.cursorX = self.buffer[self.cursorY]:len()+1
+    end,
+    
+    ["pageup"] = function(self,ir)
+      for i=1, self.gh do
+        
+      end
+    end,
+    
+    ["pagedown"] = function(self,ir)
+      
+    end
+  }
+  
+end
+
+function tb:_update(dt)
+  if self.blinktime == 0 then return end
+  self.blinktimer = self.blinktimer+dt if self.blinktimer > self.blinktime then self.blinktimer = self.blinktimer - self.blinktime  self.blinkstate = not self.blinkstate end
+  local curlen = self.buffer[self.cursorY]:len()
+  if self.blinkstate then api.rect((self.cursorX-1)*4+2,(self.cursorY)*8+2,4,5,self.curcol) else self:_redraw() end
+end
+
+function tb:_redraw()
+  local dbuff = self:getDrawBuffer()
+  for line,text in ipairs(debuff) do
+    api.print_grid(line,self.gx,self.gy)
+  end
+end
+
+function tb:_kpress(k,sc,ir)
+  if self.keymap[k] then
+    self.keymap[k](self,ir)
+    self:_redraw()
+  end
+  self:forceBlink()
+end
+
+function tb:_tinput(t)
+  self:forceBlink()
+  --print(self.buffer[self.cursorY]:sub(0,self.cursorX-1)..t..self.buffer[self.cursorY]:sub(self.cursorX,-1))
+  if self.lenl > 0 and self.buffer[self.cursorY]:len()-1 > self.lenl then return end
+  self.buffer[self.cursorY] = self.buffer[self.cursorY]:sub(0,self.cursorX-1)..t..self.buffer[self.cursorY]:sub(self.cursorX,-1)
+  self.cursorX = self.cursorX + t:len()
+  self:_redraw()
+end
+
+function tb:getBuffer()
+  local bclone = {}
+  for line,text in ipairs(self.buffer) do
+    table.insert(bclone,text)
+  end
+  return bclone
+end
+
+function tb:getLinesBuffer()
+  self:fixShifts()
+  local dbuff = {}
+  for y=self.shiftTop+1,self.shiftTop+1+self.gh do
+    if self.buffer[y] then
+      table.insert(dbuff,self.buffer[y])
+    end
+  end
+  return dbuff, self.gx, self.gy, self.shiftRight
+end
+
+function tb:getDrawBuffer()
+  self:fixShifts()
+  local dbuff = {}
+  for y=self.shiftTop+1,self.shiftTop+1+self.gh do
+    if self.buffer[y] then
+      table.insert(dbuff,self.buffer[y]:sub(self.shiftRight,self.shiftRight+self.gw))
+    end
+  end
+  return dbuff, self.gx, self.gy
+end
+
+function tb:fixShifts()
+  if self.cursorX > self.gw+self.shiftRight then self.shiftRight =  self.cursorX - (self.gw+self.shiftRight) end
+  if self.cursorY > self.gh+self.shiftTop then self.shiftTop =  self.cursorY - (self.gh+self.shiftTop) end
+  
+  if self.cursorX <= self.shiftRight then self.shiftRight =  self.shiftRight - (self.cursorX-1) end
+  if self.cursorY <= self.shiftTop then self.shiftTop =  self.shiftTop - (self.cursorY-1) end
+end
+
+function tb:forceBlink() self.blinktimer, self.blinkstate = 0, true end
+
+function tb:shiftDown(sline) --Note: the sline is shifted
+  if sline > #self.buffer then table.insert(self.buffer,"") return end
+  table.insert(self.buffer,"") -- Insert a new line
+  for i=#self.buffer-1,sline,-1 do
+    self.buffer[i+1] = self.buffer[i]
+  end
+  self.buffer[sline] = "" --Clear the start line
+  return self
+end
+
+function tb:shiftUp(sline)
+  if sline == 1 then return end
+  for i=sline,#self.buffer do --Note: the sline is shifted
+    self.buffer[i-1] = self.buffer[i]
+  end
+  table.remove(self.buffer,#self.buffer) --Remove the last line
+end
+
+return tb
