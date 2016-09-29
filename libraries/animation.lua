169e4c1b4deeee2063f6dd5370b2d047357613a3
Rami Sabbagh <ramilego4game@gmail.com>
2016-09-29 23:17:29 +0300

    WIP Library

---
+++ libraries/animation.lua
@@ -0,0 +1,15 @@
+local anim = _Class("liko12.animation")
+
+function anim:initialize(startid,length,duration)
+  
+end
+
+function anim:update(dt)
+
+end
+
+function anim:pos(x,y)
+
+end
+
+return anim
