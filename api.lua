9e3ef008c5e1ab1021eb184b054e6a7885603804
Rami Sabbagh <ramilego4game@gmail.com>
2016-10-02 15:58:43 +0300

    Nothing...

--- api.lua
+++ api.lua
@@ -5,7 +5,7 @@ _LK12VER = "V0.0.4 DEV"
 _LK12VERC = 9--9 DEV, 10 PRE
 
 _GIFSCALE = 2
-_GIFINVERTAL = 1 --How much time between every frame ? (in seconds)
+_GIFINVERTAL = 0.06 --How much time between every frame ? (in seconds)
 _GIFTIMER = 0
 
 _REBOOT = false
