af32686277aa393d6bbfd4fbd7a0030cc15c085e
Rami Sabbagh <ramilego4game@gmail.com>
2016-10-27 19:08:17 +0300

    Changed appdata folder, sorry for that !

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 _ScreenScale = tonumber(os.getenv("LIKO_SCALE") or 3)
 
 function love.conf(t)
-    t.identity = ".liko12"              -- The name of the save directory (string)
+    t.identity = "liko12"              -- The name of the save directory (string)
     t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
     t.console = false                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
