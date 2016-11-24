9ee15fdd3ca8ef0e24daeab18f15c05a7425e12f
Rami Sabbagh <ramilego4game@gmail.com>
2016-11-24 15:58:03 +0200

    Added the colorset

--- Peripherals/GPU.lua
+++ Peripherals/GPU.lua
@@ -14,6 +14,28 @@ return function(config) --A function that creates a new GPU peripheral.
   local _FontChars = config._FontChars or 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"\'`-_/1234567890!?[](){}.,;:<>+=%#^*~ ' --Font chars
   local _FontPath, _FontExtraSpacing = config._FontPath or "/Engine/font.png", config._FontExtraSpacing or 1 --Font image path, and how many extra spacing pixels between every character.
   
+  local _ColorSet = config._ColorSet or {
+    {0,0,0,255}, --Black 1
+    {28,43,83,255}, --Dark Blue 2
+    {127,36,84,255}, --Dark Red 3
+    {0,135,81,255}, --Dark Green 4
+    {171,82,54,255}, --Brown 5
+    {96,88,79,255}, --Dark Gray 6
+    {195,195,198,255}, --Gray 7
+    {255,241,233,255}, --White 8
+    {237,27,81,255}, --Red 9
+    {250,162,27,255}, --Orange 10
+    {247,236,47,255}, --Yellow 11
+    {93,187,77,255}, --Green 12
+    {81,166,220,255}, --Blue 13
+    {131,118,156,255}, --Purple 14
+    {241,118,166,255}, --Pink 15
+    {252,204,171,255} --Human Skin 16
+  } --The colorset of the gpu
+  
+  _ColorSet[0] = {0,0,0,0} --Color index 0 must be always transparent.
+  --End of config loading--
+  
   local _ShouldDraw = false --This flag means that the gpu has to update the screen for the user.
   
   local _Font = love.graphics.newImageFont(_FontPath, _FontChars, _FontExtraSpacing)
@@ -46,20 +68,39 @@ return function(config) --A function that creates a new GPU peripheral.
   
   event:trigger("love:resize", _HOST_W, _HOST_H) --Calculate LIKO12 scale to the host window for the first time.
   
+  love.graphics.setFont(_Font) --Apply the default font.
+  
   --Post initialization (Setup the in liko12 gpu settings)--
   
   --love.graphics.translate(_ScreenTX,_ScreenTY) --Offset all the drawing opereations.
   
-  love.graphics.setLineStyle("rough") --Set the line style.
-  love.graphics.setLineJoin("miter") --Set the line join style.
-  
-  love.graphics.setFont(_Font) --Apply the default font.
+  --love.graphics.setLineStyle("rough") --Set the line style.
+  --love.graphics.setLineJoin("miter") --Set the line join style.
   
   --api.clear() --Clear the canvas for the first time
   --api.stroke(1) --Set the line width to 1
   
+  --Internal Functions--
+  function _HostToLiko(x,y) --Convert a position from HOST screen to LIKO12 screen.
+    --x, y = x-_ScreenX, y-_ScreenY
+    return math.floor(x/_LIKOScale)+1, api.floor(y/_LIKOScale)+1
+  end
+  
+  function _GetColor(c) return _ColorSet[c or 1] end --Get the (rgba) table of a color id.
+  function _GetColorID(r,g,b,a) --Get the color id by the (rgba) table.
+    local a = type(a) == "nil" and 255 or a
+    for id, col in pairs(_ColorSet) do
+      if col[1] == r and col[2] == g and col[3] == b and col[4] == a then
+        return id
+      end
+    end
+    return 1
+  end
+  
   --The api starts here--
   local GPU = {}
+  
+  
   
   return GPU
 end
