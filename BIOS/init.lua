7a609f887c2c286595c6698b137c86a173ae9c31
Rami Sabbagh <ramilego4game@gmail.com>
2016-12-15 19:34:19 +0200

    Debug code

--- BIOS/init.lua
+++ BIOS/init.lua
@@ -27,9 +27,20 @@ end
 
 indexPeripherals("/Peripherals/") --Index and Load the peripherals
 
-local GPU = Peripherals.GPU({}) --Create a new GPU
+local GPU = Peripherals.GPU({_ClearOnRender=true}) --Create a new GPU
 
---Debug Points--
+--FPS display
+events:register("love:update",function(dt) love.window.setTitle("LIKO-12 FPS: "..love.timer.getFPS()) end)
+
+--Debug Draw--
 GPU.points(1,1, 192,1, 192,128, 1,128, 8)
 GPU.points(0,1, 193,1, 193,128, 0,128, 3)
 GPU.points(1,0, 192,0, 192,129, 1,129, 3)
+GPU.rect(2,2, 190,126, true, 12)
+GPU.line(2,2,191,2,191,127,2,127,2,2,12)
+GPU.line(2,2, 191,127, 9)
+GPU.line(191, 2,2,127, 9)
+GPU.rect(10,42,10,10,false,9)
+GPU.rect(10,30,10,10,false,9)
+GPU.rect(10,30,10,10,true,8)
+GPU.points(10,10, 10,19, 19,19, 19,10, 8)
