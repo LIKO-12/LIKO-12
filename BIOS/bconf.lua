7e986bbf62c08f7d4113464a1a9cfd8f7df70257
Rami Sabbagh <ramilego4game@gmail.com>
2016-12-29 22:23:40 +0200

    Added HDD

--- BIOS/bconf.lua
+++ BIOS/bconf.lua
@@ -1,6 +1,12 @@
 --The default configuation
 --per ,err = P(peripheral,mountedName,configTable)
 
+--Create a new virtual hdd system mounted as "HDD"
+P("HDD","HDD",{
+  C = 1024*1024 * 50, --Measured in bytes, equals 50 megabytes
+  D = 1024*1024 * 50 --Measured in bytes, equals 50 megabytes
+})
+
 --Create a new cpu mounted as "CPU"
 P("CPU")
 
