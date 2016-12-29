2a30bdf467f4bd917bdbd726aefa0741e2b5fab5
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2016-12-29 16:41:34 +0200

    WIP

--- BIOS/init.lua
+++ BIOS/init.lua
@@ -8,6 +8,7 @@ local coreg = require("Engine.coreg")
 local function splitFilePath(path) return path:match("(.-)([^\\/]-%.?([^%.\\/]*))$") end --A function to split path to path, name, extension.
 
 local Peripherals = {} --The loaded peripherals.
+local MPer = {} --Mounted and initialized peripherals.
 
 --A function to load the peripherals.
 local function indexPeripherals(path)
@@ -18,8 +19,9 @@ local function indexPeripherals(path)
     else
       local p, n, e = splitFilePath(path..filename)
       if e == "lua" then
-        local chunk, err = love.filesystem.load(path..n) if not chunk then error(err) end
-        Peripherals[n:sub(0,-5)] = chunk()
+        local chunk, err = love.filesystem.load(path..n)
+        if not chunk then Peripherals[n:sub(0,-5)] = "Err: "..tostring(err) else
+        Peripherals[n:sub(0,-5)] = chunk() end
       end
     end
   end
@@ -27,7 +29,48 @@ end
 
 indexPeripherals("/Peripherals/") --Index and Load the peripherals
 
-local GPU = Peripherals.GPU({_ClearOnRender=true}) --Create a new GPU
+--Peripheral, Err = P(PeriheralName, MountedName, ConfigTabel)
+local function P(per,m,conf)
+  if not per then return false, "Should provide peripheral name" end
+  if type(per) ~= "string" then return false, "Peripheral name should be a string, provided "..type(per) end
+  if not Peripherals[per] then return false, "'"..per.."' Peripheral doesn't exists" end
+  if type(Peripherals[per]) == "string" then return false, "Compile "..Peripherals[per] end
+  
+  local m = m or per
+  if type(m) ~= "string" then return false, "Mounting name should be a string, provided "..type(m) end
+  if MPer[m] then return false, "Mounting name '"..m.."' is already taken" end
+  
+  local conf = conf or {}
+  if type(conf) ~= "table" then return false, "Configuration table should be a table, provided "..type(conf) end
+  
+  local peripheral, err = pcall(Peripherals[per],conf)
+  if err then err = "Init Err: "..err end
+  if peripheral then
+    MPer[m] = peripheral
+    coreg:register(peripheral,m)
+  end
+  return peripheral, err
+end
+
+if not love.filesystem.exists("/bconf.lua") then
+  love.filesystem.write("/bconf.lua",love.filesystem.read("/BIOS/bconf.lua"))
+end
+
+local bconfC, bconfErr = love.filesystem.load("/bconf.lua")
+if not bconfC then bconfC = love.filesystem.load("/BIOS/bconf.lua") end --Load the default BConfig
+setfenv(bconfC,{P = P}) --BConfig sandboxing
+local success, bconfRErr = pcall(bconfC)
+if not success then
+  bconfC = love.filesystem.load("/BIOS/bconf.lua")
+  setfenv(bconfC,{P = P}) --BConfig sandboxing
+  bconfC()
+end --Load the default BConfig
+
+--Coroutine Creation
+
+
+
+--[[local GPU = Peripherals.GPU({_ClearOnRender=true}) --Create a new GPU
 
 --FPS display
 events:register("love:update",function(dt) love.window.setTitle("LIKO-12 FPS: "..love.timer.getFPS()) end)
@@ -43,4 +86,4 @@ GPU.line(191, 2,2,127, 9)
 GPU.rect(10,42,10,10,false,9)
 GPU.rect(10,30,10,10,false,9)
 GPU.rect(10,30,10,10,true,8)
-GPU.points(10,10, 10,19, 19,19, 19,10, 8)
+GPU.points(10,10, 10,19, 19,19, 19,10, 8)]]
