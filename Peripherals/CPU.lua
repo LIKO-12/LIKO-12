66d520c66d874136bd4c7de7e43c00656b767fa2
Rami Sabbagh <ramilego4game@gmail.com>
2016-12-30 21:07:28 +0200

    Wrote the events system

--- Peripherals/CPU.lua
+++ Peripherals/CPU.lua
@@ -1,18 +1,50 @@
 local events = require("Engine.events")
+local coreg = require("Engine.coreg")
 
 return function(config) --A function that creates a new CPU peripheral.
+  local EventStack = {}
+  local Instant = false
+  
+  local function hookEvent(pre,name)
+    events:register(pre..":"..name,function(...)
+    if Instant then
+      Instant = false coreg:resumeCoroutine(true,name,...)
+    else
+      --[[local args = {...}
+      local nargs = {name}
+      for k,v in ipairs(args) do table.insert(nargs,v) end]]
+      table.insert(EventStack,{name,...})
+    end
+    end)
+  end
+  
+  hookEvent("love","update")
+  hookEvent("love","keypressed")
+  hookEvent("love","keyreleased")
+  
+  hookEvent("GPU","mousepressed") --So they are translated to the LIKO-12 screen
+  hookEvent("GPU","mousemoved")
+  hookEvent("GPU","mousereleased")
+  hookEvent("GPU","touchpressed")
+  hookEvent("GPU","touchmoved")
+  hookEvent("GPU","touchreleased")
+  
   --The api starts here--
   local CPU = {}
   
-  function CPU.pullEvent(func)
-    
-  end
-  
-  --Register a function to be called when love.update is called
-  function CPU.hookUpdate(func)
-    if type(func) ~= "function" then return false, "UpdateFunc should be a function, provided: "..type(func) end
-    events:register("love:update",func)
-    return true
+  function CPU.pullEvent()
+    if #EventStack == 0 then
+      Instant = true
+      return 2 --To quit the coroutine resuming loop
+    else
+      local lastEvent = EventStack[1]
+      local newEventStack = {}
+      for k,v in ipairs(EventStack) do
+        if k > 1 then table.insert(newEventStack,v) end --Remove the last event.
+      end
+      EventStack = newEventStack
+      return true, unpack(lastEvent)
+    end
   end
   
   return CPU
