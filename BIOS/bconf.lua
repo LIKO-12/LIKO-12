d5c2e8e10f76fa518dda444c02aceac211c45fdb
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2016-12-30 18:46:43 +0200

    Added debug code

--- BIOS/bconf.lua
+++ BIOS/bconf.lua
@@ -2,15 +2,15 @@
 --per ,err = P(peripheral,mountedName,configTable)
 
 --Create a new virtual hdd system mounted as "HDD"
-P("HDD","HDD",{
+assert(P("HDD","HDD",{
   C = 1024*1024 * 50, --Measured in bytes, equals 50 megabytes
   D = 1024*1024 * 50 --Measured in bytes, equals 50 megabytes
-})
+}))
 
 --Create a new cpu mounted as "CPU"
-P("CPU")
+assert(P("CPU"))
 
 --Create a new gpu mounted as "GPU"
-P("GPU","GPU",{
+assert(P("GPU","GPU",{
   _ClearOnRender = true
-})
+}))
