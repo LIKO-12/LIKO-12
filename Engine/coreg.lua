fabec7769eada0a0f1db3ab67624b16a4e98b26b
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2016-12-30 11:04:48 +0200

    Coroutine system

--- Engine/coreg.lua
+++ Engine/coreg.lua
@@ -1,6 +1,108 @@
 --Corouting Registry: this file is responsible for providing LIKO12 it's api--
 local coreg = {reg={}}
 
+--Returns the current active coroutine if exists
+function coreg:getCoroutine()
+  return self.co
+end
+
+--Sets the current active coroutine
+function coreg:setCoroutine(co)
+  self.co  = co
+  return self
+end
+
+function coreg:sandboxCoroutine(co)
+  local GLOB = {
+    assert=assert,
+    error=error,
+    ipairs=ipairs,
+    pairs=pairs,
+    ripairs = lume.ripairs,
+    next=next,
+    pcall=pcall,
+    select=select,
+    tonumber=tonumber,
+    tostring=tostring,
+    type=type,
+    unpack=unpack,
+    _VERSION=_VERSION,
+    xpcall=xpcall,
+    getfenv=getfenv,
+    setfenv=setfenv,
+    string={
+      byte=string.byte,
+      char=string.char,
+      find=string.find,
+      format=string.format,
+      gmatch=string.gmatch,
+      gsub=string.gsub,
+      len=string.len,
+      lower=string.lower,
+      match=string.match,
+      rep=string.rep,
+      reverse=string.reverse,
+      sub=string.sub,
+      upper=string.upper
+    },
+    table={
+      insert=table.insert,
+      maxn=table.maxn,
+      remove=table.remove,
+      sort=table.sort
+    },
+    math={
+      abs=math.abs,
+      acos=math.acos,
+      asin=math.asin,
+      atan=math.atan,
+      atan2=math.atan2,
+      ceil=math.ceil,
+      cos=math.cos,
+      cosh=math.cosh,
+      deg=math.deg,
+      exp=math.exp,
+      floor=math.floor,
+      fmod=math.fmod,
+      frexp=math.frexp,
+      huge=math.huge,
+      ldexp=math.ldexp,
+      log=math.log,
+      log10=math.log10,
+      max=math.max,
+      min=math.min,
+      modf=math.modf,
+      pi=math.pi,
+      pow=math.pow,
+      rad=math.rad,
+      random=math.random,
+      randomseed=math.randomseed,
+      sin=math.sin,
+      sinh=math.sinh,
+      sqrt=math.sqrt,
+      tan=math.tan,
+      tanh=math.tanh,
+    },
+    coroutine={
+      resume = coroutine.resume,
+      yeild = coroutine.yeild
+    }
+  }
+  GLOB.loadstring = function(...)
+    local chunk, err = loadstring(...)
+    if not chunk then return nil, err end
+    setfenv(chunk,GLOB)
+    return chunk
+  end
+  GLOB.coroutine.create = function(...)
+    local co,err = pcall(coroutine.create,...)
+    if not co then return error(err) end
+    setfenv(co,GLOB)
+    return co
+  end  
+  GLOB._G=GLOB --Mirror Mirror
+end
+
 --Register a value to a specific key.
 --If the value is a table, then the values in the table will be registered at key:tableValueKey
 --If the value is a function, then it will be called instantly, and it must return true as the first argument to tell that it ran successfully.
