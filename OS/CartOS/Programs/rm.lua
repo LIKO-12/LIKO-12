65ebb891b422598feda49ebf6d56bc5fe0bbf5d5
Rami Sabbagh <ramilego4game@gmail.com>
2017-01-21 15:57:05 +0200

    Bugfix

--- OS/CartOS/Programs/rm.lua
+++ OS/CartOS/Programs/rm.lua
@@ -28,7 +28,7 @@ if d and p then if fs.exists(tar) then index(tar) end return end
 local d = tar:match("/(.+)")
 if d then if fs.exists(term.getdrive().."://"..tar) then index(term.getdrive().."://"..tar) end return end
 if fs.exists(term.getpath()..tar) then
-  index(term.getpath()..tar)
+  index(term.getpath()..(tar:sub(-2,-1) == "/" and tar or tar.."/"))
   return
 end
 color(9) print("Path doesn't exists")
