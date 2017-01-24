ec7f4e68fd6ea8e5883a7335dfd51d046e0121ac
Rami Sabbagh <ramilego4game@gmail.com>
2017-01-24 06:16:15 +0200

    Added SpriteGroup

--- OS/DiskOS/api.lua
+++ OS/DiskOS/api.lua
@@ -16,6 +16,13 @@ function SpriteSheet(img,w,h)
   return ss
 end
 
+function SpriteGroup(id,x,y,w,h,sx,sy,sheet)
+  local sx,sy = math.floor(sx or 1), math.floor(sy or 1)
+  for spry = 1, h or 1 do for sprx = 1, w or 1 do
+    sheet:draw((id-1)+sprx+(spry*24-24),x+(sprx*sx*8-sx*8),y+(spry*sy*8-sy*8),0,sx,sy)
+  end end
+end
+
 function isInRect(x,y,rect)
   if x >= rect[1] and y >= rect[2] and x <= rect[1]+rect[3] and y <= rect[2]+rect[4] then return true end return false
 end
