884a4c08310f6ab1f1270c9fc2225e0464d01c00
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-01-28 01:24:29 +0200

    Made ce:drawBuffer, now requires lume and colorize_lua

--- OS/DiskOS/Editors/code.lua
+++ OS/DiskOS/Editors/code.lua
@@ -3,11 +3,34 @@ local ce = {} --Code editor
 local buffer = {} --A table containing lines of code
 
 local screenW, screenH = screenSize()
+local lume = require("C://Libraries/lume")
+local clua = require("C://Libraries/colorize_lua")
+local cluacolors = {
+text = 8,
+keyword = 11,
+number = 13,
+comment = 14,
+str = 13
+}
 
+ce.bkc = 6--Background Color
 ce.cx, cy = 1, 1 --Cursor Position
 ce.tw, ce.th = termSize() --The terminal size
 ce.th = ce.th-2 --Because of the top and bottom bars
 ce.vx, ce.vy = 1,1 --View postions
+
+--A usefull print function with color support !
+function ce:colorPrint(tbl,gx,gy)
+  pushColor()
+  for i=1, #tbl, i=i+2 do
+    local col = tbl[i]
+    local txt = tbl[i+1]
+    color(col)
+    print(txt,_,true)--Disable auto newline
+  end
+  print("")--A new line
+  popColor()
+end
 
 --Check the position of the cursor so the view includes it
 function ce:checkPos()
@@ -34,7 +57,12 @@ end
 
 --Draw the code on the screen
 function ce:drawBuffer()
-  
+  local cbuffer = clua(lume.clone(lume.slice(buffer,self.shy,self.shy+self.th)),cluacolors)
+  rect(1,9,screenW,screenH-8*2,false,self.bkc)
+  for k, l in ipairs(cbuffer) do
+    printCursor(-(self.shx-1),k+1,0)
+    self:colorPrint(l)
+  end
 end
 
 function ce:entered()
