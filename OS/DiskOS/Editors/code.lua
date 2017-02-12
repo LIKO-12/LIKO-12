3abb134399b143548bb1579ac51e1f8c1420b4d9
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-02-12 21:11:56 +0200

    Up, Down, Left, Right, Backspace, better Return, Bugfixes !

--- OS/DiskOS/Editors/code.lua
+++ OS/DiskOS/Editors/code.lua
@@ -40,25 +40,28 @@ end
 
 --Check the position of the cursor so the view includes it
 function ce:checkPos()
-  --X position checking--
-  if self.cx > self.tw + (self.vx-1) then --Passed the screen to the right
-    self.vx = self.cx - (self.tw-1)
-  elseif self.cx < self.vx then --Passed the screen to the left
-    self.vx = self.cx
-  end
-  
+  local flag = false --Flag if the whole buffer requires redrawing
   --Y position checking--
   if self.cy > #buffer then self.cy = #buffer end --Passed the end of the file
   
   if self.cy > self.th + self.vy-1 then --Passed the screen to the bottom
-    self.vy = self.cy - (self.th-1)
+    self.vy = self.cy - (self.th-1); flag = true
   elseif self.cy < self.vy then --Passed the screen to the top
-    if self.cy < 1 then
-      self.cy = 1
-    else
-      self.vy = self.cy
-    end
+    if self.cy < 1 then self.cy = 1 end
+    self.vy = self.cy; flag = true
   end
+  
+  --X position checking--
+  if buffer[self.cy]:len() < self.cx-1 then self.cx = buffer[self.cy]:len()+1 end --Passed the end of the line !
+  
+  if self.cx > self.tw + (self.vx-1) then --Passed the screen to the right
+    self.vx = self.cx - (self.tw-1); flag = true
+  elseif self.cx < self.vx then --Passed the screen to the left
+    if self.cx < 1 then self.cx = 1 end
+    self.vx = self.cx; flag = true
+  end
+  
+  return flag
 end
 
 --Draw the cursor blink
@@ -70,7 +73,7 @@ end
 
 --Draw the code on the screen
 function ce:drawBuffer()
-  local cbuffer = clua(lume.clone(lume.slice(buffer,self.vy,self.vy+self.th)),cluacolors)
+  local cbuffer = clua(lume.clone(lume.slice(buffer,self.vy,self.vy+self.th-1)),cluacolors)
   rect(1,9,screenW,screenH-8*2,false,self.bgc)
   for k, l in ipairs(cbuffer) do
     printCursor(-(self.vx-2),k+1,0)
@@ -90,20 +93,76 @@ end
 function ce:textinput(t)
   buffer[self.cy] = buffer[self.cy]:sub(0,self.cx-1)..t..buffer[self.cy]:sub(self.cx,-1)
   self.cx = self.cx + t:len()
-  self:checkPos()
-  self:drawLine()
+  if self:checkPos() then self:drawBuffer() else self:drawLine() end
 end
 
 ce.keymap = {
   ["return"] = function(self)
+    local newLine = buffer[self.cy]:sub(self.cx,-1)
+    buffer[self.cy] = buffer[self.cy]:sub(0,self.cx-1)
     self.cx, self.cy = 1, self.cy+1
     if self.cy > #buffer then
-      table.insert(buffer,"")
+      table.insert(buffer,newLine)
     else
-      buffer = lume.concat(lume.slice(buffer,0,self.cy-1),{""},lume.slice(buffer,self.cy,-1))
+      buffer = lume.concat(lume.slice(buffer,0,self.cy-1),{newLine},lume.slice(buffer,self.cy,-1)) --Insert between 2 different lines
     end
     self:checkPos()
     self:drawBuffer()
+  end,
+  
+  ["left"] = function(self)
+    local flag = false
+    self.cx = self.cx -1
+    if self.cx < 1 then
+      if self.cy > 1 then
+        self.cy = self.cy -1
+        self.cx = buffer[self.cy]:len()+1
+        flag = true
+      end
+    end
+    if self:checkPos() or flag then self:drawBuffer() else self:drawLine() end
+  end,
+  
+  ["right"] = function(self)
+    local flag = false
+    self.cx = self.cx +1
+    if self.cx > buffer[self.cy]:len()+1 then
+      if buffer[self.cy+1] then
+        self.cy = self.cy +1
+        self.cx = 1
+        flag = true
+      end
+    end
+    if self:checkPos() or flag then self:drawBuffer() else self:drawLine() end
+  end,
+  
+  ["up"] = function(self)
+    self.cy = self.cy -1
+    self:checkPos()
+    self:drawBuffer()
+  end,
+  
+  ["down"] = function(self)
+    self.cy = self.cy +1
+    self:checkPos()
+    self:drawBuffer()
+  end,
+  
+  ["backspace"] = function(self)
+    if self.cx == 1 then
+      if self.cy > 1 then
+        self.cx = buffer[self.cy-1]:len()+1
+        buffer[self.cy-1] = buffer[self.cy-1] .. buffer[self.cy]
+        buffer = lume.concat(lume.slice(buffer,0,self.cy-1),lume.slice(buffer,self.cy+1,-1))
+        self.cy = self.cy-1
+        self:checkPos()
+        self:drawBuffer()
+      end
+    else
+      buffer[self.cy] = buffer[self.cy]:sub(0,self.cx-2) .. buffer[self.cy]:sub(self.cx, -1)
+      self.cx = self.cx-1
+      if self:checkPos() then self:drawBuffer() else self:drawLine() end
+    end
   end
 }
 
