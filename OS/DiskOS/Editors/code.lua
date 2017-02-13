aa1bfc071a14fe13fbc323f9d2e35dcd3181e2a9
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-02-13 16:33:32 +0200

    You can change the cursor pos by mouse now ! Added contributing guide

--- OS/DiskOS/Editors/code.lua
+++ OS/DiskOS/Editors/code.lua
@@ -1,5 +1,36 @@
 local eapi = select(1,...) --The editor library is provided as an argument
 
+--=Contributing Guide=--
+--[[
+Try your best to keep your work light, documented and teady, sine this will be the base of other places where text editors exists.
+
+self.cx, self.cy are the position of the brown blinking cursor
+* the top-left corner pos is 1, 1
+You don't have to take care when the cursor is out of range
+You only have to call self:checkPos() whenever you change self.cx or self.cy
+Note that when you call checkPos it will return you a boolean value
+if this is true: You have to call self:drawBuffer()
+if this is false: It's enough to call self:drawLine()
+Lua code: if self:checkPos() then self:drawBuffer() else self:drawLine() end
+Note: when you change the self.cy you have to drawBuffer, because it may left the brown cursor rect.
+
+self:drawBuffer() draws all visible lines
+self:drawLine() will only draw the line of self.cy
+
+self.vx, self.vy are the position of the top-left corner of the visible area in the editor
+You don't have to take care of this when working with self:checkPos()
+But if you ever wanted to change the v pos where blinking cursor is not visible you have to edit checkPos for that somehow
+Infact self:checkPos() returns true whenever it changes vx, vy or cy
+So you will have to self:drawBuffer() for that
+
+Be sure to read the contributing guide in Editors/init.lua
+
+==Contributers to this file==
+(Add your name when contributing to this file)
+
+- Rami Sabbagh (RamiLego4Game)
+]]
+
 local ce = {} --Code editor
 
 local buffer = {""} --A table containing lines of code
@@ -24,6 +55,9 @@ ce.vx, ce.vy = 1,1 --View postions
 ce.btimer = 0 --The cursor blink timer
 ce.btime = 0.5 --The cursor blink time
 ce.bflag = true --The cursor is blinking atm ?
+
+ce.sw, ce.sh = screenSize()
+local charGrid = {1,9, ce.sw,ce.sh-16, ce.tw, ce.th-2}
 
 --A usefull print function with color support !
 function ce:colorPrint(tbl)
@@ -183,6 +217,16 @@ end
 
 function ce:leaved()
   
+end
+
+function ce:mousepressed(x, y, button, istouch)
+  local cx, cy = whereInGrid(x,y, charGrid)
+  if cx then
+    self.cx = self.vx + (cx-1)
+    self.cy = self.vy + (cy-1)
+    self:checkPos()
+    self:drawBuffer()
+  end
 end
 
 function ce:touchpressed() textinput(true) end
