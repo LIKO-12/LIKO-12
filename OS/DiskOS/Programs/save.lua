be27d056fa914b7b3ca5019b76f43cd9f100ac8d
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-02-28 15:19:06 +0200

    Now requires eapi instead of using the exposed one.

--- OS/DiskOS/Programs/save.lua
+++ OS/DiskOS/Programs/save.lua
@@ -1,12 +1,11 @@
 local destination = select(1,...)
 
 local term = require("C://terminal")
+local eapi = require("C://Editors")
 destination = term.parsePath(destination)
 
 if fs.exists(destination) and fs.isDirectory(destination) then color(9) print("Destination must not be a directory") return end
 
-local eapi = _Editor
-
 fs.write(destination,eapi:export())
 
-print("Saved successfully")
+color(12) print("\nSaved successfully")
