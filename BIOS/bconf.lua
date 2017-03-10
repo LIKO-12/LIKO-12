36abf9cc51a1c1a21e2fd29ae46170413f07a7a3
= <ramilego4game@gmail.com>
2017-03-10 09:46:08 +0200

    Now passes the CPUKit to the other peripherals

--- BIOS/bconf.lua
+++ BIOS/bconf.lua
@@ -1,21 +1,22 @@
 --The default configuation
 --per ,err = P(peripheral,mountedName,configTable)
 
---Create a new virtual hdd system mounted as "HDD"
-assert(P("HDD","HDD",{
-  C = 1024*1024 * 25, --Measured in bytes, equals 50 megabytes
-  D = 1024*1024 * 25 --Measured in bytes, equals 50 megabytes
-}))
-
 --Create a new cpu mounted as "CPU"
-assert(P("CPU"))
+local CPU, CPUKit = assert(P("CPU"))
 
 --Create a new gpu mounted as "GPU"
 assert(P("GPU","GPU",{
   --_LIKO_W = 8*48, --384
   --_LIKO_H = 8*32, --256
-  _ClearOnRender = true
+  _ClearOnRender = true,
+  CPUKit = CPUKit
 }))
 
 --Create a new keyboard api mounted as "KB"
-assert(P("Keyboard"))
+assert(P("Keyboard","Keyboard",{CPUKit = CPUKit}))
+
+--Create a new virtual hdd system mounted as "HDD"
+assert(P("HDD","HDD",{
+  C = 1024*1024 * 25, --Measured in bytes, equals 50 megabytes
+  D = 1024*1024 * 25 --Measured in bytes, equals 50 megabytes
+}))
