3d0529c6a476b5ddb59593c6b2d3611e8c3b159f
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-03-20 23:20:04 +0200

    Bugfix black cursor in gif recordings

--- Peripherals/GPU/init.lua
+++ Peripherals/GPU/init.lua
@@ -134,8 +134,11 @@ return function(config) --A function that creates a new GPU peripheral.
     
     vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords) {
       int index=int(Texel(texture, texture_coords).r*255.0+0.5);
+      float ta=float(Texel(texture,texture_coords).a);
       // lookup the colour in the palette by index
-      return palette[index]/255.0;
+      vec4 col=palette[index]/255.0;
+      col.a = col.a*color.a*ta;
+      return col;
   }]])
   _DisplayShader:send('palette', unpack(_DisplayPalette))
   
