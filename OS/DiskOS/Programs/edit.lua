b68a27661b66e2807ccf66fedeeb86c6a24b10a0
= <ramilego4game@gmail.com>
2017-04-02 15:49:25 +0300

    Impovements: No longer creates a new empty file if not saved. You can now press file actions (top right buttons) by mouse. The file actions buttons are now drawn dynamically

--- OS/DiskOS/Programs/edit.lua
+++ OS/DiskOS/Programs/edit.lua
@@ -6,7 +6,6 @@ local tar = table.concat(args," ") --The path may include whitespaces
 local term = require("C://terminal")
 tar = term.parsePath(tar)
 
-if not fs.exists(tar) then fs.write(tar,"") end
 if fs.isDirectory(tar) then color(9) print("Can't edit directories !") return end
 local eapi = require("C://Editors")
 local edit = {}
@@ -23,12 +22,16 @@ function edit:drawBottomBar()
   rect(1,sheight-7,swidth,8,false,self.flavor)
 end
 
+local controlID = 11
+local controlNum = 3 --The number of the control buttons at the top right corner of the editor.
+local controlGrid = {swidth-8*controlNum+1,1, 8*controlNum,8, controlNum,1}
+
 function edit:drawTopBar()
   rect(1,1,swidth,8,false,self.flavor)
   SpriteGroup(55, 1,1, 4,1, 1,1, false, self.editorsheet) --The LIKO12 Logo
-  SpriteGroup(11, swidth-8*3+1,1, 3,1, 1,1, false, self.editorsheet)
+  SpriteGroup(controlID, controlGrid[1],controlGrid[2], controlGrid[5],controlGrid[6], 1,1, false, self.editorsheet)
   if sid then
-    SpriteGroup(35+sid, swidth-(2-sid+1)*8+1,1, 1,1, 1,1, false, self.editorsheet)
+    SpriteGroup(controlID+24+sid, controlGrid[1]+sid*8,controlGrid[2], 1,1, 1,1, false, self.editorsheet)
   end
 end
 
@@ -44,10 +47,29 @@ if tar:sub(-4,-1) ~= ".lua" then texteditor.colorize = false end
 local screen = screenshot()
 local px,py,pc = printCursor()
 cursor("normal")
-texteditor:import(fs.read(tar))
+if fs.exists(tar) then texteditor:import(fs.read(tar)) end
 texteditor:entered()
 
 local eflag = false
+local hflag = false --hover flag
+
+local controls = {
+  function() --Reload
+    texteditor:leaved()
+    if fs.exists(tar) then texteditor:import(fs.read(tar)) end
+    texteditor:entered()
+  end,
+  
+  function() --Save
+    local data = texteditor:export()
+    fs.write(tar,data)
+  end,
+  
+  function() --Exit
+    texteditor:leaved()
+    return true
+  end 
+}
 
 for event, a,b,c,d,e,f in pullEvent do
   if event == "keypressed" then
@@ -55,7 +77,13 @@ for event, a,b,c,d,e,f in pullEvent do
       --[[texteditor:leaved()
       break]]
       eflag = not eflag
-      if eflag then sid = 1 else sid = false end
+      if eflag then
+        cursor("none")
+        sid = 1
+      else
+        cursor("normal")
+        sid = false
+      end
       edit:drawTopBar()
     elseif eflag then
       if a == "left" then
@@ -67,17 +95,7 @@ for event, a,b,c,d,e,f in pullEvent do
         if sid > 2 then sid = 0 end
         edit:drawTopBar()
       elseif a == "return" then
-        if sid == 0 then --Reload
-          texteditor:leaved()
-          texteditor:import(fs.read(tar))
-          texteditor:entered()
-        elseif sid == 1 then --Save
-          local data = texteditor:export()
-          fs.write(tar,data)
-        elseif sid == 2 then --Exit
-          texteditor:leaved()
-          break
-        end
+        if controls[sid+1]() then break end
         sid, eflag = false, false
         edit:drawTopBar()
       end
@@ -98,6 +116,56 @@ for event, a,b,c,d,e,f in pullEvent do
       
       if texteditor.keymap and texteditor.keymap[key] then texteditor.keymap[key](texteditor,c)
       elseif texteditor.keymap and texteditor.keymap[sc] then texteditor.keymap[sc](texteditor,c) end
+      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
+    end
+  elseif event == "mousepressed" and not eflag then
+    local cx, cy = whereInGrid(a,b,controlGrid)
+    if cx then
+      cursor("handpress")
+      hflag = "d"
+      sid = cx-1
+      edit:drawTopBar()
+    else
+      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
+    end
+  elseif event == "mousemoved" and not eflag then
+    local cx, cy = whereInGrid(a,b,controlGrid)
+    if cx then
+      if hflag and hflag == "d" then
+        sid = cx-1
+        edit:drawTopBar()
+      elseif not hflag then
+        cursor("handrelease")
+        hflag = "h"
+      end
+    else
+      if hflag and hflag == "h" then
+        hflag = false
+        cursor("normal")
+      end
+      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
+    end
+  elseif event == "mousereleased" and not eflag then
+    local cx, cy = whereInGrid(a,b,controlGrid)
+    if cx then
+      if hflag and hflag == "d" then
+        cursor("handrelease")
+        if controls[sid+1]() then break end
+        sid, hflag = false, false
+        edit:drawTopBar()
+      elseif not hflag then
+        hflag = "h"
+        cursor("handrelease")
+      end
+    else
+      if hflag then
+        if hflag == "d" then
+          sid = false
+          edit:drawTopBar()
+        end
+        cursor("normal")
+        hflag = nil
+      end
       if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
     end
   elseif eflag then
