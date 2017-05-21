dbd5ba57e05ca89d9bf500dc7dbc52a40416fc75
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-05-21 10:09:12 +0300

    Finished All Memory Opreations, Needs Layout Building Now.

--- Peripherals/RAM.lua
+++ Peripherals/RAM.lua
@@ -97,14 +97,16 @@ return function(config)
     end
   end
   
+  local function tohex(val) return string.format("0x%X",val) end
+  
   local api = {}
   
   function api:poke(address,value)
     if type(address) ~= "number" then return false, "Address must be a number, provided: "..type(address) end
     if type(value) ~= "number" then return false, "Value must be a number, provided: "..type(value) end
-    if address < 0 or address > ramsize-1 then return false, "Address out of range (0x"..string.format("%X",address).."), must be in range [0,"..lastaddr.."]" end
-    if value < 0 or value > 255 then return false, "Value out of range ("..value..") must be in range [0,255]" end
     address, value = math.floor(address), math.floor(value)
+    if address < 0 or address > ramsize-1 then return false, "Address out of range ("..tohex(address).."), must be in range [0x0,"..lastaddr.."]" end
+    if value < 0 or value > 255 then return false, "Value out of range ("..value..") must be in range [0,255]" end
     
     local handler = devkit.defaultHandler
     for k,h in ipairs(handlers) do
@@ -118,7 +120,8 @@ return function(config)
   
   function api:peek(address)
     if type(address) ~= "number" then return false, "Address must be a number, provided: "..type(address) end
-    if address < 0 or address > ramsize-1 then return false, "Address out of range (0x"..string.format("%X",address).."), must be in range [0,"..lastaddr.."]" end
+    address = math.floor(address)
+    if address < 0 or address > ramsize-1 then return false, "Address out of range ("..tohex(address).."), must be in range [0x0,"..lastaddr.."]" end
     
     local handler = devkit.defaultHandler
     for k,h in ipairs(handlers) do
@@ -133,9 +136,9 @@ return function(config)
   function api:memget(address,length)
     if type(address) ~= "number" then return false, "Address must be a number, provided: "..type(address) end
     if type(length) ~= "number" then return false, "Length must be a number, provided: "..type(length) end
-    if address < 0 or address > ramsize-1 then return false, "Address out of range (0x"..string.format("%X",address).."), must be in range [0,"..lastaddr.."]" end
-    if address+length < 1 or address+length > ramsize then return false, "Length out of range ("..length..")" end
     address, length = math.floor(address), math.floor(length)
+    if address < 0 or address > ramsize-1 then return false, "Address out of range ("..tohex(address).."), must be in range [0x0,"..lastaddr.."]" end
+    if length == 0 or address+length < 0 or address+length > ramsize then return false, "Length out of range ("..length..")" end
     local endAddress = address+length-1
     
     local str = ""
@@ -154,22 +157,23 @@ return function(config)
     return true, str
   end
   
-  --NOT COMPLETE FOR EDGE CASES
   function api:memset(address,data)
     if type(address) ~= "number" then return false, "Address must be a number, provided: "..type(address) end
     if type(data) ~= "string" then return false, "Data must be a string, provided: "..type(data) end
-    if address < 0 or address > ramsize-1 then return false, "Address out of range (0x"..string.format("%X",address).."), must be in range [0,"..lastaddr.."]" end
-    local length = data:len()
-    if address+length < 1 or address+length > ramsize then return false, "Data too long to fit in the memory ("..length..")" end
     address = math.floor(address)
+    if address < 0 or address > ramsize-1 then return false, "Address out of range ("..tohex(address).."), must be in range [0x0,"..lastaddr.."]" end
+    local length = data:len()
+    if length == 0 then return false, "Cannot set empty string" end
+    if address+length < 1 or address+length > ramsize then return false, "Data too long to fit in the memory ("..length.." character)" end
     local endAddress = address+length-1
     
     for k,h in ipairs(handlers) do
       if endAddress > h.startAddr then
         if address < h.endAddr then
-          local sa, ea = address, endAddress
+          local sa, ea, d = address, endAddress, data
           if sa < h.startAddr then sa = h.startAddr end
           if ea > h.endAddr then ea = h.endAddr end
+          d = data:sub(sa-address+1,ea-address+1)
           h.handler("memset",sa,data)
         end
       end
@@ -179,7 +183,41 @@ return function(config)
   end
   
   function api:memcpy(from_address,to_address,length)
+    if type(from_address) ~= "number" then return false, "Source Address must be a number, provided: "..type(from_address) end
+    if type(to_address) ~= "number" then return false, "Destination Address must be a number, provided: "..type(to_address) end
+    if type(length) ~= "number" then return false,"Length must be a number, provided: "..type(length) end
+    from_address, to_address, length = math.floor(from_address), math.floor(to_address), math.floor(length)
+    if from_address < 0 or from_address > ramsize-1 then return false, "Source Address out of range ("..tohex(from_address).."), must be in range [0x0,"..lastaddr.."]" end
+    if to_address < 0 or to_address > ramsize-1 then return false, "Destination Address out of range ("..tohex(to_address).."), must be in range [0x0,"..lastaddr.."]" end
+    if length == 0 then return false, "Length can't be 0" end
+    if from_address+length < 1 or from_address+length > ramsize then return false, "Length out of range ("..length..")" end
+    if to_address+length < 1 then length = 1-to_address end
+    if to_address+length > ramsize then length = ramsize-to_address end
+    local from_end = from_address+length-1
+    local to_end = to_address+length-1
     
+    for k1,h1 in ipairs(handlers) do
+      if from_end > h1.startAddr and from_address < h1.endAddr then
+        local sa1, ea1 = from_address, from_end
+        if sa1 < h1.startAddr then sa1 = h1.startAddr end
+        if ea1 > h1.endAddr then ea1 = h1.endAddr end
+        for k2,h2 in ipairs(handlers) do
+          if to_end > h2.startAddr and to_address < h2.endAddr then
+            local sa2, ea2 = to_address, to_end
+            if sa2 < h2.startAddr then sa2 = h2.startAddr end
+            if ea2 > h2.endAddr then ea2 = h2.endAddr end
+            if h1.handler == h2.handler then --Direct Copy
+              h1.handler("memcpy",sa1,sa2,ea2-to_address+1)
+            else --InDirect Copy
+              local d = h1.handler("memget",sa1,ea2-to_address+1)
+              h2.handler("memset",sa2,d)
+            end
+          end
+        end
+      end
+    end
+    
+    return true
   end
   
   devkit.ramsize = ramsize
