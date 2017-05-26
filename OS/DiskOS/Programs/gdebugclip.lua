69e7d89bf8e68c472875659203af9cfcf3b99a74
Rami Sabbagh <ramilego4game@gmail.com>
2017-05-26 23:31:50 +0300

    Updated to work with the new GPU update

--- OS/DiskOS/Programs/gdebugclip.lua
+++ OS/DiskOS/Programs/gdebugclip.lua
@@ -6,17 +6,16 @@ fs.write("C://gdebugclip.lk12",data)
 --color(12) print("Saved debug image successfully")
 clear(0)
 local img = image(data)
-img:draw(1,1)
 
 local sw,sh = screenSize()
 
 for event in pullEvent do
  if event == "update" then
   clear(0)
-  img:draw(1,1)
+  img:draw(0,0)
   local mx,my = getMPos()
   point(mx,my ,10)
-  color(7) print(mx..","..my,5,sh-8)
+  color(7) print(mx..","..my,4,sh-9)
  end
  if event == "keypressed" then return end
 end
