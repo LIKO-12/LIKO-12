a02e87b5619db25eceb318b36ee4dda6cc2b485d
Rami Sabbagh <ramilego4game@gmail.com>
2017-06-20 17:51:17 +0300

    Added dir commands, it's the same of ls command

---
+++ OS/DiskOS/Programs/dir.lua
@@ -0,0 +1,11 @@
+--Lists the folders and files at the current directory.
+--The same of ls.lua
+local term = require("C://terminal") --Require the terminal api.
+local path = term.getpath() --Get the current active directory.
+local files = fs.directoryItems(path) --Returns a table containing the names of folders and files in the given directory
+
+for k, f in ipairs(files) do
+  color(fs.isFile(path..f) and 7 or 11)
+  print(f.." ",false)
+end
+print("")
