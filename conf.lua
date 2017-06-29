944d1ea71d6c1d07aaeef133fd90ccb1e8e4ec00
Rami Sabbagh <ramilego4game@gmail.com>
2017-06-29 17:50:42 +0300

    Enabled the love.thread module \o/

--- conf.lua
+++ conf.lua
@@ -9,20 +9,20 @@ function love.conf(t)
     
     t.window = false --The window will be created later by the GPU Peripheral.
  
-    t.modules.audio = false             -- Enable the audio module (boolean)
+    t.modules.audio = false             -- Disable the audio module (boolean)
     t.modules.event = true              -- Enable the event module (boolean)
     t.modules.graphics = true           -- Enable the graphics module (boolean)
     t.modules.image = true              -- Enable the image module (boolean)
-    t.modules.joystick = false          -- Enable the joystick module (boolean)
+    t.modules.joystick = false          -- Disable the joystick module (boolean)
     t.modules.keyboard = true           -- Enable the keyboard module (boolean)
     t.modules.math = true               -- Enable the math module (boolean)
     t.modules.mouse = true              -- Enable the mouse module (boolean)
-    t.modules.physics = false           -- Enable the physics module (boolean)
-    t.modules.sound = true              -- Enable the sound module (boolean)
+    t.modules.physics = false           -- Disable the physics module (boolean)
+    t.modules.sound = false             -- Disable the sound module (boolean)
     t.modules.system = true             -- Enable the system module (boolean)
     t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
     t.modules.touch = true              -- Enable the touch module (boolean)
-    t.modules.video = false             -- Enable the video module (boolean)
+    t.modules.video = false             -- Disable the video module (boolean)
     t.modules.window = true             -- Enable the window module (boolean)
-    t.modules.thread = false            -- Enable the thread module (boolean)
+    t.modules.thread = true             -- Enable the thread module (boolean)
 end
