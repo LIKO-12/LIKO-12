e390e8d1b992bd82251c40a2e9bda9fb12a56d58
RamiLego4Game <ramilego4game@gmail.com>
2017-07-27 00:52:36 +0300

    Added a function to return a list of available LIKO-12 api functions

--- OS/DiskOS/osapi.lua
+++ OS/DiskOS/osapi.lua
@@ -23,3 +23,40 @@ function printUsage(...)
     end
   end
 end
+
+function dofile(path,...)
+  local chunk, err = fs.load(path)
+  if not chunk then return error(tostring(err)) end
+  local args = {pcall(chunk,...)}
+  if not args[1] then return error(tostring(args[2])) end
+  for k,v in ipairs(args) do
+    if k > 1 then
+      args[k-1] = v
+    end
+  end
+  return unpack(args)
+end
+
+local apilist = {}
+
+do
+  local list = apilist
+  for k,v in pairs(_G) do
+    if type(v) ~= "table" then table.insert(list,k) end
+  end
+  
+  local function add(...)
+    for k,v in ipairs({...}) do
+      table.insert(list,v)
+    end
+  end
+  
+  add("dofile","printUsage","getAPI")
+  add("bump","class","lume")
+  add("Sprite","fget","fset","map","eventLoop","pget","pset","sget","sset","mget","mset","Controls")
+  add("SpriteMap","SheetFlagsData","TileMap","MapObj","btn","btnp","__BTNUpdate","__BTNKeypressed","__BTNTouchControl","_BTNGamepad")
+end
+
+function getAPI()
+  return apilist
+end
