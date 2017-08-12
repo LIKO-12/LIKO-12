d890e2328a0fe39aff1dfe856778878afd517688
RamiLego4Game <ramilego4game@gmail.com>
2017-08-12 23:49:02 +0300

    Made it load and run the ls.lua file instead of duplicating it

--- OS/DiskOS/Programs/dir.lua
+++ OS/DiskOS/Programs/dir.lua
@@ -1,5 +1,5 @@
 --Lists the folders and files at the current directory.
---The same of ls.lua
+--An alies of ls.lua
 
 if select(1,...) == "-?" then
   printUsage(
@@ -10,20 +10,4 @@ if select(1,...) == "-?" then
 end
 
 local term = require("terminal") --Require the terminal api.
-local path = term.getpath() --Get the current active directory.
-local dir = select(1,...) 
-
-if dir then
-	local newpath, exists = term.resolve(dir)
-	if not exists then color(9) print("Folder doesn't exists !") return end
-	if not fs.isDirectory(newpath) then color(9) print("It should be a folder, provided a file !") return end
-	path = newpath.."/"
-end
-
-local files = fs.directoryItems(path) --Returns a table containing the names of folders and files in the given directory
-
-for k, f in ipairs(files) do
-  color(fs.isFile(path..f) and 7 or 11)
-  print(f.." ",false)
-end
-print("")
+term.execute("ls",...)
