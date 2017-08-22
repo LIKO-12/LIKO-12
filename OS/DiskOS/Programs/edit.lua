cd3298a2f5ba0285dd1c5f6de1b1b5b778c28349
RamiLego4Game <ramilego4game@gmail.com>
2017-08-22 05:47:31 +0300

    Updated to use the new Editors.utils api

--- OS/DiskOS/Programs/edit.lua
+++ OS/DiskOS/Programs/edit.lua
@@ -12,182 +12,81 @@ local term = require("terminal")
 tar = term.resolve(tar)
 
 if fs.exists(tar) and fs.isDirectory(tar) then color(8) print("Can't edit directories !") return end
-local eapi = require("Editors")
-local edit = {}
-edit.editorsheet = eapi.editorsheet
-edit.flavor = eapi.flavor
-edit.flavorBack = eapi.flavorBack
-edit.background = eapi.background
+local eutils = require("Editors.utils")
+local tool = eutils:newTool()
 
-local swidth, sheight = screenSize()
+local ok, editor = assert(pcall(assert(fs.load("C:/Editors/code.lua")),tool))
+if tar:sub(-4,-1) ~= ".lua" then editor.colorize = false end
 
-local sid --Selected option id
-
-function edit:drawBottomBar()
-  rect(0,sheight-8,swidth,8,false,self.flavor)
+local data --Data to import at start.
+if fs.exists(tar) then
+  data = fs.read(tar):gsub("\r\n","\n")
 end
 
-local controlID = 11
-local controlNum = 3 --The number of the control buttons at the top right corner of the editor.
-local controlGrid = {swidth-8*controlNum,0, 8*controlNum,8, controlNum,1}
-
-function edit:drawTopBar()
-  rect(0,0,swidth,8,false,self.flavor)
-  SpriteGroup(55, 0,0, 4,1, 1,1, false, self.editorsheet) --The LIKO12 Logo
-  SpriteGroup(controlID, controlGrid[1],controlGrid[2], controlGrid[5],controlGrid[6], 1,1, false, self.editorsheet)
-  if sid then
-    SpriteGroup(controlID+24+sid, controlGrid[1]+sid*8,controlGrid[2], 1,1, 1,1, false, self.editorsheet)
-  end
+local function reload(tool)
+  if fs.exists(tar) then tool.editor:import(fs.read(tar)) end
+  _systemMessage("Reloaded successfully",1)
 end
 
-function edit:drawUI()
-  clear(self.background) --Clear the screen
-  self:drawTopBar() --Draw the top bar
-  self:drawBottomBar() --Draw the bottom bar
+local function save(tool)
+  local ndata = tool.editor:export()
+  fs.write(tar,ndata)
+  _systemMessage("Saved successfully",1)
 end
 
-local ok, texteditor = assert(pcall(assert(fs.load("C:/Editors/code.lua")),edit))
-if tar:sub(-4,-1) ~= ".lua" then texteditor.colorize = false end
-
-local screen = screenshot()
-local px,py,pc = printCursor()
-cursor("normal")
-if fs.exists(tar) then texteditor:import(fs.read(tar):gsub("\r\n","\n")) end
-texteditor:entered()
-
-local eflag = false
-local hflag = false --hover flag
-
-local controls = {
-  function() --Reload
-    texteditor:leaved()
-    if fs.exists(tar) then texteditor:import(fs.read(tar)) end
-    texteditor:entered()
-  end,
-
-  function() --Save
-    local data = texteditor:export()
-    fs.write(tar,data)
-  end,
-
-  function() --Exit
-    texteditor:leaved()
+local function hotkey(tool,key,sc)
+  if sc == "ctrl-r" then --Run the file
+    local bkframe = screenshot() --Backup the screen
+    local px, py, px = printCursor() --Backup the printing cursor
+    pushMatrix() --Backup the matrix
+    pushPalette() --Backup the palette
+    pushColor() --Backup the current color
+    
+    cam() --Reset the camera.
+    pal() palt() --Reset the palettes.
+    printCursor(0,0,0) --Reset the printing cursor.
+    clear() --Clear the screen.
+    color(7) --Set the active color to white.
+    
+    if tool.editor.colorize then --If it's a Lua file
+      local code = tool.editor:export() --Get the Lua code.
+      local chunk, cerr = loadstring(code)
+      if not chunk then
+        color(8)
+        print("CERR: "..tostring(cerr))
+      else
+        local ok, err = pcall(chunk)
+        if not ok then
+          color(8)
+          print("ERR: "..tostring(err))
+        end
+      end
+      color(9)
+      print("[Press any key to continue]",false) flip()
+      
+      local kflag
+      for event, a in pullEvent do
+        if event == "keypressed" then
+          kflag = a
+        elseif event == "keyreleased" and kflag and kflag == a then
+          break
+        end
+      end
+    end
+    
+    cam() --Reset the camera.
+    pal() palt() --Reset the palettes.
+    clear() --Clear the screen.
+    
+    bkframe:image():draw() --Restore the screen frame
+    
+    popColor() --Restore the color
+    popPalette() --Restore the palette
+    popMatrix() --Restore the matrix
+    printCursor(px,py,pc) --Restore the printing cursor
+    
     return true
   end
-}
-
-for event, a,b,c,d,e,f in pullEvent do
-  if event == "keypressed" then
-    if a == "escape" then
-      --[[texteditor:leaved()
-      break]]
-      eflag = not eflag
-      if eflag then
-        cursor("none")
-        sid = 1
-      else
-        cursor("normal")
-        sid = false
-      end
-      pushMatrix() cam() edit:drawTopBar() popMatrix()
-    elseif eflag then
-      if a == "left" then
-        sid = sid - 1
-        if sid < 0 then sid = 2 end
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif a == "right" then
-        sid = sid + 1
-        if sid > 2 then sid = 0 end
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif a == "return" then
-        if controls[sid+1]() then break end
-        sid, eflag = false, false
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      end
-    else
-      local key, sc = a, b
-      if(isKDown("lalt", "ralt")) then
-        key = "alt-" .. key
-        sc = "alt-" .. sc
-      end
-      if(isKDown("lctrl", "rctrl", "capslock")) then
-        key = "ctrl-" .. key
-        sc = "ctrl-" .. sc
-      end
-      if(isKDown("lshift", "rshift")) then
-        key = "shift-" .. key
-        sc = "shift-" .. sc
-      end
-
-      if texteditor.keymap then
-        local usedKey
-        if texteditor.keymap[key] then usedKey = key
-        elseif texteditor.keymap[sc] then usedKey = sc
-        end
-        if usedKey then
-          texteditor.keymap[usedKey](texteditor,c)
-          texteditor.lastKey = usedKey
-        end
-      end
-      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
-    end
-  elseif event == "mousepressed" and not eflag then
-    local cx, cy = whereInGrid(a,b,controlGrid)
-    if cx then
-      cursor("handpress")
-      hflag = "d"
-      sid = cx-1
-      pushMatrix() cam() edit:drawTopBar() popMatrix()
-    else
-      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
-    end
-  elseif event == "mousemoved" and not eflag then
-    local cx, cy = whereInGrid(a,b,controlGrid)
-    if cx then
-      if hflag and hflag == "d" then
-        sid = cx-1
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif not hflag then
-        cursor("handrelease")
-        hflag = "h"
-      end
-    else
-      if hflag and hflag == "h" then
-        hflag = false
-        cursor("normal")
-      end
-      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
-    end
-  elseif event == "mousereleased" and not eflag then
-    local cx, cy = whereInGrid(a,b,controlGrid)
-    if cx then
-      if hflag and hflag == "d" then
-        cursor("handrelease")
-        if controls[sid+1]() then break end
-        sid, hflag = false, false
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif not hflag then
-        hflag = "h"
-        cursor("handrelease")
-      end
-    else
-      if hflag then
-        if hflag == "d" then
-          sid = false
-          pushMatrix() cam() edit:drawTopBar() popMatrix()
-        end
-        cursor("normal")
-        hflag = nil
-      end
-      if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
-    end
-  elseif eflag then
-    if event == "touchpressed" then textinput(true) end
-  else
-    if texteditor[event] then texteditor[event](texteditor,a,b,c,d,e,f) end
-  end
 end
 
-clear()
-screen:image():draw(0,0)
-printCursor(px,py,pc)
+tool:start(editor,reload,save,data,hotkey)
--- OS/DiskOS/Programs/paint.lua
+++ OS/DiskOS/Programs/paint.lua
@@ -1,6 +1,4 @@
 --Paint Program--
-if false then color(8) print("Work in progress ....") return end
-
 local args = {...}
 if #args < 1 or args[1] == "-?" then
   printUsage("paint <filename>", "Creates or edits an existing image.")
@@ -42,187 +40,31 @@ else --Load the image
   imgdata = img:data()
 end
 
-local eapi = require("Editors")
-local edit = {}
-edit.editorsheet = eapi.editorsheet
-edit.flavor = eapi.flavor
-edit.flavorBack = eapi.flavorBack
-edit.background = 0
+local eutils = require("Editors.utils")
+local tool = eutils:newTool()
 
-local swidth, sheight = screenSize()
+local ok, editor = assert(pcall(assert(fs.load("C:/Editors/paint.lua")),tool,img,imgdata))
 
-local bgsprite = eapi.editorsheet:extract(59):image()
-local bgquad = bgsprite:quad(0,0,swidth,sheight-8*2)
-
-local sid --Selected option id
-
-function edit:drawBottomBar()
-  rect(0,sheight-8,swidth,8,false,self.flavor)
-end
-
-local controlID = 11
-local controlNum = 3 --The number of the control buttons at the top right corner of the editor.
-local controlGrid = {swidth-8*controlNum,0, 8*controlNum,8, controlNum,1}
-
-function edit:drawTopBar()
-  palt(0,true)
-  rect(0,0,swidth,8,false,self.flavor)
-  SpriteGroup(55, 0,0, 4,1, 1,1, false, self.editorsheet) --The LIKO12 Logo
-  SpriteGroup(controlID, controlGrid[1],controlGrid[2], controlGrid[5],controlGrid[6], 1,1, false, self.editorsheet)
-  if sid then
-    SpriteGroup(controlID+24+sid, controlGrid[1]+sid*8,controlGrid[2], 1,1, 1,1, false, self.editorsheet)
-  end
-  palt(0,false)
-end
-
-function edit:drawBackground()
-  rect(0,8,swidth,sheight-8*2,false,0)
-  bgsprite:draw(0,8,0,1,1,bgquad)
-end
-
-function edit:drawUI()
-  self:drawBackground()
-  self:drawTopBar() --Draw the top bar
-  self:drawBottomBar() --Draw the bottom bar
-end
-
-local screen = screenshot()
-local px,py,pc = printCursor()
-local ok, painteditor = assert(pcall(assert(fs.load("C:/Editors/paint.lua")),edit,img,imgdata))
-
-cursor("normal")
-painteditor:entered()
-
-local eflag = false
-local hflag = false --hover flag
-
-local controls = {
-  function() --Reload
-    painteditor:leaved()
-    if fs.exists(tar) then
-      local data = fs.read(tar)
-      local ok, err = pcall(image,data)
-      if not ok then color(8) print(err) return end
-      img = err
-      imgdata = img:data()
-      painteditor:import(img,imgdata)
+local function reload(tool)
+  if fs.exists(tar) then
+    local data = fs.read(tar)
+    local ok, err = pcall(image,data)
+    if not ok then
+      _systemMessage("ERR: "..tostring(err),5,9,4)
+      cprint("[Paint]: Failed to reload: "..tostring(err))
+      return
     end
-    painteditor:entered()
-  end,
-  
-  function() --Save
-    local data = painteditor:export()
-    fs.write(tar,data)
-  end,
-  
-  function() --Exit
-    painteditor:leaved()
-    return true
-  end 
-}
-
-for event, a,b,c,d,e,f in pullEvent do
-  if event == "keypressed" then
-    if a == "escape" then
-      eflag = not eflag
-      if eflag then
-        cursor("none")
-        sid = 1
-      else
-        cursor("normal")
-        sid = false
-      end
-      pushMatrix() cam() edit:drawTopBar() popMatrix()
-    elseif eflag then
-      if a == "left" then
-        sid = sid - 1
-        if sid < 0 then sid = 2 end
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif a == "right" then
-        sid = sid + 1
-        if sid > 2 then sid = 0 end
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif a == "return" then
-        if controls[sid+1]() then break end
-        sid, eflag = false, false
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      end
-    else
-      local key, sc = a, b
-      if(isKDown("lalt", "ralt")) then
-        key = "alt-" .. key
-        sc = "alt-" .. sc
-      end
-      if(isKDown("lctrl", "rctrl", "capslock")) then
-        key = "ctrl-" .. key
-        sc = "ctrl-" .. sc
-      end
-      if(isKDown("lshift", "rshift")) then
-        key = "shift-" .. key
-        sc = "shift-" .. sc
-      end
-      
-      if painteditor.keymap and painteditor.keymap[key] then painteditor.keymap[key](painteditor,c)
-      elseif painteditor.keymap and painteditor.keymap[sc] then painteditor.keymap[sc](painteditor,c) end
-      if painteditor[event] then painteditor[event](painteditor,a,b,c,d,e,f) end
-    end
-  elseif event == "mousepressed" and not eflag then
-    if painteditor[event] then painteditor[event](painteditor,a,b,c,d,e,f) end
-    local cx, cy = whereInGrid(a,b,controlGrid)
-    if cx then
-      cursor("handpress")
-      hflag = "d"
-      sid = cx-1
-      pushMatrix() cam() edit:drawTopBar() popMatrix()
-    end
-  elseif event == "mousemoved" and not eflag then
-    if painteditor[event] then painteditor[event](painteditor,a,b,c,d,e,f) end
-    local cx, cy = whereInGrid(a,b,controlGrid)
-    if cx then
-      if hflag and hflag == "h" then cursor("handrelease") end
-      if hflag and hflag == "d" then
-        sid = cx-1
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif not hflag then
-        cursor("handrelease")
-        hflag = "h"
-      end
-    else
-      if hflag and hflag == "h" then
-        hflag = false
-        cursor("normal")
-      end
-    end
-  elseif event == "mousereleased" and not eflag then
-    if painteditor[event] then painteditor[event](painteditor,a,b,c,d,e,f) end
-    local cx, cy = whereInGrid(a,b,controlGrid)
-    if cx then
-      if hflag and hflag == "d" then
-        cursor("handrelease")
-        if controls[sid+1]() then break end
-        sid, hflag = false, false
-        pushMatrix() cam() edit:drawTopBar() popMatrix()
-      elseif not hflag then
-        hflag = "h"
-        cursor("handrelease")
-      end
-    else
-      if hflag then
-        if hflag == "d" then
-          sid = false
-          pushMatrix() cam() edit:drawTopBar() popMatrix()
-        end
-        cursor("normal")
-        hflag = nil
-      end
-    end
-  elseif eflag then
-    if event == "touchpressed" then textinput(true) end
-  else
-    if painteditor[event] then painteditor[event](painteditor,a,b,c,d,e,f) end
+    img = err
+    imgdata = img:data()
+    tool.editor:import(img,imgdata)
+    _systemMessage("Reloaded successfully",1)
   end
 end
 
-clear()
-screen:image():draw(0,0)
-printCursor(px,py,pc)
+local function save(tool)
+  local ndata = tool.editor:export()
+  fs.write(tar,ndata)
+  _systemMessage("Saved successfully",1)
+end
+
+tool:start(editor,reload,save)
