5d9fcb94e19b8da739c4610558566ba74332d508
RamiMobile <ramilego4game@gmail.com>
2017-09-27 15:10:20 +0300

    Working on the image handler

--- OS/DiskOS/System/RAM.lua
+++ OS/DiskOS/System/RAM.lua
@@ -2,13 +2,17 @@
 
 --The RAM table is created by boot.lua, It's the peripheral table.
 if not RAM then return end --Incase if the RAM peripheral is disabled.
-
 --Make the non-system functions as globals
 for k,v in pairs(RAM) do
   if k:sub(1,1) ~= "_" then
     _G[k] = v
   end
 end
+
+--Localize the bit api for better perfromance.
+local band, bor, bxor, lshift, rshift = bit.band, bit.bor, bit.bxor, bit.lshift, bit.rshift
+
+local sw,sh = screenSize()
 
 --A function to convert from kilobytes into bytes, a SyntaxSugar.
 local function KB(v) return v*1024 end
@@ -44,8 +48,118 @@ function RAM.initialize()
 end
 
 --Create a new RAM handler for an imagedata.
-function RAM.newImageHandler(width,height)
-
+function RAM.newImageHandler(img)
+  local imgline = img:width()/2 --The size of each image line in bytes.
+  local imgsize = imgline * img:height() --The size of the image in bytes.
+  local imgWidth, imgHeight = img:size()
+  
+  local changed = false --Is there any changes applied on the image ?
+  
+  local function hand(mode,startAddress,address,...)
+    local address = address - startAddress +1
+    
+    if mode == "poke" then
+      local pix = ...
+      
+      --Calculate the position of the left pixel
+      local x = address % imgline
+      local y = math.floor(address / imgline)
+      
+      --Separate the 2 pixels from each other
+      local lpix = band(pix,0xF0)
+      local rpix = band(pix,0x0F)
+      
+      --Shift the left pixel
+      lpix = rshift(lpix,4)
+      
+      --Set the pixels
+      img:setPixel(x,y,lpix)
+      img:setPixel(x,y,rpix)
+      
+    elseif mode == "peek" then
+      --Calculate the position of the left pixel
+      local x = address % imgline
+      local y = math.floor(address / imgline)
+      
+      --Get the colors of the 2 pixels
+      local lpix = img:getPixel(x,y)
+      local rpix = img:getPixel(x+1,y)
+      
+      --Shift the left pixel.
+      lpix = lshift(lpix,4)
+      
+      --Merge the 2 pixels into 1 byte.
+      local pix = bor(lpix,rpix)
+      
+      --Return the final result
+      return pix
+      
+    elseif mode == "memget" then
+      --Requires verification.
+      local length = ...
+      
+      local x = address % imgline
+      local y = math.floor(address / imgline)
+      
+      local xStart, data = x, ""
+      
+      for Y = y, imgHeight, 2 do
+        for X = xStart, imgWidth, 2 do
+          
+          local lpix = img:getPixel(X,Y)
+          local rpix = img:getPixel(X+1,Y)
+          
+          lpix = lshift(lpix,4)
+          
+          local pix = bor(lpix,rpix)
+          local char = string.char(pix)
+          
+          data = data .. char
+          
+          length = length -1
+          
+          if length == 0 then
+            return data
+          end
+          
+        end
+        xStart = 0 --Trick
+      end
+      
+      return data
+      
+    elseif mode == "memset" then
+      --Requires verification.
+      local data = ...
+      local length = data:len()
+      
+      local x = address % imgline - 2
+      local y = math.floor(address / imgline) - 1
+      
+      local iter = string.gmatch(data,".")
+      
+      for i=1,length do
+        
+        x = (x+2) % imgWidth
+        y = y+1
+        
+        local char = iter()
+        local pix = string.byte(char)
+        
+        local lpix = band(pix,0xF0)
+        local rpix = band(pix,0x0F)
+        
+        lpix = rshift(lpix,4)
+        
+        img:setPixel(x,y,lpix)
+        img:setPixel(x+1,y,rpix)
+        
+      end
+      
+    elseif mode == "memcpy" then
+      
+    end
+  end
 end
 
 return RAM
