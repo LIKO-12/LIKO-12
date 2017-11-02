d5eb1a942279112d967ebdf1b77c1e8e090ae9ea
RamiMobile <ramilego4game@gmail.com>
2017-11-02 07:12:56 +0200

    Moved the VRAM handler to a seperate file

--- Peripherals/GPU/init.lua
+++ Peripherals/GPU/init.lua
@@ -385,13 +385,11 @@ return function(config) --A function that creates a new GPU peripheral.
   local indirect = { --The functions that must be called via coroutine.yield
     "flip"
   }
+  
+  local newImageHandler = love.filesystem.load(perpath.."imageHandler.lua")
 
   local VRAMBound = false
   local VRAMImg
-  local VRAMLine = _LIKO_W/2
-  local VRAMLine4 = _LIKO_W
-  local VRAMSize = VRAMLine*_LIKO_H
-  local VRAMSize4 = VRAMLine4*_LIKO_H
   
   local function BindVRAM()
     if VRAMBound then return end
@@ -418,195 +416,12 @@ return function(config) --A function that creates a new GPU peripheral.
     end
   end
   
-  local function AddressPos(address)
-    local x = address % VRAMLine
-    local y = math.floor(address / VRAMLine)
-    return x*2, y
-  end
-  
-  local function VRAMHandler(mode,startAddress,address,...)
-    address = address - startAddress
-    BindVRAM() --Make sure that the VRAM is bound.
-    if mode == "poke" then
-      
-      local pix = ...
-      
-      --Calculate the position of the left pixel
-      local x = (address % VRAMLine) * 2
-      local y = math.floor(address / VRAMLine)
-      
-      --Separate the 2 pixels from each other
-      local lpix = band(pix,0xF0)
-      local rpix = band(pix,0x0F)
-      
-      --Shift the left pixel
-      lpix = rshift(lpix,4)
-      
-      --Set the pixels
-      VRAMImg:setPixel(x,y,lpix,0,0,255)
-      VRAMImg:setPixel(x+1,y,rpix,0,0,255)
-      
-      --Update ShouldDraw Flag
-      _ShouldDraw = true
-      
-    elseif mode == "poke4" then
-      
-      local pix = ...
-      
-      --Calculate the position of the left pixel
-      local x = address % VRAMLine4
-      local y = math.floor(address / VRAMLine4)
-      
-      --Set the pixels
-      VRAMImg:setPixel(x,y,pix,0,0,255)
-      
-      --Update Should Draw Flag
-      _ShouldDraw = true
-      
-    elseif mode == "peek" then
-      
-      --Calculate the position of the left pixel
-      local x = (address % VRAMLine) * 2
-      local y = math.floor(address / VRAMLine)
-      
-      --Get the colors of the 2 pixels
-      local lpix = VRAMImg:getPixel(x,y)
-      local rpix = VRAMImg:getPixel(x+1,y)
-      
-      --Shift the left pixel.
-      lpix = lshift(lpix,4)
-      
-      --Merge the 2 pixels into 1 byte.
-      local pix = bor(lpix,rpix)
-      
-      --Return the final result
-      return pix
-      
-    elseif mode == "peek4" then
-      
-      --Calculate the position of the left pixel
-      local x = address % VRAMLine4
-      local y = math.floor(address / VRAMLine4)
-      
-      --Return the pixel color
-      local pix = VRAMImg:getPixel(x,y)
-      return pix
-      
-    elseif mode == "memget" then
-      
-      local length = ...
-      
-      local x = (address % VRAMLine) * 2
-      local y = math.floor(address / VRAMLine)
-      
-      local xStart, data = x, ""
-      
-      for Y = y, imgHeight-1 do
-        for X = xStart, imgWidth-1, 2 do
-          
-          local lpix = VRAMImg:getPixel(X,Y)
-          local rpix = VRAMImg:getPixel(X+1,Y)
-          
-          lpix = lshift(lpix,4)
-          
-          local pix = bor(lpix,rpix)
-          local char = string.char(pix)
-          
-          data = data .. char
-          
-          length = length -1
-          
-          if length == 0 then
-            return data
-          end
-          
-        end
-        xStart = 0 --Trick
-      end
-      
-      return data
-      
-    elseif mode == "memset" then
-      
-      local data = ...
-      local length = data:len()
-      
-      local x = (address % VRAMLine) * 2
-      local y = math.floor(address / VRAMLine)
-      
-      local iter = string.gmatch(data,".")
-      
-      for i=1,length do
-        
-        local char = iter()
-        local pix = string.byte(char)
-        
-        local lpix = band(pix,0xF0)
-        local rpix = band(pix,0x0F)
-        
-        lpix = rshift(lpix,4)
-        
-        VRAMImg:setPixel(x,y,lpix,0,0,255)
-        VRAMImg:setPixel(x+1,y,rpix,0,0,255)
-        
-        x = x+2
-        
-        if x >= _LIKO_W then
-          x = x - _LIKO_W
-          y = y+1
-        end
-        
-      end
-      
-      --Update Should Draw Flag
-      _ShouldDraw = true
-      
-    elseif mode == "memcpy" then
-      
-      local toAddress, length = ...
-      
-      local addressEnd = address+length-1
-      local toAddressEnd = toAddress+length-1
-      
-      for line0=0,VRAMSize,VRAMLine do
-        local line0End = line0+VRAMLine-1
-        
-        if addressEnd >= line0 and address <= line0End then
-          local sa1 = (address < line0) and line0 or address
-          local ea1 = (addressEnd > line0End) and line0End or addressEnd
-          
-          local toAddress = toAddress + (sa1 - address)
-          local toAddressEnd = toAddressEnd + (ea1 - addressEnd)
-          
-          for line1=0,VRAMSize,VRAMLine do
-            local line1End = line1+VRAMLine-1
-            
-            if toAddressEnd >= line1 and toAddress <= line1End then
-              local sa2 = (toAddress < line1) and line1 or toAddress
-              local ea2 = (toAddressEnd > line1End) and line1End or toAddressEnd
-              
-              local address = address + (sa2 - toAddress)
-              local addressEnd = addressEnd + (ea2 - toAddressEnd)
-              
-              local len = addressEnd - address + 1
-              
-              local fromX = (address % VRAMLine) * 2
-              local fromY = math.floor(address / VRAMLine)
-              
-              local toX = (sa2 % VRAMLine) * 2
-              local toY = math.floor(sa2 / VRAMLine)
-              
-              VRAMImg:paste(VRAMImg,toX,toY,fromX,fromY,len*2,1)
-            end
-          end
-        end
-      end
-      
-      --Update Should Draw Flag
-      _ShouldDraw = true
-      
-    end
-  end
+  local VRAMHandler; VRAMHandler = newImageHandler(_LIKO_W,_LIKO_H,function()
+    _ShouldDraw = true
+  end,function()
+    BindVRAM()
+    VRAMHandler("setImage",0,0,VRAMImg)
+  end)
   
   --The api starts here--
   
