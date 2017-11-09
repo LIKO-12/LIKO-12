fb6046281022d87d1217de485bb350abd049013f
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-11-09 07:05:36 +0200

    Upgraded the write mode to use a header, TODO: Upgrade the read mode

--- OS/DiskOS/Programs/disk.lua
+++ OS/DiskOS/Programs/disk.lua
@@ -17,6 +17,8 @@ local term = require("terminal")
 local source = term.resolve(args[2])
 local destination = term.resolve(args[3])
 
+local diskheader = "LK12;FileDisk;DiskOS;" --The header of each file disk.
+
 if not fs.exists(source) then print("Source doesn't exist !") return end
 if fs.exists(destination) then print("Destination already exists !") return end
 if fs.isDirectory(source) then print("Source can't be a directory !") return end
@@ -24,17 +26,23 @@ if fs.isDirectory(source) then print("Source can't be a directory !") return end
 source = fs.read(source)
 
 if mode == "read" then
+  
   FDD.importDisk(source)
   local fdata = {}
   for i=0,64*1024-1 do table.insert(fdata,string.char(peek(RamUtils.FRAM+i))) end
   fdata = table.concat(fdata)
   fs.write(destination,fdata)
   color(11) print("Read disk successfully")
+  
 elseif mode == "write" then
-  if source:len() > 64*1024 then print("File too big (Should be 64kb or less) !") return end
-  memset(0x6000,string.rep("\n",64*1024))
-  memset(0x6000,source)
+  
+  if source:len() > 64*1024-(diskheader:len()+4) then print("File too big (Should be almost 64kb or less) !") return end
+  memset(0x6000,string.rep(RamUtils.Null,64*1024))
+  memset(0x6000,diskheader) --Set the disk header
+  memset(0x6000+diskheader:len(), RamUtils.numToBin(source:len(),4)) --Set the file size
+  memset(0x6000+diskheader:len()+4,source) --Set the file data
   local data = FDD.exportDisk()
   fs.write(destination,data)
   color(11) print("Wrote disk successfully")
+  
 end
