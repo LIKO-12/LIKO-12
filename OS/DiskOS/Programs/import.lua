33a5b3f1a2720dc9dfd740f3e303cf99f5eee44b
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-12-13 13:13:18 +0200

    Added @label support

--- OS/DiskOS/Programs/import.lua
+++ OS/DiskOS/Programs/import.lua
@@ -11,23 +11,32 @@ local sw, sh = screenSize()
 if not source or source == "-?" then
   printUsage(
     "import <image>","Imports an image (gif, jpg, png)",
-    "import <image> <destination>","Converts an image to a .lk12 image file"
+    "import <image> <destination>","Converts an image to a .lk12 image file",
+    "import @label","Import the label image",
+    "import @label <destination>","Converts the label image to a .lk12 image file"
   )
   return
 end
 
-source = term.resolve(source)
+if source ~= "@label" then
+  source = term.resolve(source)
+end
+
 if destination then destination = term.resolve(destination) end
 
-if not fs.exists(source) then color(8) print("Source doesn't exists") return end
-if fs.isDirectory(source) then color(8) print("Source can't be a directory") return end
+if source ~= "@label" then
+  if not fs.exists(source) then color(8) print("Source doesn't exists") return end
+  if fs.isDirectory(source) then color(8) print("Source can't be a directory") return end
+end
+
 if destination then if fs.exists(destination) and fs.isDirectory(destination) then color(8) print("Destination must not be a directory") return end end
+
 if destination then --Convert mode
-  local imgd = imagedata(fs.read(source))
+  local imgd = (source == "@label") and getLabelImage() or imagedata(fs.read(source))
   fs.write(destination,imgd:encode())
   color(11) print("Converted Successfully")
 else --Import to disk
-  local imgd = imagedata(fs.read(source))
+  local imgd = (source == "@label") and getLabelImage() or imagedata(fs.read(source))
   local image = imagedata(sw,sh)
   image:paste(imgd)
   image = image:encode()
