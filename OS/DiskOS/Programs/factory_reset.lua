c4ceff79edcf578ab03f3f8eb564a03ed0bb07ff
Rami Sabbagh <ramilego4game@gmail.com>
2017-12-14 18:16:34 +0200

    Added `-wipe` argument, and patched to use imagedata instead of VRAM

--- OS/DiskOS/Programs/factory_reset.lua
+++ OS/DiskOS/Programs/factory_reset.lua
@@ -1,14 +1,30 @@
 if select(1,...) == "-?" then
   printUsage(
-    "factory_reset","Reflashes DiskOS and Reboots"
+    "factory_reset","Reflashes DiskOS and Reboots",
+    "factory_reset -wipe","Wipes the C drive, reflashes DiskOS and reboots"
   )
   return
 end
 
 fs.remove("C:/boot.lua")
-for addr=0x0, 0x14FFF do
-  poke(addr,0x82)
+
+if select(1,...) == "-wipe" then
+  local term = require("C:/terminal.lua")
+  term.execute("rm","C:/")
+  
+  clear(0)
+else
+  local img = imagedata(screenSize())
+  img:map(function(x,y,c)
+    if x%2 == 0 then
+      return 8
+    else
+      return 2
+    end
+  end)
+  img:image():draw(0,0)
 end
+
 flip()
 sleep(1)
 reboot()
