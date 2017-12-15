95b622541089e392ac37e2b63d4b2be0fabb1c2f
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-12-15 17:18:26 +0200

    Added a function to play a random sound

--- Peripherals/Audio/init.lua
+++ Peripherals/Audio/init.lua
@@ -2,11 +2,21 @@ local perpath = select(1,...) --The path to the FDD folder
 
 return function(config)
   
-  local sfxr = love.filesystem.load(perpath.."sfxr.lua")
+  local sfxr = love.filesystem.load(perpath.."sfxr.lua")()
   
   local au, devkit, indirect = {}, {}, {}
   
-  
+  function au.playRandom()
+    
+    local sound = sfxr.newSound()
+    sound:randomize()
+    local sounddata = sound:generateSoundData()
+    local source = love.audio.newSource(sounddata)
+    source:play()
+    
+    return true
+    
+  end
   
   return au, devkit, indirect
   
