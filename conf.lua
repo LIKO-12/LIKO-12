4302460095f04a69bf0b7f6764c479edab939aba
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-12-15 17:19:22 +0200

    Enabled the audio modules, YES I DID IT !!
    Former-commit-id: 312fee52f417aadecbba0ba778dbee2665a92af2

--- conf.lua
+++ conf.lua
@@ -16,7 +16,7 @@ function love.conf(t)
     
     t.window = false --The window will be created later by the GPU Peripheral.
  
-    t.modules.audio = false             -- Disable the audio module (boolean)
+    t.modules.audio = true              -- Enable the audio module (boolean)
     t.modules.event = true              -- Enable the event module (boolean)
     t.modules.graphics = true           -- Enable the graphics module (boolean)
     t.modules.image = true              -- Enable the image module (boolean)
@@ -25,7 +25,7 @@ function love.conf(t)
     t.modules.math = true               -- Enable the math module (boolean)
     t.modules.mouse = true              -- Enable the mouse module (boolean)
     t.modules.physics = false           -- Disable the physics module (boolean)
-    t.modules.sound = false             -- Disable the sound module (boolean)
+    t.modules.sound = true              -- Enable the sound module (boolean)
     t.modules.system = true             -- Enable the system module (boolean)
     t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
     t.modules.touch = true              -- Enable the touch module (boolean)
