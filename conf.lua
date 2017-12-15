e8461c231b0e55cf4aed4f1cdf988b40fcaf02fa
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2017-12-15 16:49:01 +0200

    Version PRE-08

--- conf.lua
+++ conf.lua
@@ -1,10 +1,10 @@
 --Nothing should be changed with this config expect: window size, enabling a module.
-_LVERSION = "V0.6.0_PRE_07"
+_LVERSION = "V0.6.0_DEV_08"
 _LVer = {
 	magor = 0,
 	minor = 6,
 	patch = 0,
-	build = "7_PRE", --"8_DEV"
+	build = "8_DEV", --"8_DEV"
 }
 function love.conf(t)
     t.identity = "liko12"               -- The name of the save directory (string)
