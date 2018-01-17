06a7aa06121e587cdcce8bc8f8a39ee47aed9370
RamiLego4Game <ramilego4game@gmail.com>
2018-01-17 22:17:51 +0200

    Rewrite BIOS
    Former-commit-id: 3d34f49c9e3c3781eaf47c76f0484828ae5a6ece

---
+++ BIOS/oldinit.lua
@@ -0,0 +1,309 @@
+--The BIOS should control the system of LIKO-12 and load the peripherals--
+--For now it's just a simple BIOS to get LIKO-12 working.
+local DevMode = love.filesystem.exists("devmode.txt")
+
+local FThread --File tracking thread
+
+local _LIKO_Version, _LIKO_Old = _LVer.magor..".".._LVer.minor..".".._LVer.patch..".".._LVer.build
+if love.filesystem.exists(".version") then
+  _LIKO_Old = love.filesystem.read(".version")
+  if _LIKO_Old == _LIKO_Version then
+    _LIKO_Old = false
+  end
+else
+  love.filesystem.write(".version",tostring(_LIKO_Version))
+end
+
+--Require the engine libraries--
+local events = require("Engine.events")
+local coreg = require("Engine.coreg")
+
+local function splitFilePath(path) return path:match("(.-)([^\\/]-%.?([^%.\\/]*))$") end --A function to split path to path, name, extension.
+
+local Peripherals = {} --The loaded peripherals.
+local MPer = {} --Mounted and initialized peripherals.
+local BPer = {} --Peripherals ready for use IN the BIOS only.
+local Devkits = {} --Peripherals Devkits.
+local InDirect = {} --Peripherals indirect list.
+local DirectAPI = {} --The api functions that has to be called directly without a coroutine yield.
+
+--A function to load the peripherals.
+local function indexPeripherals(path)
+  local files = love.filesystem.getDirectoryItems(path)
+  for k,filename in ipairs(files) do
+    if love.filesystem.isDirectory(path..filename) then
+      if love.filesystem.exists(path..filename.."/init.lua") then
+        local chunk, err = love.filesystem.load(path..filename.."/init.lua")
+        if not chunk then Peripherals[filename] = "Err: "..tostring(err) else
+        Peripherals[filename] = chunk(path..filename.."/") end
+      end
+    else
+      local p, n, e = splitFilePath(path..filename)
+      if e == "lua" then
+        local chunk, err = love.filesystem.load(path..n)
+        if not chunk then Peripherals[n:sub(0,-5)] = "Err: "..tostring(err) else
+        Peripherals[n:sub(0,-5)] = chunk(path) end
+      end
+    end
+  end
+end
+
+indexPeripherals("/Peripherals/") --Index and Load the peripherals
+
+--Peripheral, Err = P(PeriheralName, MountedName, ConfigTabel)
+local function P(per,m,conf)
+  if not per then return false, "Should provide peripheral name" end
+  if type(per) ~= "string" then return false, "Peripheral name should be a string, provided "..type(per) end
+  if not Peripherals[per] then return false, "'"..per.."' Peripheral doesn't exists" end
+  if type(Peripherals[per]) == "string" then return false, "Compile "..Peripherals[per] end
+  
+  local m = m or per
+  if type(m) ~= "string" then return false, "Mounting name should be a string, provided "..type(m) end
+  if MPer[m] then return false, "Mounting name '"..m.."' is already taken" end
+  
+  local conf = conf or {}
+  if type(conf) ~= "table" then return false, "Configuration table should be a table, provided "..type(conf) end
+  
+  events:group(per..":"..m)
+  local success, peripheral, devkit, indirect = pcall(Peripherals[per],conf)
+  events:group()
+  
+  if success then
+    MPer[m] = peripheral
+    Devkits[m] = devkit
+    InDirect[m] = {}
+    for k,v in ipairs(indirect or {}) do
+      InDirect[m][v] = true
+    end
+    coreg:register(peripheral,m)
+  else
+    peripheral = "Init Err: "..tostring(peripheral)
+  end
+  return success, peripheral, devkit, indirect
+end
+
+if not love.filesystem.exists("/bconf.lua") or DevMode or true then
+  love.filesystem.write("/bconf.lua",love.filesystem.read("/BIOS/bconf.lua"))
+end
+
+local passert = function(ok, per, devkit, indirect) --Peripheral assert
+  if not ok then return error(per) end
+  return per, devkit, indirect
+end
+
+local confSandbox = {P = P,error=error,assert=passert,_OS=love.system.getOS()}
+
+local ok, bchunk, err = pcall(love.filesystem.load,"/bconf.lua")
+--if not bconfC then bconfC, bconfDErr = love.filesystem.load("/BIOS/bconf.lua") end --Load the default BConfig
+if not ok then error(bchunk) end
+if not bchunk then error(err) end
+setfenv(bchunk,confSandbox) --BConfig sandboxing
+local success, run_err = pcall(bchunk)
+if not success then error(run_err)
+  local ok, default_bchunk, err = pcall(love.filesystem.load,"/BIOS/bconf.lua")
+  if not ok then error(default_bchunk) end
+  if not default_bchunk then error(err) end
+  setfenv(default_bchunk,confSandbox) --BConfig sandboxing
+  default_bchunk()
+end --Load the default BConfig
+
+DirectAPI = {} --Convert it to a table.
+for per, funcs in pairs(MPer) do
+  DirectAPI[per] = {}
+  for fname, func in pairs(funcs) do
+    if not InDirect[per][fname] then --Cache it
+      DirectAPI[per][fname] = function(...)
+        local result = {func(...)}
+        if result[1] then --It ran successfully
+          local nres = {}
+          for k,v in ipairs(result) do
+            nres[k-1] = v
+          end
+          return unpack(nres)
+        else --Error
+          return error(result[2] or "Unknown")
+        end
+      end
+    end
+  end
+end
+
+--BIOS Api for use in the OS
+coreg:register(function()
+  local list = {}
+  for per, funcs in pairs(MPer) do
+    list[per] = {}
+    for name, func in pairs(funcs) do
+      table.insert(list[per],name)
+    end
+  end
+  return true, list
+end,"BIOS:listPeripherals")
+
+coreg:register(function()
+  return true, DirectAPI
+end,"BIOS:DirectAPI")
+
+coreg:register(function()
+  return true, _LIKO_Version, _LIKO_Old
+end,"BIOS:GetVersion")
+
+local function exe(ok,err,...) --Execute a LIKO12 api function (to handle errors)
+  if ok then
+    return err,...
+  else
+    return error(err)
+  end
+end
+
+--Setup Peripherals for use in the BIOS
+for name, per in pairs(MPer) do
+  if type(per) == "table" then
+    BPer[name] = {}
+    for fname, func in pairs(per) do
+      local f = func
+      BPer[name][fname] = function(...) return exe(f(...)) end
+    end
+  end
+end
+
+--Setup shortcuts for use.
+local fs = BPer.HDD
+local gpu = BPer.GPU
+local cpu = BPer.CPU
+local ram = BPer.RAM
+local ramkit = Devkits.RAM
+local keyboard = BPer.Keyboard
+local fdd = BPer.FDD
+
+if not fs then error("No HDD peripheral to boot from !") end
+
+--Installs a specific OS on the drive C
+local function installOS(os,path)
+  local path = path or "/"
+  fs.drive("C") --Opereating systems are installed on C drive
+  local files = love.filesystem.getDirectoryItems("/OS/"..os..path)
+  for k,v in pairs(files) do
+    if love.filesystem.isDirectory("/OS/"..os..path..v) then
+      fs.newDirectory(path..v)
+      installOS(os,path..v.."/")
+    else
+      fs.write(path..v,love.filesystem.read("/OS/"..os..path..v))
+    end
+  end
+end
+
+--Called when there is no opereating system to boot from on the C drive.
+local function noOS()
+  if gpu then --Show a gui asking to install diskos.
+    installOS("DiskOS")
+  else --Install diskos directly ;)
+    installOS("DiskOS")
+  end
+end
+
+--Boots into the install opreating system, creates the coroutine, sandboxes it and start !
+local function bootOS()
+  fs.drive("C") --Switch to the C drive.
+  
+  if not fs.exists("/boot.lua") or DevMode then _LIKO_Old = false; noOS() end
+  
+  if _LIKO_Old then --Show update screen
+    noOS() --Nah just reflash
+    love.filesystem.write(".version",tostring(_LIKO_Version)) --Update the .version file
+  end
+  
+  --File track thread
+  if DevMode and love.thread then
+	FThread = love.thread.newThread("/BIOS/filethread.lua")
+	FThread:start()
+	
+	events:register("love:reboot",function()
+		local channel = love.thread.getChannel("BIOSFileThread")
+		channel:push(true) --Shutdown the thread
+	end)
+	
+	events:register("love:quit",function()
+		local channel = love.thread.getChannel("BIOSFileThread")
+		channel:push(true) --Shutdown the thread
+		FThread:wait()
+	end)
+  end
+  
+  local bootchunk, err = fs.load("/boot.lua")
+  if not bootchunk then error(err or "") end --Must be replaced with an error screen.
+  
+  local coglob = coreg:sandbox(bootchunk)
+  local co = coroutine.create(bootchunk)
+  coreg:setCoroutine(co,coglob) --For peripherals to use.
+  
+  if cpu then cpu.clearEStack() end --Remove any events made while booting.
+  
+  coreg:resumeCoroutine() --Boot the OS !
+end
+
+if gpu then
+  --Post Screen--
+  gpu.clear() --Fill with black.
+  gpu.color(7) --Set the color to white.
+  
+  --Load the bios logos.
+  local lualogo = gpu.image(love.filesystem.read("/BIOS/lualogo.lk12"))
+  local likologo = gpu.image(love.filesystem.read("/BIOS/likologo.lk12"))
+  
+  local sw, sh = gpu.screenSize()
+  
+  local stages = {0.5,0,0.3,0,0.3,0,1.5,0.2,0}
+  local timer = 0
+  local stage = 1
+  
+  events:group("BIOS:POST")
+  events:register("love:update", function(dt)
+    if stage > #stages then return end
+    
+    if stage == 2 then
+      lualogo:draw(sw-lualogo:width()-6,5)
+      likologo:draw(2,7)
+      
+      gpu.print("LIKO-12 - Fantasy Computer",15,6)
+      gpu.print("Copyright (C) Rami Sabbagh",15,13)
+      
+      gpu.printCursor(0,3,0)
+      gpu.print("NormBIOS Revision 060-012")
+      if DevMode then gpu.print("-==Devmode Enabled==-") end
+      gpu.print("")
+      
+      gpu.print("Press DEL to enter setup",2,sh-7)
+      
+    elseif stage == 4 then
+      gpu.print("Main CPU: LuaJIT 5.1")
+      if ram then gpu.print("RAM: "..(ramkit.ramsize/1024).." Kilo-Bytes ("..ramkit.ramsize.." Bytes)") end
+      gpu.print("GPU: "..sw.."x"..sh.." 4-Bit (16 Color Palette)")
+      gpu.print("")
+      gpu.print("Harddisks: ")
+    elseif stage == 6 then
+      Devkits["HDD"].calcUsage()
+      for letter,drive in pairs(Devkits["HDD"].drives) do
+        local size = math.floor((drive.size/1024) * 100)/100
+        local usage = math.floor((drive.usage/1024) * 100)/100
+        local percentage = math.floor(((usage*100)/size) * 100)/100
+        gpu.print("Drive "..letter..": "..usage.."/"..size.." KB ("..percentage.."%)")
+      end
+    elseif stage == 8 then
+      gpu.clear()
+      gpu.printCursor(0,0,0)
+    elseif stage == 9 then
+      events:unregisterGroup("BIOS:POST")
+      bootOS()
+    end
+    
+    timer = timer + dt
+    if timer > stages[stage] then
+      stage = stage + 1
+      timer = 0
+    end
+  end)
+else
+  Devkits["HDD"].calcUsage()
+  bootOS() --Incase the gpu doesn't exists (Then can't enter the bios nor do the boot animation
+end
