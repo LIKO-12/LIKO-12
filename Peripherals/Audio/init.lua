1a76cf8fcb7f0b62ea4feb60bf31c12016cbc95b
RamiLego4Game <ramilego4game@gmail.com>
2018-01-18 23:51:25 +0200

    Updated SFXR to work with QueueableSources

--- Peripherals/Audio/init.lua
+++ Peripherals/Audio/init.lua
@@ -1,4 +1,4 @@
-local perpath = select(1,...) --The path to the FDD folder
+local perpath = select(1,...) --The path to the Audio folder
 
 local json = require("Engine.JSON")
 local coreg = require("Engine.coreg")
@@ -7,12 +7,10 @@ local events = require("Engine.events")
 return function(config)
   
   local sfxr = love.filesystem.load(perpath.."sfxr.lua")()
-  local thread = love.thread.newThread(perpath.."loadthread.lua")
-  local chIn, chOut = love.thread.newChannel(), love.thread.newChannel()
+  local thread = love.thread.newThread(perpath.."sfxrthread.lua")
+  local chIn = love.thread.newChannel()
   
-  local LoadJob = false
-  
-  thread:start(perpath, chIn, chOut)
+  thread:start(chIn)
   
   events:register("love:reboot", function()
     chIn:push("stop")
@@ -23,53 +21,13 @@ return function(config)
     thread:wait()
   end)
   
-  local au, yAU, devkit = {}, {}, {}
+  local AU, yAU, devkit = {}, {}, {}
   
-  function yAU.newGenerator(p)
+  function AU.play(chn,p)
     
-    if type(p) ~= "table" then return false, "Parameters should be a table, provided: "..type(p) end
+    if type(p) ~= "table" then return error("Parameters should be a table, provided: "..type(p)) end
     
-    local params = {
-      repeatspeed = 0,
-      waveform = 0, --*
-      envelope = {
-        attack = 0,
-        sustain = 0.3,
-        punch = 0,
-        decay = 0.4
-      },
-      frequency = {
-        start = 0.3,
-        min = 0,
-        slide = 0, --
-        dslide = 0 --
-      },
-      vibrato = {
-        depth = 0,
-        speed = 0
-      },
-      change = {
-        amount = 0, --
-        speed = 0
-      },
-      duty = {
-        ratio = 0,
-        sweep = 0 --
-      },
-      phaser = {
-        offset = 0, --
-        sweep = 0
-      },
-      lowpass = {
-        cutoff = 1,
-        sweep = 0, --
-        resonance = 0
-      },
-      highpass = {
-        cutoff = 0,
-        sweep = 0 --
-      }
-    }
+    local params = {repeatspeed=0,waveform=0,envelope={attack=0,sustain=0.3,punch=0,decay=0.4},frequency={start=0.3,min=0,slide=0,dslide=0},vibrato={depth=0,speed=0},change={amount=0,speed=0},duty={ratio=0,sweep=0},phaser={offset=0,sweep=0},lowpass={cutoff=1,sweep=0,resonance=0},highpass={cutoff=0,sweep=0}}
     
     for k1,v1 in pairs(p) do
       if type(v1) == "table" and type(params[k1]) == "table" then
@@ -87,11 +45,7 @@ return function(config)
     
     local job = json:encode(params)
     
-    chIn:push(job)
-    
-    LoadJob = true
-    
-    return 2
+    chIn:push({channel=chn or 1,params=job})
     
   end
   
@@ -102,152 +56,8 @@ return function(config)
       error("Thread: "..terr)
     end
     
-    if LoadJob then
-      
-      local sounddata = chOut:pop()
-      if sounddata then
-        
-        LoadJob = false
-        
-        coreg:resumeCoroutine(true, function()
-          
-          local source = love.audio.newSource(sounddata)
-          
-          return function()
-            source:play()
-          end
-          
-        end)
-        
-      end
-    end
-    
   end)
   
-  function au.newSound()
-    
-    local s = {} --The sound object.
-    local sounddata
-    
-    local params = {
-      repeatspeed = 0,
-      waveform = 0, --*
-      envelope = {
-        attack = 0,
-        sustain = 0.3,
-        punch = 0,
-        decay = 0.4
-      },
-      frequency = {
-        start = 0.3,
-        min = 0,
-        slide = 0, --
-        dslide = 0 --
-      },
-      vibrato = {
-        depth = 0,
-        speed = 0
-      },
-      change = {
-        amount = 0, --
-        speed = 0
-      },
-      duty = {
-        ratio = 0,
-        sweep = 0 --
-      },
-      phaser = {
-        offset = 0, --
-        sweep = 0
-      },
-      lowpass = {
-        cutoff = 1,
-        sweep = 0, --
-        resonance = 0
-      },
-      highpass = {
-        cutoff = 0,
-        sweep = 0 --
-      }
-    }
-    
-    function s:get(field)
-      if type(field) ~= "string" then return error("Field should be a string, provided: "..type(field)) end
-      
-      local i, sub = field:match("(.+)%.(.+)")
-      if i then
-        if type(params[i]) ~= "table" then
-          return error("Field doesn't exists.")
-        else
-          if type(params[i][sub]) ~= "number" then
-            return error("Field doesn't exists")
-          else
-            return params[i][sub]
-          end
-        end
-      else
-        if type(params[field]) ~= "number" then
-          return error("Field doesn't exists.")
-        end
-        
-        return params[field]
-      end
-    end
-    
-    function s:set(field,value)
-      if type(field) ~= "string" then return error("Field should be a string, provided: "..type(field)) end
-      if type(value) ~= "number" then return error("Value should be a number, provided: "..type(value)) end
-      
-      local i, sub = field:match("(.+)%.(.+)")
-      if i then
-        if type(params[i]) ~= "table" then
-          return error("Field doesn't exists.")
-        else
-          if type(params[i][sub]) ~= "number" then
-            return error("Field doesn't exists")
-          end
-          
-          if params[i][sub] ~= value then
-            sounddata = false
-          end
-          params[i][sub] = value
-        end
-      else
-        if type(params[field]) ~= "number" then
-          return error("Field doesn't exists.")
-        end
-        
-        if params[field] ~= value then
-          sounddata = nil
-        end
-        params[field] = value
-      end
-    end
-    
-    function s:generate()
-      if sounddata then return end
-      
-    end
-    
-    function s:play()
-      self:generate()
-      
-      local source = love.audio.newSource(sounddata)
-      source:play()
-    end
-    
-  end
-  
-  function au.playRandom()
-    
-    local sound = sfxr.newSound()
-    sound:randomize()
-    local sounddata = sound:generateSoundData()
-    local source = love.audio.newSource(sounddata)
-    source:play()
-    
-  end
-  
-  return au, yAU, devkit
+  return AU, yAU, devkit
   
 end
