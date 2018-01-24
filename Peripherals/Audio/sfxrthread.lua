1589d1626ae7d369ed24d723ec44d31a0baf0b96
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2018-01-24 07:07:42 +0200

    Now operates on 22050 sampling rate
    Former-commit-id: 5600fd9001aa0f61b6e926c56c2bf283d5df3fff

--- Peripherals/Audio/sfxrthread.lua
+++ Peripherals/Audio/sfxrthread.lua
@@ -3,6 +3,7 @@ local chIn = ...
 require("love.audio")
 require("love.sound")
 require("love.timer")
+require("love.system")
 
 local json = require("Engine.JSON")
 
@@ -22,8 +23,12 @@ local sfx2_count = false
 local sfx1_iter = false
 local sfx2_iter = false
 
-local buffer_size = 44100/4
-local buffer_time = 0.25
+local onMobile = (love.system.getOS() == "Android")
+
+local rate = onMobile and 22050 or 44100
+
+local buffer_size = rate/4
+local buffer_time = buffer_size/rate
 
 local function popJob()
   if sfx1_count or sfx2_count then
@@ -34,6 +39,8 @@ local function popJob()
     return arg
   end
 end
+
+love.timer.step()
 
 while true do
   for newJob in popJob do
@@ -51,8 +58,8 @@ while true do
       end
       
       qs1:clear()
-      sfx1_count = sfx1:getEnvelopeLimit()
-      sfx1_iter = sfx1:generate()
+      sfx1_count = sfx1:getEnvelopeLimit(rate)
+      sfx1_iter = sfx1:generate(rate)
     elseif newJob.channel == 2 then
       local params = json:decode(newJob.params)
       for k1,v1 in pairs(params) do
@@ -66,8 +73,8 @@ while true do
       end
       
       qs2:clear()
-      sfx2_count = sfx2:getEnvelopeLimit()
-      sfx2_iter = sfx2:generate()
+      sfx2_count = sfx2:getEnvelopeLimit(rate)
+      sfx2_iter = sfx2:generate(rate)
     end
   end
   
@@ -86,9 +93,10 @@ while true do
         sfx1_count = sfx1_count - buffer_size
       end
       
-      local buffer = love.sound.newSoundData(buffer_size, 44100, 16, 1)
+      local buffer = love.sound.newSoundData(buffer_size, rate, 16, 1)
+      local setSample = buffer.setSample
       for i=0, buffer_size-1 do
-        buffer:setSample(i,sfx1_iter())
+        setSample(buffer,i,sfx1_iter())
       end
       
       qs1:queue(buffer)
@@ -111,9 +119,10 @@ while true do
         sfx2_count = sfx2_count - buffer_size
       end
       
-      local buffer = love.sound.newSoundData(buffer_size, 44100, 16, 1)
+      local buffer = love.sound.newSoundData(buffer_size, rate, 16, 1)
+      local setSample = buffer.setSample
       for i=0, buffer_size-1 do
-        buffer:setSample(i,sfx2_iter())
+        setSample(buffer,i,sfx2_iter())
       end
       
       qs2:queue(buffer)
@@ -123,7 +132,8 @@ while true do
   
   love.timer.step()
   local dt = love.timer.getDelta()
-  local st = (buffer_time - dt)*0.9
+  local active = (sfx1_count and 1 or 0) + (sfx2_count and 1 or 0)
+  local st = (buffer_time - dt*2*active)*0.9
   
   if st > 0.01 then
     love.timer.sleep(st)
