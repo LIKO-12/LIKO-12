3c41595af3725d6f48edccadda9d37dda8d2129e
matimati433 <matimati433@users.noreply.github.com>
2018-02-09 21:02:21 +0100

    Add an BIOS setup menu
    Former-commit-id: 35bbf5a94575cb5fcada922aee519157ef9a06e5

--- BIOS/setup.lua
+++ BIOS/setup.lua
@@ -12,8 +12,20 @@ GPU.clear(0)
 GPU.color(7)
 GPU.printCursor(0,0,0)
 
-GPU.print("COMMING SOON")
-
-CPU.sleep(2)
-
+GPU.print("Setup")
+GPU.print("Press F1 to reflash DiskOS")
+GPU.print("Press R to reboot")
+while true do
+    for event, a, b, c, d, e, f in CPU.pullEvent do
+        if event == "keypressed" and a == "f1" and c == false then
+           GPU.print("Flashing in 5 seconds...")
+           CPU.sleep(5)
+           love.filesystem.load("BIOS/installer.lua")(Handled,"DiskOS",false)
+           CPU.reboot()
+        end
+        if event == "keypressed" and a == "r" and c == false then
+            CPU.reboot()
+        end
+    end
+end
 CPU.reboot()
