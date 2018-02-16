3de9420d8e898b088da117cfad013075bb9323f1
RamiLego4Game <ramilego4game@gmail.com>
2018-02-16 09:53:29 +0200

    Complete the sfx object

--- OS/DiskOS/Libraries/sfx.lua
+++ OS/DiskOS/Libraries/sfx.lua
@@ -11,9 +11,32 @@ local function newSFX(notes,speed)
   --Initialize the sfx data.
   for i=1,sfx.notes*4, 4 do
     sfx[i] = sfx.notetime
-    sfx[i+1] = -1
+    sfx[i+1] = 0
     sfx[i+2] = 0
     sfx[i+3] = 1
+  end
+  
+  function sfx:setNote(id,note,oct,waveform,volume)
+    if note and oct then
+      local freq = AudioUtils.noteFrequency(note,oct)
+      self[id*4 + 3] = freq
+    end
+    
+    if waveform then
+      self[id*4 + 2] = waveform
+    end
+    
+    if volume then
+      self[id*4 + 4] = math.min(math.abs(volume), 1)
+    end
+    
+    return self
+  end
+  
+  function sfx:getNote(id)
+    local note,oct = AudioUtils.frequencyNote(self[id*4 + 3])
+    
+    return note,oct,self[id*4 + 2],self[id*4 + 4]
   end
   
   function sfx:_updateDataTime()
@@ -26,6 +49,8 @@ local function newSFX(notes,speed)
     self.speed = speed
     self.notetime = self.speed/self.notes
     self:_updateDataTime()
+    
+    return self
   end
   
   function sfx:getSpeed()
@@ -36,11 +61,21 @@ local function newSFX(notes,speed)
     self.notetime = time
     self.speed = self.notetime*self.notes
     self:_updateDataTime()
+    
+    return self
   end
   
   function sfx:getNoteTime()
-    
+    return self.notetime
   end
+  
+  function sfx:play(chn)
+    Audio.play(self,chn or 0)
+    
+    return self
+  end
+  
+  return sfx
   
 end
 
