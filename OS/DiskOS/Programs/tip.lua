d5af15164416d8189b75b448df7fc0ba230be388
boredom101 <ymnewmark@gmail.com>
2018-03-04 00:40:33 -0500

    Moved to tips to new file.
    The tips are now in thier own file, simplifying the process of adding new tips
    
    
    Former-commit-id: 096f6330538295c0a8db19a2cb31e5799145d904

--- OS/DiskOS/Programs/tip.lua
+++ OS/DiskOS/Programs/tip.lua
@@ -1,18 +1,14 @@
 --A group of tips to be displayed in the terminal as "Tip of the day:"
 
-local tips = {
-  "You can view the system files by switching to drive C 'cd C:'",
-  "You can factory_reset DiskOS by typing 'factory_reset'",
-  "You can update old LIKO-12 games (older than V0.0.5A) by using the 'loadcart' command",
-  "You can play piano notes by using the 'play' program",
-  "You can check the drives usage by typing 'drives'",
-  "You can check the available API functions by typing 'apis'",
-  "You can store your own programs in D:/Programs/",
-  "You can always look in other people's code to see how to use a feature",
-  "There are included libraries for things ranging from collision detection to object orientation",
-  "If you want code to run on boot up, place it in autoexec.lua in the D: drive.",
-  "Updates overwrite the C: drive, so place your own data in the D: drive",
-}
+local function split(str)
+  local t = {}
+  for val in str:gmatch("[^\r\n]+") do
+    table.insert(t, val)
+  end
+  return t
+end
+
+local tips = split(fs.read("C:/Help/Tips"))
 
 local seed = tonumber(os.date("%Y%m%d",os.time()))
 
