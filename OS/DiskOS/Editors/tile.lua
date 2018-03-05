046c074121281f10da6c825a2d8b97066e5db883
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2018-03-05 20:43:05 +0200

    Toolbar drawing

--- OS/DiskOS/Editors/tile.lua
+++ OS/DiskOS/Editors/tile.lua
@@ -3,6 +3,8 @@ local MapObj = require("Libraries.map")
 
 local t = {} --The tilemap editor
 
+--=====-- Spritesheet Values --=====--
+
 local imgw, imgh = 8, 8 --The sprite size (MUST NOT CHANGE)
 
 local swidth, sheight = screenSize() --The screen size
@@ -12,6 +14,8 @@ local bsizeH = bankH*imgh --The height of each bank in pixels
 local sizeW, sizeH = sheetW*imgw, sheetH*imgh --The size of the spritessheet in pixels
 
 local SpriteMap --We be recieved later in t:entered()
+
+--========-- Map Values --========--
 
 local MapW, MapH = math.floor(swidth*0.75), sheight --The size of the map in cells.
 
@@ -28,26 +32,64 @@ local mapdx, mapdy = 0,0 --Map drawing offsets.
 local bgsprite = eapi.editorsheet:extract(59):image() --The background image sprite.
 local bgquad = bgsprite:quad(0,0,MapVPW,MapVPH) --The quad of the background image.
 
+--=======-- GUI VARIABLES --=======--
+
+local selectedTool = 1
+
+local hotbarTiles = {0,22,23,73,46,47,48,70,71,49}
+
+local selectedSlot = 4
+
+--=========-- Functions --=========--
+
 function t:entered()
   SpriteMap = eapi.leditors[eapi.editors.sprite].SpriteMap
-  eapi:drawUI()
+  eapi:drawTopBar()
   self:redraw()
 end
 
 function t:redraw()
   self:drawMap()
+  self:drawToolbar()
 end
 
 function t:drawToolbar()
+  --Draw the background
+  rect(swidth-9,8, 8,sheight-8, false, 0)
   
+  --Draw the hotbar tiles
+  for i=0,9 do
+    local sprid = hotbarTiles[i+1]
+    
+    if sprid == 0 then
+      eapi.editorsheet:draw(120, swidth-8,8+i*8)
+    else
+      SpriteMap:draw(sprid, swidth-8,8+i*8)
+    end
+  end
+  
+  --Draw hotbar selection box
+  rect(swidth-9,8*selectedSlot-1, 10,10, true, 1)
+  rect(swidth-11,8*selectedSlot-3, 12,14, true, 1)
+  rect(swidth-10,8*selectedSlot-2, 11,12, true, 7)
+  
+  --Draw the tools
+  rect(swidth-9,sheight-5*8,8,5*8, false, 9)
+  for i=0,4 do
+    local sprid = 114+i
+    
+    if i == selectedTool then sprid = sprid + 24 end
+    
+    eapi.editorsheet:draw(sprid,swidth-8,sheight-5*8+i*8)
+  end
 end
 
 function t:drawMap()
   --Clip to map area
-  clip(0,8,swidth-8,sheight-8)
+  clip(0,8,swidth-9,sheight-8)
   
   --Draw the background.
-  rect(0,8,swidth-8,sheight-8, false,0)
+  rect(0,8,swidth-9,sheight-8, false,0)
   pal(1,2) --Change blue to red.
   bgsprite:draw(0,8, 0,1,1, bgquad)
   pal() --Reset blue to blue.
