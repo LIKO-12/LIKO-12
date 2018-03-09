43008121ba3b88d6f72ec3f8a49ae42b66f639d3
RamiLego4Game <ramilego4game@gmail.com>
2018-03-09 21:55:43 +0200

    Added genetic algorithm library

--- OS/DiskOS/System/osapi.lua
+++ OS/DiskOS/System/osapi.lua
@@ -39,7 +39,7 @@ do
   end
   
   add("dofile","printUsage","getAPI")
-  add("bump","class","lume","JSON","luann")
+  add("bump","class","lume","JSON","luann","geneticAlgo")
   add("Sprite","fget","fset","map","eventLoop","pget","pset","sget","sset","mget","mset","Controls","SFX")
   add("SpriteMap","SheetFlagsData","TileMap","MapObj","SFXS","SfxObj","btn","btnp","__BTNUpdate","__BTNKeypressed","__BTNTouchControl","_BTNGamepad")
 end
