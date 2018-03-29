777e2b9e3511bd5c1b6e7ef09e04b1efdede65fd
RamiLego4Game <ramilego4game@gmail.com>
2018-03-29 15:22:03 +0200

    Got rid of the `___autoEventLoop()` thing

--- OS/DiskOS/Runtime/Resources/00_Code.lua
+++ OS/DiskOS/Runtime/Resources/00_Code.lua
@@ -7,7 +7,6 @@ local eapi = require("Editors")
 local codeid = eapi.editors.code
 
 local luacode = eapi.leditors[codeid]:export()
-luacode = luacode .. "\n__".."_autoEventLoop()" --Because trible _ are not allowed in LIKO-12
 
 Globals._GameCode = luacode
 
