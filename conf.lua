e2551595e957eaf5b8bae2c375716ada322151a7
RamiLego4Game <ramilego4game@gmail.com>
2018-04-05 16:40:46 +0200

    Update conf.lua to 11.0

--- conf.lua
+++ conf.lua
@@ -18,16 +18,20 @@ end
 
 function love.conf(t)
     t.identity = build.Appdata or "liko12"-- The name of the save directory (string)
-    t.version = "0.10.2"                -- The LÖVE version this game was made for (string)
+    t.version = "11.0"                -- The LÖVE version this game was made for (string)
     t.console = false                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.externalstorage = true            -- True to save files (and read from the save directory) in external storage on Android (boolean) 
     t.gammacorrect = false              -- Enable gamma-correct rendering, when supported by the system (boolean)
     
+    t.audio.mixwithsystem = true        -- Keep background music playing when opening LOVE (boolean, iOS and Android only)
+    
     t.window = false --The window will be created later by the GPU Peripheral.
  
     t.modules.audio = true              -- Enable the audio module (boolean)
+    t.modules.data = true               -- Enable the data module (boolean)
     t.modules.event = true              -- Enable the event module (boolean)
+    t.modules.font = true               -- Enable the font module (boolean)
     t.modules.graphics = true           -- Enable the graphics module (boolean)
     t.modules.image = true              -- Enable the image module (boolean)
     t.modules.joystick = true           -- Enable the joystick module (boolean)
@@ -37,9 +41,9 @@ function love.conf(t)
     t.modules.physics = false           -- Disable the physics module (boolean)
     t.modules.sound = true              -- Enable the sound module (boolean)
     t.modules.system = true             -- Enable the system module (boolean)
+    t.modules.thread = true             -- Enable the thread module (boolean)
     t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
     t.modules.touch = true              -- Enable the touch module (boolean)
     t.modules.video = false             -- Disable the video module (boolean)
     t.modules.window = true             -- Enable the window module (boolean)
-    t.modules.thread = true             -- Enable the thread module (boolean)
 end
