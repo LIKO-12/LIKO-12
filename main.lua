bce756e31f3d2731910d4b0df6ea801790134e9b
RamiLego4Game <ramilego4game@gmail.com>
2018-04-05 17:11:19 +0200

    Update main.lua to 11.0

--- main.lua
+++ main.lua
@@ -39,65 +39,65 @@ function love.load(args)
 end
 
 function love.run(arg)
-  while true do
+  local function runReset()
     events = require("Engine.events")
     events:register("love:reboot",function(args) --Code can trigger this event to do a soft restart.
       reboot = args or {}
     end)
-   
+
     if love.math then
       love.math.setRandomSeed(os.time())
     end
-   
+
     if love.load then love.load(reboot or arg) end reboot = false
-   
+
     -- We don't want the first frame's dt to include time taken by love.load.
     if love.timer then love.timer.step() end
-   
+
     local dt = 0
-   
-    -- Main loop time.
-    while true do
-      -- Process events.
-      if love.event then
-        love.event.pump()
-        for name, a,b,c,d,e,f in love.event.poll() do
-          if name == "quit" then
-            local r = events:trigger("love:quit") --If any event returns true the quit will be cancelled
-            for k,v in pairs(r) do
-              if v and v[1] then r = nil break end
-            end
-            if r then return a end
-          else
-            events:trigger("love:"..name,a,b,c,d,e,f)
+  end
+
+  return function()
+    -- Process events.
+    if love.event then
+      love.event.pump()
+      for name, a,b,c,d,e,f in love.event.poll() do
+        if name == "quit" then
+          local r = events:trigger("love:quit")
+          --If any event returns true the quit will be cancelled
+          for k,v in pairs(r) do
+            if v and v[1] then r = nil break end
           end
+          if r then return a end
+        else
+          events:trigger("love:"..name,a,b,c,d,e,f)
         end
       end
-   
-      -- Update dt, as we'll be passing it to update
-      if love.timer then
-        love.timer.step()
-        dt = love.timer.getDelta()
-      end
-   
-      -- Call update and draw
-      events:trigger("love:update",dt) -- will pass 0 if love.timer is disabled
-    
-      if love.graphics and love.graphics.isActive() then
-        events:trigger("love:graphics")
-      end
-    
-      if love.timer then love.timer.sleep(0.001) end
-      
-      if reboot then
-        for k,v in pairs(package.loaded) do
-          if not package_exceptions[k] then package.loaded[k] = nil end
-        end--Reset the required packages
-        
-        love.graphics.reset() --Reset the GPU
-        events = nil --Must undefine this.
-        break --Break our game loop
-      end
+    end
+
+    -- Update dt, as we'll be passing it to update
+    if love.timer then
+      love.timer.step()
+      dt = love.timer.getDelta()
+    end
+
+    -- Call update and draw
+    events:trigger("love:update",dt) -- will pass 0 if love.timer is disabled
+
+    if love.graphics and love.graphics.isActive() then
+      events:trigger("love:graphics")
+    end
+
+    if love.timer then love.timer.sleep(0.001) end
+
+    if reboot then
+      for k,v in pairs(package.loaded) do
+        if not package_exceptions[k] then package.loaded[k] = nil end
+      end--Reset the required packages
+
+      love.graphics.reset() --Reset the GPU
+      events = nil --Must undefine this.
+      runReset() --Reset
     end
   end
 end
