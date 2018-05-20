d02891386a688fd65607f5b35de761a623ac64ab
RamiLego4Game <ramilego4game@gmail.com>
2018-05-20 15:52:23 +0300

    Tiny change

--- conf.lua
+++ conf.lua
@@ -18,7 +18,7 @@ end
 
 function love.conf(t)
     t.identity = build.Appdata or "liko12"-- The name of the save directory (string)
-    t.version = "11.1"                -- The LÖVE version this game was made for (string)
+    t.version = "11.1"                  -- The LÖVE version this game was made for (string)
     t.console = false                   -- Attach a console (boolean, Windows only)
     t.accelerometerjoystick = false     -- Enable the accelerometer on iOS and Android by exposing it as a Joystick (boolean)
     t.externalstorage = true            -- True to save files (and read from the save directory) in external storage on Android (boolean)
