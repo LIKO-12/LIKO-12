446fdc91720ff8f68a4873f9f047530ab30339a6
RamiLego4Game <ramilego4game@gmail.com>
2018-05-21 18:56:09 +0300

    Increase LIKO-12 version

--- conf.lua
+++ conf.lua
@@ -4,8 +4,8 @@
 _LVer = {
 	magor = 0,
 	minor = 8,
-	patch = 0,
-	tag = "PRE", --"DEV"
+	patch = 1,
+	tag = "DEV",
 }
 _LVERSION = string.format("V%d.%d.%d_%s",_LVer.magor,_LVer.minor,_LVer.patch,_LVer.tag)
 
