19a8d7cc4f75c731256b8c86d64c36b73abc9bb0
RamiLego4Game <ramilego4game@gmail.com>
2018-05-21 19:40:07 +0300

    Add a button and an imageButton to the guitest
    Former-commit-id: 3940b27fc5ece0d29e33bb261ea324ba3f8b605b

--- OS/DiskOS/Programs/guitest.lua
+++ OS/DiskOS/Programs/guitest.lua
@@ -1,17 +1,39 @@
 --A program to test the GUI library.
 
-local GUIClass = require("Libraries.GUI")
+local GUIState = require("Libraries.GUI")
 
-local GUI = GUIClass(5)
+local MainState = GUIState(5)
 
-local s1 = GUI:newObject("slider", 20,20)
+local b1 = MainState:newObject("button","Button",5,5)
+
+
+local trashIcon = image(
+[[LK12;GPUIMG;13x13;
+5999999999995
+9999555559999
+9555555555559
+9556666666559
+9956666666599
+9956565656599
+9956565656599
+9956565656599
+9956565656599
+9956565656599
+9956666666599
+9995555555999
+5999999999995]]
+)
+
+local ib1 = MainState:newObject("imageButton",5,15)
+ib1:setImage(trashIcon,4,9)
+--local s1 = MainState:newObject("slider", 20,20)
 
 clear(5)
-GUI:redraw()
+MainState:redraw()
 
 for event,a,b,c,d,e,f in pullEvent do
   
-  GUI:event(event,a,b,c,d,e,f)
+  MainState:event(event,a,b,c,d,e,f)
   
   if event == "keypressed" then
     if a == "escape" then
