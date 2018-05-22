1a29d0ff6549b87420f7e30c944609fa640d1721
RamiLego4Game <ramilego4game@gmail.com>
2018-05-22 20:33:43 +0300

    WIP GUI

---
+++ OS/DiskOS/Libraries/GUI/panel.lua
@@ -0,0 +1,17 @@
+local path = select(1,...):sub(1,-(string.len("panel")+1))
+
+local base = require(path..".base")
+
+--Panel (An objects container)
+local panel = class("DiskOS.GUI.panel",base)
+
+--Create a new panel object:
+--<gui> -> The GUI instance that should contain the panel.
+--[x],[y] -> The position of the top-left corner of the panel.
+--[w],[h] -> The size of the panel.
+function panel:initialize(gui,x,y,w,h,bgcolor)
+  base.initialize(self,gui,x,y,w,h)
+  
+end
+
+return panel
