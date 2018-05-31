5eb2a05668b167a8d285c664d758688b33da872a
RamiLego4Game <ramilego4game@gmail.com>
2018-05-31 21:32:57 +0300

    New 2 WIP APIS
    Former-commit-id: 3f162aa8a42fa0dc50e3e13a9e2146e62d43d9fc

---
+++ OS/DiskOS/APIS/BuildUtils.lua
@@ -0,0 +1,13 @@
+--Native Builds Utilities.
+
+--Variables.
+
+
+--Localized Lua Library
+
+
+--The API
+local BuildUtils = {}
+
+--Make the buildutils a global
+_G["BuildUtils"] = BuildUtils
---
+++ OS/DiskOS/APIS/TextUtils.lua
@@ -0,0 +1,13 @@
+--Text Utilities.
+
+--Variables.
+
+
+--Localized Lua Library
+
+
+--The API
+local TextUtils = {}
+
+--Make the textutils a global
+_G["TextUtils"] = TextUtils
