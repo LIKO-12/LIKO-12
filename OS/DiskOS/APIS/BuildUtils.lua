3478aeba440f83512478536ab39ff9ca9c1535e2
RamiLego4Game <ramilego4game@gmail.com>
2018-06-01 21:22:13 +0300

    WIP BuildUtils and DiskUtils

--- OS/DiskOS/APIS/BuildUtils.lua
+++ OS/DiskOS/APIS/BuildUtils.lua
@@ -9,5 +9,26 @@
 --The API
 local BuildUtils = {}
 
+--Mount LIKO-12 Sourcecode
+function BuildUtils.mountSRC()
+  fs.mount(BIOS.getSRC())
+end
+
+function BuildUtils.filesTree(dir)
+  if fs.isFile(dir) then return fs.read(dir) end
+  
+  local tree = {}
+  
+  for id, name in ipairs(fs.getDirectoryItems()) do
+    tree[id] = BuildUtils.filesTree(dir.."/"..name)
+  end
+  
+  return tree
+end
+
+function BuildUtils.getSavefile()
+  
+end
+
 --Make the buildutils a global
 _G["BuildUtils"] = BuildUtils
---
+++ OS/DiskOS/APIS/DiskUtils.lua
@@ -0,0 +1,21 @@
+--Disk File (.lk12) Utilities.
+
+--Variables.
+
+
+--Localized Lua Library
+
+
+--The API
+local DiskUtils = {}
+
+function DiskUtils.save(ctype,clvl)
+  
+end
+
+function DiskUtils.write()
+  
+end
+
+--Make the diskutils a global
+_G["DiskUtils"] = DiskUtils
