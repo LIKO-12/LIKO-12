0645a0b15d9af3c6eedc5aa99e03f5d542b89f98
RamiLego4Game <ramilego4game@gmail.com>
2018-06-17 14:33:22 +0300

    Cleanup
    Former-commit-id: f6aff0842bb39a3d56482b2c95c0c64cfa7d7596

--- OS/DiskOS/Libraries/SyntaxParser/languages/lua.lua
+++ OS/DiskOS/Libraries/SyntaxParser/languages/lua.lua
@@ -1,114 +1,129 @@
+--Lua syntax parser
+
+--Lua Keywords
 local keywords = {
   "and", "break", "do", "else", "elseif",
   "end", "false", "for", "function", "if",
   "in", "local", "nil", "not", "or",
   "repeat", "return", "then", "true", "until", "while"
 }
-local api = getAPI()
-local callbacks = {"_draw","_draw30","_draw60","_init","_keypressed","_keyreleased","_mousemoved","_mousepressed","_mousereleased","_textinput","_touchcontrol","_touchmoved","_touchpressed","_touchreleased","_update","_update30","_update60","_wheelmoved","_filedropped","self"}
-local escapable = {"a", "b", "f", "n", "r", "t", "v", "\\", "\"", "'"}
 
--- Convert values to keys
+--Lua escapable characters
+local escapable = {"a", "b", "f", "n", "r", "t", "v", "\\", '"', "'"}
+
+--LIKO-12 API Functions
+local api = getAPI()
+
+--LIKO-12 Callbacks
+local callbacks = {"_draw","_draw30","_draw60","_init","_keypressed","_keyreleased","_mousemoved","_mousepressed","_mousereleased","_textinput","_touchcontrol","_touchmoved","_touchpressed","_touchreleased","_update","_update30","_update60","_wheelmoved","_filedropped","self"}
+
+--Convert values to keys for instant searching
 for _, list in ipairs({keywords, api, callbacks, escapable}) do
-  for i, word in ipairs(list) do
-    list[word] = true
+  for i=1, #list do
+    list[list[i]] = true
     list[i] = nil
   end
 end
 
+--The start state of the tokenizer
 local function startState()
   return {
     tokenizer = "base",
+    multilen = 0, --Multiline comment/string '==' number, example: [=[ COMMENT ]=] -> 1
     starter = ""
   }
 end
 
 local function token(stream, state)
-  local result = nil
-
   if state.tokenizer == "base" then
-    local char = stream:next()
-    local pos = stream.pos
-    -- Comment and multiline comment matching
-    if char == "-" and stream:eat('%-') then
-      if stream:match("^%[=*%[") then
-        state.multilen = stream.pos - pos - 3
+    local char = stream:next() --Read a character from the stream
+    local pos = stream.pos --The position of the next character
+    
+      --Comment and multiline comment matching
+    if char == "-" and stream:eat('%-') then --Parse the '--'
+      if stream:match("^%[=*%[") then --It's a multilineComment
         state.tokenizer = "multilineComment"
-      else
-        stream:skipToEnd()
-        result = "comment"
+        state.multilen = stream.pos - pos - 3
+      else --It's a single line comment
+        stream:skipToEnd() --The rest of the line is a comment
+        return "comment"
       end
-      -- String matching
+      
+      --String matching
     elseif char == '"' or char == "'" then
       state.starter = char
       state.tokenizer = "string"
       return "string" -- Return immediatelly so quotes doesn't get affected by escape sequences
+      
       -- Decimal numbers
     elseif char == '.' and stream:match('%d+') then
-      result = 'number'
+      return "number"
+      
       -- Hex
     elseif char == "0" and stream:eat("[xX]") then
       stream:eatChain("%x")
-      result = "number"
+      return "number"
+      
       -- Ints and floats numbers
     elseif char:find('%d') then
       stream:eatChain("%d")
       stream:match("\\.%d+")
       local nextChar = stream:peek() or "" -- TODO: Do this to hex and decimals too
       if not nextChar:find("[%w_]") then
-        result = "number"
+        return "number"
       end
-      -- elseif operators[char] then
-      --     return 'operator'
+      
       -- Multiline string matching
     elseif char == "[" and stream:match("^=*%[") then
       state.multilen = stream.pos - pos - 1
       state.tokenizer = "multilineString"
       return "string"
+      
       -- Keyword matching
     elseif char:find('[%w_]') then
       stream:eatChain('[%w_]')
       local word = stream:current()
       if keywords[word] then
-        result = "keyword"
+        return "keyword"
       elseif api[word] then
-        result = "api"
+        return "api"
       elseif callbacks[word] then
-        result = "callback"
+        return "callback"
       end
     end
   end
 
   if state.tokenizer == "string" then
     local char = stream:next()
-    result = "string"
     if char == "\\" then
       local escaped = stream:peek()
       if escaped and escapable[escaped] then
         stream:next()
-        result = "escape"
+        return "escape"
       end
     elseif char == state.starter then
       state.starter = ""
       state.tokenizer = "base"
-      --else
-      --    stream:skipToEnd()
     else
       if stream:eol() then state.tokenizer = "base" end
     end
+    
+    return "string"
 
   elseif state.tokenizer == "multilineString" then
     local char = stream:next()
-    result = "string"
+    
     if char == "\\" then
       local escaped = stream:peek()
       if escaped and escapable[escaped] then
         stream:next()
-        result = "escape"
+        return "escape"
       end
     elseif char == "]" and stream:match("^" .. string.rep("=", state.multilen) .. "%]") then
       state.tokenizer = "base"
     end
+    
+    return "string"
 
   elseif state.tokenizer == "multilineComment" then
     if stream:skipTo("%]" .. string.rep("=", state.multilen) .. "%]") then
@@ -117,10 +132,9 @@ local function token(stream, state)
     else
       stream:skipToEnd()
     end
-    result = "comment"
+    
+    return "comment"
   end
-
-  return result
 end
 
 return {
