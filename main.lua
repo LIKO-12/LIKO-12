64f1a7b4f410842f15e2f94ee3e858d67e18e755
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-13 22:54:13 +0300

    Enable back love deprecation output

--- main.lua
+++ main.lua
@@ -24,8 +24,6 @@ I don't think anyone would want to edit anything in this file.
 - Rami Sabbagh (RamiLego4Game)
 ]]
 
-love.setDeprecationOutput(false) --Disable desprecation output temporary (Until the 11.0 update is done)
-
 local package_exceptions = {
   "bit", "utf8", "ffi", "ssl.core", "ssl.context", "ssl.x209", "ssl", "https", "socket.http", "ltn12", "mime", "socket.smtp", "socket", "socket.url"
 }
