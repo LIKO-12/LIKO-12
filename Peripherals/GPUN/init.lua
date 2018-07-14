959c49e84a12e411e6d08f132cb27fa5d90c22c0
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-14 22:49:13 +0300

    Appdata directories creation
    Former-commit-id: 3258b21fb0fc0bb927283a088b267764e195a3a0

--- Peripherals/GPUN/init.lua
+++ Peripherals/GPUN/init.lua
@@ -3,6 +3,19 @@ local perpath = select(1,...) --The path to the GPU folder.
 
 return function(config) --A function that creates a new GPU peripheral.
   
+  --Create appdata directories:
+  if not love.filesystem.getInfo("Shaders","directory") then
+    love.filesystem.createDirectory("Shaders")
+  end
+  
+  if not love.filesystem.getInfo("Screenshots","directory") then
+    love.filesystem.createDirectory("Screenshots")
+  end
+  
+  if not love.filesystem.getInfo("GIF Recordings","directory") then
+    love.filesystem.createDirectory("GIF Recordings")
+  end
+  
   --GPU: the non-yielding APIS of the GPU.
   --yGPU: the yield APIS of the GPU.
   --GPUKit: Shared data between the GPU files.
