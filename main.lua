2d637520748b4c9afd3c525d950bff4ff7866346
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-15 19:12:24 +0300

    Cleared LuaCheck warnings from `main.lua`

--- main.lua
+++ main.lua
@@ -6,14 +6,16 @@ local reboot, events = false
 I did create an events system for LIKO-12, making my work more modular.
 Below there is a modified love.run function which implements 4 things:
 - Instead of calling love callbacks, it triggers the events with name "love:callback", for ex: "love:mousepressed".
-- It contains a small and a nice trick which reloads all the code files (expect main.lua & conf.lua) then reboots LIKO-12 without haveing to restart love.
+- It contains a small and a nice trick which reloads all the code files (expect main.lua & conf.lua)
+    then reboots LIKO-12 without haveing to restart love.
 - When the love.graphics is active (usable) it triggers "love:graphics" event.
 - If any "love:quit" event returned true, the quit will be canceled.
 
 About the soft restart:
 * To do a soft restart trigger the "love:reboot" event, you can pass custom args for love.load to get after reboot.
-* This works by clearing package.loaded expect native libraries, calling love.graphics.reset(), reseting the events library, and finally restarts love.run from the top.
-* In case you changed something in main.lua or conf.lua then you can do a hard restart by calling love.event.quit("restart")
+* This works by clearing package.loaded expect native libraries, calling love.graphics.reset(),
+    reseting the events library, and finally restarts love.run from the top.
+* In case you changed something in main.lua or conf.lua, you can do a hard restart by calling love.event.quit("restart")
 * In DiskOS you can run 'reboot' command to do a soft reboot, or 'reboot hard' to do a hard one (by restarting love).
 
 I don't think anyone would want to edit anything in this file.
@@ -25,7 +27,9 @@ I don't think anyone would want to edit anything in this file.
 ]]
 
 local package_exceptions = {
-  "bit", "utf8", "ffi", "ssl.core", "ssl.context", "ssl.x209", "ssl", "https", "socket.http", "ltn12", "mime", "socket.smtp", "socket", "socket.url"
+  "bit", "utf8", "ffi", --LuaJIT
+  "ssl.core", "ssl.context", "ssl.x209", "ssl", "https", --LuaSec
+  "socket.http", "ltn12", "mime", "socket.smtp", "socket", "socket.url" --LuaSocket
 }
 
 for k,v in ipairs(package_exceptions) do package_exceptions[v] = k end
@@ -33,24 +37,24 @@ for k,v in ipairs(package_exceptions) do package_exceptions[v] = k end
 love.filesystem.load("Engine/errhand.lua")() --Apply the custom error handler.
 
 --Internal Callbacks--
-function love.load(args)
+function love.load()
   love.filesystem.load("BIOS/init.lua")() --Initialize the BIOS.
   events.trigger("love:load")
 end
 
 function love.run()
-  arg = {love.arg.parseGameArguments(arg), arg}
+  local loadArg = {love.arg.parseGameArguments(arg), arg}
   
   local dt = 0
   
   local function runReset()
     events = require("Engine.events")
     events.register("love:reboot",function(args) --Code can trigger this event to do a soft restart.
-      local args = args or {}
+      args = args or {}
       reboot = {love.arg.parseGameArguments(args),args}
     end)
 
-    if love.load then love.load(reboot or arg) end reboot = false
+    if love.load then love.load(reboot or loadArg) end reboot = false
 
     -- We don't want the first frame's dt to include time taken by love.load.
     if love.timer then love.timer.step() end
@@ -97,7 +101,7 @@ function love.run()
     if love.timer then love.timer.sleep(0.001) end
 
     if reboot then
-      for k,v in pairs(package.loaded) do
+      for k in pairs(package.loaded) do
         if not package_exceptions[k] then package.loaded[k] = nil end
       end--Reset the required packages
 
