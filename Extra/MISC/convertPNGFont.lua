7887245336e6af528ee18e1bd3300723e572496e
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-18 20:10:01 +0300

    Upload font converting string.
    Former-commit-id: c06528e8915b45e789bba8f7c2854b69845dfbbd

---
+++ Extra/MISC/convertPNGFont.lua
@@ -0,0 +1,17 @@
+local pngData = fs.read("D:/font4x6.png")
+local imgData = imagedata(pngData)
+local fontData = fontdata(4,6)
+
+
+imgData:map(function(x,y,c)
+ local cb = math.floor(x/5)+1
+ x = x%5 -1
+ 
+ if y == 6 or x == -1 or cb == 256 then return end
+ 
+ fontData:setPixel(cb,x,y,(c==7))
+end)
+
+fs.write("D:/font4x6.lk12",fontData:encode())
+
+print("done")
