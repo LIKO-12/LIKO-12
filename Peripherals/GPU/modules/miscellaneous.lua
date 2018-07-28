7649b0fed7511bb3920863646f1955896a672049
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-28 23:34:45 +0300

    Fix GPU system message errors, and allow time to be 0 (for cancelling messages)

--- Peripherals/GPU/modules/miscellaneous.lua
+++ Peripherals/GPU/modules/miscellaneous.lua
@@ -19,15 +19,15 @@ MiscVars.MSGTimer = 0 --The message timer.
 
 local function systemMessage(msg,time,tcol,col,hideInGif)
   if not msg then MiscVars.MSGTimer = 0 end --Clear last message
-  if type(msg) ~= "string" then return false, "Message must be a string, provided: "..type(msg) end
+  if type(msg) ~= "string" then return error("Message must be a string, provided: "..type(msg)) end
   
-  if time and type(time) ~= "number" then return false, "Time must be a number or a nil, provided: "..type(time) end
-  if tcol and type(tcol) ~= "number" then return false, "Text color must be a number or a nil, provided: "..type(tcol) end
-  if col and type(col) ~= "number" then return false, "Body Color must be a number or a nil, provided: "..type(col) end
+  if time and type(time) ~= "number" then return error("Time must be a number or a nil, provided: "..type(time)) end
+  if tcol and type(tcol) ~= "number" then return error("Text color must be a number or a nil, provided: "..type(tcol)) end
+  if col and type(col) ~= "number" then return error("Body Color must be a number or a nil, provided: "..type(col)) end
   time, tcol, col = time or 1, math.floor(tcol or 4), math.floor(col or 9)
-  if time <= 0 then return false, "Time must be bigger than 0" end
-  if tcol < 0 or tcol > 15 then return false, "Text Color ID out of range ("..tcol..") Must be [0,15]" end
-  if col < 0 or col > 15 then return false, "Body Color ID out of range ("..col..") Must be [0,15]" end
+  if time < 0 then return error("Time can't be negative") end
+  if tcol < 0 or tcol > 15 then return error("Text Color ID out of range ("..tcol..") Must be [0,15]") end
+  if col < 0 or col > 15 then return error("Body Color ID out of range ("..col..") Must be [0,15]") end
   MiscVars.LastMSG = msg
   MiscVars.LastMSGTColor = tcol
   MiscVars.LastMSGColor = col
@@ -38,7 +38,8 @@ local function systemMessage(msg,time,tcol,col,hideInGif)
 end
 
 function GPU._systemMessage(msg,time,tcol,col,hideInGif)
-  return systemMessage(msg,time,tcol,col,hideInGif)
+  local ok, err = pcall(systemMessage,msg,time,tcol,col,hideInGif)
+  if not ok then return error(err) end
 end
 
 events.register("love:update",function(dt)
