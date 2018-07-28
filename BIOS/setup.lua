2b5c254b9a6941e9ddad8ba26e58f1c47f36d101
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-28 19:11:59 +0300

    Implemented `ShowAppdata` and `Wipe a drive` tools

--- BIOS/setup.lua
+++ BIOS/setup.lua
@@ -21,6 +21,7 @@ local TC = Handled.TC
 local sw,sh = GPU.screenSize()
 local tw,th = GPU.termSize()
 local fw,fh = GPU.fontSize()
+local mobile = CPU.isMobile()
 
 local checkboard = GPU.imagedata("LK12;GPUIMG;2x2;7007")
 
@@ -30,12 +31,12 @@ local tabs = {}
 local tools = {}
 
 --Touch to Keyboard
-local TMap = {"left","right","up","down","z","x","c"}
+local TMap = {"left","right","up","down","return","escape","backspace"}
 local function touchToKeyboard(down,tid)
   CPU.triggerEvent(down and "keypressed" or "keyreleased",TMap[tid],TMap[tid],false)
 end
 
---Functions
+--The event loop
 local function eventLoop(evlist)
   for event, a,b,c,d,e,f in CPU.pullEvent do
     if event == "touchcontrol" then
@@ -44,7 +45,7 @@ local function eventLoop(evlist)
     
     if evlist then
       if evlist[event] then
-        if evlist[event](a,b,c,d,e,f) then break end
+        if evlist[event](evlist,a,b,c,d,e,f) then break end
       end
     else
       if tabs[selectedTab][2][event] then
@@ -54,11 +55,25 @@ local function eventLoop(evlist)
   end
 end
 
---Graphics
+--FS Size calculation
+local function storageSize(size)
+  if size < 1024 then --Bytes
+    return size.." B"
+  elseif size < 1024^2 then --Kilo-bytes
+    return (math.floor(size/102.4)/10).." KB"
+  else --Mega-bytes
+    return (math.floor(size/(1024^2 * 0.1))/10).." MB"
+  end
+end
+
+--==Graphics==--
+
+--Show a blue-colored system message.
 local function systemMessage(msg,time,textC,bgC,hideInGif)
   GPU._systemMessage(msg,time,textC or 1, bgC or 12, hideInGif)
 end
 
+--Print with background text.
 local function printBG(text,x,y,tc,bc,et)
   et = et or 0
   local bgw, bgh = #text*(fw+1)+1+et*2, fh+2
@@ -68,6 +83,7 @@ local function printBG(text,x,y,tc,bc,et)
   return bgw
 end
 
+--Print with background centered text.
 local function printCenterBG(text,y,tc,bc)
   local txtw = #text*(fw+1)-1
   return printBG(text,(sw-txtw)/2,y,tc,bc)
@@ -84,18 +100,33 @@ local function drawBars(title)
   GPU.patternFill()
   
   if title then
-    printCenterBG("@=- BIOS SETUP V3.0 -=@",1,1,12)
+    printCenterBG(title,1,1,12)
   end
 end
 
-local function drawUI()
+--==User Interface==--
+
+--Draw the basic UI.
+local function drawUI(subMenu)
   GPU.clear(5) --Light Gray
   
   --Draw the top & bottom bars
-  drawBars("@=- BIOS SETUP V3.0 -=@")
+  drawBars(subMenu or "@=- BIOS SETUP V3.0 -=@")
+  
+  if subMenu then
+    --Draw black lines without space for the tabs bar
+    GPU.line(-1,8,sw+1,8,0)
+    GPU.line(-1,sh-9,sw+1,sh-9,0)
+    
+    return
+  end
   
   --Draw the tabs body
   GPU.rect(0,8,sw,8,false,13)
+  
+  --Draw black lines
+  GPU.line(-1,16,sw+1,16,0)
+  GPU.line(-1,sh-9,sw+1,sh-9,0)
   
   --Draw the tabs
   local nextTabX = 5
@@ -104,12 +135,9 @@ local function drawUI()
     local tc = (selectedTab == t and 13 or 0)
     nextTabX = nextTabX + printBG(tabs[t][1],nextTabX,9, tc,bc,1) + 10
   end
-  
-  --Draw black lines
-  GPU.line(-1,16,sw+1,16,0)
-  GPU.line(-1,sh-9,sw+1,sh-9,0)
 end
 
+--Handle the keypresses for the tabs, returns true when the tab is changed.
 local function keypressTabs(key,sc,isrepeat)
   local oldTab = selectedTab
   if key == "left" then
@@ -117,11 +145,13 @@ local function keypressTabs(key,sc,isrepeat)
   elseif key == "right" then
     selectedTab = math.min(selectedTab+1,#tabs)
   end
-  if selectedTab ~= oldTab and tabs[selectedTab][2].init then
-    tabs[selectedTab][2].init(tabs[selectedTab][2])
+  if selectedTab ~= oldTab and tabs[selectedTab][2].selected then
+    tabs[selectedTab][2].selected(tabs[selectedTab][2])
+    return true
   end
 end
 
+--Draw a list of options
 local function drawOptions(tree,selectedOption)
   for i=1, #tree do
     local name = tree[i][1]
@@ -148,6 +178,7 @@ local function drawOptions(tree,selectedOption)
   end
 end
 
+--Handle the option selection, returns the new selection id when the key is up or down.
 local function keypressOptions(key,tree,selectedOption)
   if key == "up" then
     local newSel = math.max(selectedOption-1,1)
@@ -157,6 +188,10 @@ local function keypressOptions(key,tree,selectedOption)
     return tree[newSel][1] == "" and newSel+1 or newSel
   end
 end
+
+--==Tabs==--
+
+--##Information Tab##--
 
 tabs[1] = {"Info",{
   selectedOption = 1,
@@ -228,6 +263,8 @@ local defaultEvents = {
 tabs[2] = {"Peripherals",defaultEvents}
 tabs[3] = {"Boot",defaultEvents}
 
+--##Tools Tab##--
+
 local function arrowSelected(id,selectedID)
   if id == selectedID then
     return "\xDC\x88\x88" -- <--
@@ -272,6 +309,75 @@ tools[1] = function()
     CPU.openAppData("/")
     systemMessage("Openned Successfully.")
   end
+end
+
+--Show Appdata
+local appdataPath = CPU.getSaveDirectory().."/"
+local showAppdataEvents = {
+  update = function()
+    drawUI("@=- APPData Path -=@")
+    
+    GPU.color(7)
+    GPU.print(appdataPath,0,sh*0.45-(fh-1)/2,sw,"center")
+    
+    printCenterBG("Press "..(mobile and "the green button" or "escape").." to return back",sh*0.66,6,5)
+  end,
+  
+  keypressed = function(self,key)
+    return (key == "escape")
+  end
+}
+tools[2] = function() eventLoop(showAppdataEvents) end
+
+--Wipe a drive
+local wipeADriveEvents = {
+  selected = function(self)
+    self.options = {}
+    self.selectedOption = 1
+    
+    for name,info in pairs(fs.drives()) do
+      local subinfo = info.readonly and "[ReadOnly]" or string.format("[%s/%s]",storageSize(info.usage),storageSize(info.size))
+      
+      self.options[#self.options + 1] = {
+        "Wipe drive ("..name..")"..string.rep(" ",tw-13-#name-#subinfo-2),
+        subinfo
+      }
+    end
+  end,
+  
+  update = function(self)
+    drawUI("@=- Wipe a drive -=@")
+    
+    --Warning box
+    GPU.rect(0,8,sw,8,false,2)
+    GPU.line(-1,8,sw+1,8,1)
+    GPU.color(8) GPU.print("\xE3!\xE2 WARNING: THIS CANNOT BE REVERTED !",1,10)
+    GPU.line(-1,16,sw+1,16,0)
+    
+    drawOptions(self.options,self.selectedOption)
+    
+    printBG("\xC2 Press "..(mobile and "the green button" or "escape").." to return back",2,sh-16,0)
+  end,
+  
+  keypressed = function(self,key,_,isrepeat)
+    local newSel = keypressOptions(key,self.options,self.selectedOption)
+    if newSel then self.selectedOption = newSel; return end
+    
+    if key == "return" and not isrepeat then
+      local driveName = self.options[self.selectedOption][1]:sub(13,-1):gsub(" ",""):sub(1,-2)
+      systemMessage("Wiping drive ("..driveName..")...",100)
+      GPU.flip()
+      fs.delete(driveName..":/")
+      systemMessage("Wiped drive ("..driveName..") successfully.",1,1,12)
+      GPU.flip()
+    elseif key == "escape" then
+      return true
+    end
+  end
+}
+tools[3] = function()
+  wipeADriveEvents.selected(wipeADriveEvents)
+  eventLoop(wipeADriveEvents)
 end
 
 --Toggle Devmode
