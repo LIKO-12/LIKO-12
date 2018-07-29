5dd054af0c277b62f4edd5efb2c8a0f4446b6418
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-07-29 20:45:21 +0300

    Setup release deployment
    Former-commit-id: 146528220657ef0adf0a58e497f7b4e5c7676f45

--- Extra/Travis/deploy_release.sh
+++ Extra/Travis/deploy_release.sh
@@ -1,3 +1,59 @@
 #!/bin/bash
 
-----==== Work In Progress ====----
+echo -----------------------------------------
+echo ----==== Deploying Release Build ====----
+echo -----------------------------------------
+echo
+echo ----==== Creating the new release ====----
+
+gothub release \
+  --user RamiLego4Game \
+  --repo LIKO-12 \
+  --tag $TRAVIS_TAG \
+  --name "Draft Release" \
+  --description "Please wait for @RamiLego4Game to setup this release." \
+  --draft
+
+echo ----==== Uploading the builds ====----
+
+echo - Uploading windows build
+
+gothub upload \
+  --user RamiLego4Game \
+  --repo LIKO-12 \
+  --tag $TRAVIS_TAG \
+  --name "LIKO-12_V"$TRAVIS_TAG"_PRE_Windows.zip" \
+  --file ../BuildUtils/Builds/LIKO-12_Nightly_Windows.zip \
+  --replace
+
+echo - Uploading linux build
+
+gothub upload \
+  --user RamiLego4Game \
+  --repo LIKO-12 \
+  --tag $TRAVIS_TAG \
+  --name "LIKO-12_V"$TRAVIS_TAG"_PRE_Linux_x86_64.AppImage" \
+  --file ../BuildUtils/Builds/LIKO-12_Nightly_Linux_x86_64.AppImage \
+  --replace
+
+echo - Uploading mac build
+
+gothub upload \
+  --user RamiLego4Game \
+  --repo LIKO-12 \
+  --tag $TRAVIS_TAG \
+  --name "LIKO-12_V"$TRAVIS_TAG"_PRE_Mac.zip" \
+  --file ../BuildUtils/Builds/LIKO-12_Nightly_Mac.zip \
+  --replace
+
+echo - Uploading universal build
+
+gothub upload \
+  --user RamiLego4Game \
+  --repo LIKO-12 \
+  --tag $TRAVIS_TAG \
+  --name "LIKO-12_V"$TRAVIS_TAG"_PRE_Universal.love" \
+  --file ../BuildUtils/Builds/LIKO-12_Nightly_Universal.love \
+  --replace
+
+echo ----==== Done ====----
