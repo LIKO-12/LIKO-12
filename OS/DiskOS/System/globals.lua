0f321693898cc37d3464fa4906c009f4be48fe15
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2018-08-10 21:10:52 +0300

    Update api functions names
    Former-commit-id: 64ab8b7d64e89b86a7e41a15899efe74e836eb04

--- OS/DiskOS/System/globals.lua
+++ OS/DiskOS/System/globals.lua
@@ -43,8 +43,7 @@ do
     end
   end
   
-  add("dofile","printUsage","getAPI")
-  add("bump","class","lume","JSON","luann","geneticAlgo","vector")
+  add("dofile","printUsage","_APIVer","Library")
   add("Sprite","fget","fset","map","eventLoop","pget","pset","sget","sset","mget","mset","Controls","SFX","SaveID","SaveData","LoadData")
   add("SpriteMap","SheetFlagsData","TileMap","MapObj","SFXS","SfxObj","_GameCode","btn","btnp","__BTNUpdate","__BTNKeypressed","__BTNTouchControl","_BTNGamepad","_DISABLE_PAUSE")
 end
