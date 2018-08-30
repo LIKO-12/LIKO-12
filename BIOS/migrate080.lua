f1f089e301b6485e530e51d9e5bf4f91596b0df6
RamiLego4Game (Mobile) <ramilego4game@gmail.com>
2018-08-30 19:09:16 +0300

    Finished the migration script
    Former-commit-id: 4d87b0643a96020e8f87741d54013cb2fcaab987

--- BIOS/migrate080.lua
+++ BIOS/migrate080.lua
@@ -12,13 +12,13 @@ local oIdentity = "liko12"
 
 --Helper functions
 local function msg(...)
-  GPU._systemMessage(table.concat({...}," "),3600,0,7)
+  GPU._systemMessage(table.concat({...}),3600,0,7)
   CPU.sleep(0)
 end
 
 --Activate old identity
 local function activate()
-  love.filesyste.setIdentity(oIdentity)
+  love.filesystem.setIdentity(oIdentity)
 end
 
 --Deactivate old identity
@@ -26,9 +26,103 @@ local function deactivate()
   love.filesystem.setIdentity(nIdentity)
 end
 
+--Directories indexing function.
+local function index(path,list,ext,rec)
+  if not love.filesystem.getInfo(path) then return end
+  
+  for _,file in ipairs(love.filesystem.getDirectoryItems(path)) do
+    if love.filesystem.getInfo(path..file).type == "file" then
+      if not ext or file:sub(-#ext,-1) == ext then
+        list[#list + 1] = path..file
+      end
+    elseif rec then
+      index(path..file.."/",list,ext,rec)
+    end
+  end
+end
+
 --Start migrating
-msg("Migrating your old data...")
+msg("Migrating your old data (0%)...")
 
+local Data = {} --The files in the D drive.
+local Screenshots = {} --The .png files in the appdata folder.
+local GIFs = {} --The .gif files in the appdata folder.
+local Shaders = {} --The GPU shaders.
 
+activate()
+index("/",Screenshots,".png")
+index("/Screenshots",Screenshots,".png",true)
+
+index("/",GIFs,".gif")
+index("/GIF Recordings/",GIFs,".gif",true)
+
+index("/Shaders/",Shaders,false,true)
+index("/drives/D/",Data,false,true)
+
+if love.filesystem.getInfo("/drives/C/user.json") then
+  Data[#Data + 1] = "/drives/C/user.json"
+end
+deactivate()
+
+local total = #Screenshots + #GIFs + #Shaders + #Data
+local processed = 0
+
+local function progress(skip)
+  if not skip then processed = processed + 1 end
+  msg("Migrating your old data (",math.floor((processed/total)*100),"%)...")
+end
+
+for i=1, #Data do
+  local src = Data[i]
+  local dst = "/D"..src:sub(3,-1)
+  activate()
+  local data = love.filesystem.read(src)
+  deactivate()
+  love.filesystem.createDirectory(fs.getDirectory(dst))
+  love.filesystem.write(dst,data)
+  progress()
+end
+
+for i=1, #Shaders do
+  local path = Shaders[i]
+  activate()
+  local data = love.filesystem.read(path)
+  deactivate()
+  love.filesystem.createDirectory(fs.getDirectory(path))
+  love.filesystem.write(path,data)
+  progress()
+end
+
+for i=1, #GIFs do
+  local src = GIFs[i]
+  local dst = src:sub(1,16) == "/GIF Recordings/" and src or "/GIF Recordings"..src
+  activate()
+  local data = love.filesystem.read(src)
+  deactivate()
+  love.filesystem.createDirectory(fs.getDirectory(dst))
+  love.filesystem.write(dst,data)
+  progress()
+end
+
+for i=1, #Screenshots do
+  local src = Screenshots[i]
+  local dst = src:sub(1,13) == "/Screenshots/" and src or "/Screenshots"..src
+  activate()
+  local data = love.filesystem.read(src)
+  deactivate()
+  love.filesystem.createDirectory(fs.getDirectory(dst))
+  love.filesystem.write(dst,data)
+  progress()
+end
+
+activate()
+if love.filesystem.getInfo(".version") then
+  love.filesystem.remove(".version")
+end
+
+if love.filesystem.getInfo("Miscellaneous/.version") then
+  love.filesystem.remove("Miscellaneous/.version")
+end
+deactivate()
 
 GPU._systemMessage("",0)
