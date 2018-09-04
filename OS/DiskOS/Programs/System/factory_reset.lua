0676aa082aa3072d07572d17467f64ab0851afee
boredom101 <ymnewmark@gmail.com>
2018-09-03 20:18:50 -0400

    Fixed Whitespace
    Former-commit-id: e55a460cb030ef3c96cff51525bcb25ddb0fb9cc

--- OS/DiskOS/Programs/System/factory_reset.lua
+++ OS/DiskOS/Programs/System/factory_reset.lua
@@ -5,23 +5,23 @@ if select(1,...) == "-?" then
   )
   return
 end
- fs.delete("C:/boot.lua")
- if select(1,...) == "-wipe" then
-  local term = require("C:/terminal.lua")
-  term.execute("rm","C:/")
+  fs.delete("C:/boot.lua")
+  if select(1,...) == "-wipe" then
+    local term = require("C:/terminal.lua")
+    term.execute("rm","C:/")
 
-  clear(0)
-else
-  local img = imagedata(screenSize())
-  img:map(function(x,y,c)
-    if x%2 == 0 then
-      return 8
-    else
-      return 2
-    end
-  end)
+    clear(0)
+  else
+    local img = imagedata(screenSize())
+    img:map(function(x,y,c)
+      if x%2 == 0 then
+        return 8
+      else
+        return 2
+      end
+    end)
   img:image():draw(0,0)
 end
- flip()
+flip()
 sleep(1)
 reboot()
