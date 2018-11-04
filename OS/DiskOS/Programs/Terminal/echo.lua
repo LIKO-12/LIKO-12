f6f3d554579549d5943180a6dae5b6638b752926
Martín Aguilar <42896961+mrtnpwn@users.noreply.github.com>
2018-11-04 16:34:05 -0600

    Fix identation
    Former-commit-id: 8b0307b15f6523dbd77e1e3115c702a6a6da8781

--- OS/DiskOS/Programs/Terminal/echo.lua
+++ OS/DiskOS/Programs/Terminal/echo.lua
@@ -2,12 +2,12 @@ local args = {...}
 local message = {}
 
 if args[1] == '-?' or args[1] == nil or #args < 2 then
-	printUsage('echo <message>', 'Print a message to stdout')
-	return
+  printUsage('echo <message>', 'Print a message to stdout')
+  return
 end
 
 for i, x in pairs(args) do
-	table.insert(message, x)
+  table.insert(message, x)
 end
 
 local result = table.concat(message, ' ')
