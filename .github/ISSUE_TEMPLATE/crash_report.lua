34ec1b91aec5d4372868a85da44dd91f54cc3541
Rami Sabbagh <ramilego4game@gmail.com>
2019-04-18 12:06:35 +0300

    Create crash_report.lua
    Former-commit-id: 068889568d19aba888f1265bd33d712b7271fdf4

---
+++ .github/ISSUE_TEMPLATE/crash_report.lua
@@ -0,0 +1,42 @@
+---
+name: Crash report
+about: Submit a crash report to help us improve
+
+---
+
+**Crash Type**
+Is it LIKO-12's blue screen of death, red error messags, app crash or a whole device crash ?
+
+**To Reproduce**
+Steps to reproduce the behavior:
+1. Type '...'
+2. Press '...'
+3. Click on '....'
+4. Scroll down to '....'
+5. See error
+
+**Desktop (please complete the following information):**
+ - OS: [e.g. Windows 7, Ubuntu, OS X]
+ - Architecture: [e.g. x86, x64]
+ - LIKO-12 Version: [e.g. v1.0.0, Nightly 30/12/2019]
+ - Downloaded from: [e.g. itch.io, github releases]
+ - Executable type: [e.g. Windows .exe, .AppImage, .love File, git fork]
+
+**Smartphone (please complete the following information):**
+ - Device: [e.g. Samasung J700H]
+ - OS: [e.g. Android Oreo, Android 7.0]
+ - LIKO-12 Version [e.g. v1.0.0, Nightly 30/12/2019]
+ - Downloaded from: [e.g. Play Store, itch.io, github releases]
+
+**Screenshots**
+If applicable, add screenshots to help explain your problem.
+
+**Crash logs/reports**
+```
+Paste the crash log/report here:
+
+
+```
+
+**Additional context**
+Add any other context about the problem here.
