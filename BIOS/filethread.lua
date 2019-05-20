7ccbd62d21bfdf8050b4adcfc3ed5e83cdd3a7fc
Olivier Grech <olivier-grech@users.noreply.github.com>
2019-05-20 17:38:48 +0200

    Improve filethread (#242)
    * Improve formatting and add some comments
    
    * Rename some variables
    
    * Store delay between checks in a variable
    
    * Use 2 spaces for indentation
    
    * Tiny spelling fix
    
    * Fix the tab indentation problem

--- BIOS/filethread.lua
+++ BIOS/filethread.lua
@@ -1,76 +1,93 @@
---A thread to keep track of files being edited, and automatically update them in the appdata folder
---So there would be no longer need to restart LIKO-12 for some tasks.
+--Thread used to keep track of files being edited and automatically update them
+--in the appdata folder so LIKO-12 does not need to be restarted for some tasks
 
---By default it only tracks the DiskOS folder.
+--Only tracks the DiskOS folder by default
 
 require("love.system")
 require("love.timer")
 
 local reg = {}
 
-local tpath = "/OS/DiskOS/" --Tracking Path
-local dpath = "/drives/C/" --Destination Path
+local delay_between_checks = 3 --Time in seconds between each check
+local source_path = "/OS/DiskOS/"
+local target_path = "/drives/C/"
 
-local channel = love.thread.getChannel("BIOSFileThread") --Stop the thread when needed.
+local channel = love.thread.getChannel("BIOSFileThread") --Stop the thread when needed
 
-local function checkDir(dir,r)
+local function checkDir(dir, r)
 	dir = dir or ""
 	r = r or reg
-	local path = tpath..dir
+	local path = source_path .. dir
 	local items = love.filesystem.getDirectoryItems(path)
-	for k, file in ipairs(items) do if file:sub(1,1) ~= "." then
-		local fpath = path..file
-		if love.filesystem.getInfo(fpath,"file") then --It's a file
-			local fupdate = false --Should the file be updated ?
-			if not love.filesystem.getInfo(dpath..dir..file) then --Add new file
-				--print("New file added")
-				fupdate = true
-			else --Check old file
-        local info = love.filesystem.getInfo(fpath)
-				local modtime = info and info.modtime
-				if modtime then
-					if r[file] then --It's registered
-						if modtime > r[file] then --It has been edited !
-							fupdate = true
+
+	-- Check every file in the tracking path
+	for k, file in ipairs(items) do
+		if file:sub(1, 1) ~= "." then
+			local fpath = path .. file
+			if love.filesystem.getInfo(fpath, "file") then
+				-- It's a file
+				local file_need_update = false --Should the file be updated?
+				if not love.filesystem.getInfo(target_path .. dir .. file) then
+					-- The files does not exist in the target path, create it
+					file_need_update = true
+				else
+					-- Get the last timestamp at which the file was modified
+					local info = love.filesystem.getInfo(fpath)
+					local modtime = info and info.modtime
+					if modtime then
+						--Check the previous timestamp to see if the file need to be updated
+						if r[file] then
+							--The file is already registered
+							if modtime > r[file] then
+								--The file has been edited since last time, update it
+								file_need_update = true
+							end
+						else
+							--The file is not registered yet, register it
+							r[file] = info.modtime
 						end
-					else --It's not registered !
-						r[file] = info.modtime --Register it.
+					else
+						print("Error: failed to get modification time.")
 					end
-				else
-					print("Error: failed to get modification time.")
 				end
-			end
-			
-			--Update the file
-			if fupdate then
-        local info = love.filesystem.getInfo(fpath)
-				r[file] = info and info.modtime or false
-				local data, rerr = love.filesystem.read(fpath)
-				if data then
-					local ok, werr = love.filesystem.write(dpath..dir..file,data)
-					if not ok then r[file] = nil print("Error: Failed to write,",werr) else
-						--print("Updated File",fpath)
+				--Update the file if needed
+				if file_need_update then
+					local info = love.filesystem.getInfo(fpath)
+					r[file] = info and info.modtime or false
+					local data, rerr = love.filesystem.read(fpath)
+					if data then
+						local ok, werr = love.filesystem.write(target_path .. dir .. file, data)
+						if not ok then
+							r[file] = nil
+							print("Error: failed to write,", werr)
+						end
+					else
+						print("Error: failed to read,", rerr)
 					end
-				else
-					print("Error: Failed to read,",rerr)
 				end
+			else
+				--It's a directory, recursively check its content
+				if not r[file] then
+					r[file] = {}
+				end
+				checkDir(dir .. file .. "/", r[file])
 			end
-		else --Nested directory
-			if not r[file] then r[file] = {} end
-			checkDir(dir..file.."/",r[file])
 		end
-	end end
+	end
 end
 
 print("Started File Thread")
 
 while true do
 	local shut = channel:pop()
-	if shut then break end --Finish the thread.
-	
+	if shut then
+		--Finish the thread
+		break
+	end
+
 	checkDir()
-  
-  love.timer.sleep(3)
+
+	love.timer.sleep(delay_between_checks)
 end
 
 print("Finished File Thread")
