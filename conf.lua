f20f9f596d95bea79b9a61c433467c669f7bba74
Rami Sabbagh <ramilego4game@gmail.com>
2019-05-22 20:55:09 +0300

    Set version to 1.0.2 - Development

--- conf.lua
+++ conf.lua
@@ -15,8 +15,8 @@ I don't think anyone would want to edit anything in this file, but on change the
 _LVer = {
 	major = 1,
 	minor = 0,
-	patch = 1,
-	tag = "Release" --Release, Pre-Relase, Development
+	patch = 2,
+	tag = "Development" --Release, Pre-Relase, Development
 }
 _LVERSION = string.format("V%d.%d.%d-%s",_LVer.major,_LVer.minor,_LVer.patch,_LVer.tag)
 
