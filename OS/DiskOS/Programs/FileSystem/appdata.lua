8b2047d7c0ac5b577681accf7b0d66c4177c69aa
RamiLego4Game (HP Ubuntu) <ramilego4game@gmail.com>
2019-08-21 20:24:13 +0300

    Add `--path` option to `folder` and `appdata`

--- OS/DiskOS/Programs/FileSystem/appdata.lua
+++ OS/DiskOS/Programs/FileSystem/appdata.lua
@@ -1,9 +1,12 @@
-if select(1,...) and select(1,...) == "-?" then
-  printUsage("appdata","Open LIKO-12 appdata folder in the host os explorer")
+if select(1,...) == "-?" then
+  printUsage(
+    "appdata","Open LIKO-12 appdata folder in the host os explorer",
+    "appdata --path","Shows the real path of the appdata folder"
+  )
   return
 end
 
-if isMobile() then
+if isMobile() or select(1,...) == "--path" then
   color(9) print("Appdata folder location: ")
   color(6) print(getSaveDirectory().."/")
 else
