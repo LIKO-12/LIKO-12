d9cde63a729b1500976b93b534e476dcc355e24b
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-10-09 21:43:48 +0300

    User proper objects system and weak tables for images, imagedatas and spritebatches.
    Also spritebatches now have type and typeOf methods.

--- Peripherals/GPU/modules/image.lua
+++ Peripherals/GPU/modules/image.lua
@@ -9,20 +9,154 @@ local RenderVars = GPUVars.Render
 local CalibrationVars = GPUVars.Calibration
 local VRamVars = GPUVars.VRam
 
---==Varss Constants==--
+--==Vars Constants==--
 local Verify = SharedVars.Verify
 local ofs = CalibrationVars.Offsets
 local UnbindVRAM = VRamVars.UnbindVRAM
 
+--==Variables==--
+local weakMetatable = { __mode = "k" } --Weak keys
+local weakImage = setmetatable({}, weakMetatable)
+local weakSourceData = setmetatable({}, weakMetatable)
+local weakSpriteBatch = setmetatable({}, weakMetatable)
+
 --==GPU Image API==--
 
-function GPU.quad(x,y,w,h,sw,sh)
-  local ok, err = pcall(love.graphics.newQuad,x,y,w,h,sw,sh)
-  if ok then
-    return err
-  else
-    return error(err)
+--Shared values between spritebatch objects
+local sharedSpriteBatchObject = {}
+local spriteBatchMetatable = { __index = sharedSpriteBatchObject, __metatable = false }
+do
+  function sharedSpriteBatchObject:usage() return usage end
+  function sharedSpriteBatchObject:clear()
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    spritebatch:clear() return self end
+  function sharedSpriteBatchObject:flush()
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    spritebatch:flush() return self end
+  function sharedSpriteBatchObject:setBufferSize(size)
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    Verify(size,"bufferSize","number")
+    if not (size >= 1) then return error("Buffersize should be 1 or bigger, provided: "..size) end
+    spritebatch:setBufferSize(size)
+    return self
   end
+  function sharedSpriteBatchObject:getBufferSize()
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    return spritebatch:getBufferSize() end
+  function sharedSpriteBatchObject:getCount()
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    return spritebatch:getCount() end
+  function sharedSpriteBatchObject:add(quad,x,y,r,sx,sy,ox,oy,kx,ky)
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    x,y,r,sx,sy,ox,oy,kx,ky = x or 0, y or 0, r or 0, sx or 1, sy or sx or 1, ox or 0, oy or 0, kx or 0, ky or 0
+    if type(quad) ~= "userdata" then return error("Quad should be provided, ("..type(quad)..") is not accepted.") end
+    Verify(x,"x","number") Verify(y,"y","number")
+    Verify(r,"r","number")
+    Verify(sx,"sx","number") Verify(sy,"sy","number")
+    Verify(ox,"ox","number") Verify(ox,"ox","number")
+    Verify(kx,"kx","number") Verify(kx,"kx","number")
+    return spritebatch:add(quad,x,y,r,sx,sy,ox,oy,kx,ky)
+  end
+  function sharedSpriteBatchObject:set(id,quad,x,y,r,sx,sy,ox,oy,kx,ky)
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    x,y,r,sx,sy,ox,oy,kx,ky = x or 0, y or 0, r or 0, sx or 1, sy or sx or 1, ox or 0, oy or 0, kx or 0, ky or 0
+    if type(quad) ~= "userdata" then return error("Quad should be provided, ("..type(quad)..") is not accepted.") end
+    Verify(id,"id","number")
+    Verify(x,"x","number") Verify(y,"y","number")
+    Verify(r,"r","number")
+    Verify(sx,"sx","number") Verify(sy,"sy","number")
+    Verify(ox,"ox","number") Verify(ox,"ox","number")
+    Verify(kx,"kx","number") Verify(kx,"kx","number")
+    local ok, err = pcall(spritebatch.set,spritebatch,id,quad,x,y,r,sx,sy,ox,oy,kx,ky)
+    if not ok then return error(err) end
+    return self
+  end
+  function sharedSpriteBatchObject:draw(x,y,r,sx,sy,quad) UnbindVRAM()
+    local spritebatch = weakSpriteBatch[self]; if not spritebatch then return error("Use : instead of . when calling spritebatch object methods!") end
+    x, y, r, sx, sy = x or 0, y or 0, r or 0, sx or 1, sy or 1
+    GPU.pushColor()
+    love.graphics.setShader(RenderVars.ImageShader)
+    love.graphics.setColor(1,1,1,1)
+    if quad then
+      love.graphics.draw(spritebatch,quad,math.floor(x+ofs.quad[1]),math.floor(y+ofs.quad[2]),r,sx,sy)
+    else
+      love.graphics.draw(spritebatch,math.floor(x+ofs.image[1]),math.floor(y+ofs.image[2]),r,sx,sy)
+    end
+    love.graphics.setShader(RenderVars.DrawShader)
+    GPU.popColor()
+    RenderVars.ShouldDraw = true
+    return self
+  end
+
+  function sharedSpriteBatchObject:type() return "GPU.spriteBatch" end
+  function sharedSpriteBatchObject:typeOf(t) if t == "GPU" or t == "spriteBatch" or t == "GPU.spritebatch" or t == "LK12" then return true end end
+end
+
+--Shared values between image objects
+local sharedImageObject = {}
+local imageMetatable = { __index = sharedImageObject, __metatable = {} }
+do
+  function sharedImageObject:draw(x,y,r,sx,sy,quad) UnbindVRAM()
+    local Image = weakImage[self]; if not Image then return error("Use : instead of . when calling image object methods!") end
+
+    x, y, r, sx, sy = x or 0, y or 0, r or 0, sx or 1, sy or 1
+    GPU.pushColor()
+    love.graphics.setShader(RenderVars.ImageShader)
+    love.graphics.setColor(1,1,1,1)
+    if quad then
+      love.graphics.draw(Image,quad,math.floor(x+ofs.quad[1]),math.floor(y+ofs.quad[2]),r,sx,sy)
+    else
+      love.graphics.draw(Image,math.floor(x+ofs.image[1]),math.floor(y+ofs.image[2]),r,sx,sy)
+    end
+    love.graphics.setShader(RenderVars.DrawShader)
+    GPU.popColor()
+    RenderVars.ShouldDraw = true
+    return self
+  end
+
+  function sharedImageObject:refresh()
+    local Image = weakImage[self]; if not Image then return error("Use : instead of . when calling image object methods!") end
+    Image:replacePixels(weakSourceData[self])
+    return self
+  end
+
+  function sharedImageObject:size()
+    local Image = weakImage[self]; if not Image then return error("Use : instead of . when calling image object methods!") end
+    return Image:getDimensions() end
+  function sharedImageObject:width()
+    local Image = weakImage[self]; if not Image then return error("Use : instead of . when calling image object methods!") end
+    return Image:getWidth() end
+  function sharedImageObject:height()
+    local Image = weakImage[self]; if not Image then return error("Use : instead of . when calling image object methods!") end
+    return Image:getHeight() end
+  function sharedImageObject:data() 
+    local SourceData = weakSourceData[self]; if not SourceData then return error("Use : instead of . when calling image object methods!") end
+    return GPU.imagedata(SourceData) end
+  function sharedImageObject:quad(x,y,w,h)
+    local SourceData = weakSourceData[self]; if not SourceData then return error("Use : instead of . when calling image object methods!") end
+    return love.graphics.newQuad(x,y,w or self:width(),h or self:height(),self:width(),self:height()) end
+  
+  function sharedImageObject:batch(bufferSize, usage)
+    local Image = weakImage[self]; if not Image then return error("Use : instead of . when calling image object methods!") end
+    bufferSize, usage = bufferSize or 1000, usage or "static"
+    
+    Verify(bufferSize,"bufferSize","number")
+    if not (bufferSize >= 1) then return error("Buffersize should be 1 or bigger, provided: "..bufferSize) end
+    
+    Verify(usage,"usage","string")
+    if usage ~= "dynamic" and usage ~= "static" and usage ~= "stream" then
+      return error("Invalid usage: "..usage)
+    end
+    
+    local spritebatch = love.graphics.newSpriteBatch(Image, bufferSize, usage)
+    local sb = setmetatable({}, spriteBatchMetatable)
+    weakSpriteBatch[sb] = spritebatch --It's a weak secret
+    
+    return sb
+  end
+  
+  function sharedImageObject:type() return "GPU.image" end
+  function sharedImageObject:typeOf(t) if t == "GPU" or t == "image" or t == "GPU.image" or t == "LK12" then return true end end
 end
 
 function GPU.image(data)
@@ -39,104 +173,17 @@ function GPU.image(data)
     SourceData = data
   end
   
-  local i = {}
-  
-  function i:draw(x,y,r,sx,sy,quad) UnbindVRAM()
-    x, y, r, sx, sy = x or 0, y or 0, r or 0, sx or 1, sy or 1
-    GPU.pushColor()
-    love.graphics.setShader(RenderVars.ImageShader)
-    love.graphics.setColor(1,1,1,1)
-    if quad then
-      love.graphics.draw(Image,quad,math.floor(x+ofs.quad[1]),math.floor(y+ofs.quad[2]),r,sx,sy)
-    else
-      love.graphics.draw(Image,math.floor(x+ofs.image[1]),math.floor(y+ofs.image[2]),r,sx,sy)
-    end
-    love.graphics.setShader(RenderVars.DrawShader)
-    GPU.popColor()
-    RenderVars.ShouldDraw = true
-    return self
-  end
-  
-  function i:refresh()
-    Image:replacePixels(SourceData)
-    return self
-  end
-  
-  function i:size() return Image:getDimensions() end
-  function i:width() return Image:getWidth() end
-  function i:height() return Image:getHeight() end
-  function i:data() return GPU.imagedata(SourceData) end
-  function i:quad(x,y,w,h) return love.graphics.newQuad(x,y,w or self:width(),h or self:height(),self:width(),self:height()) end
-  function i:batch(bufferSize, usage)
-    bufferSize, usage = bufferSize or 1000, usage or "static"
-    
-    Verify(bufferSize,"bufferSize","number")
-    if not (bufferSize >= 1) then return error("Buffersize should be 1 or bigger, provided: "..bufferSize) end
-    
-    Verify(usage,"usage","string")
-    if usage ~= "dynamic" and usage ~= "static" and usage ~= "stream" then
-      return error("Invalid usage: "..usage)
-    end
-    
-    local spritebatch = love.graphics.newSpriteBatch(Image,bufferSize, usage)
-    
-    local sb = {}
-    
-    function sb:usage() return usage end
-    function sb:clear() spritebatch:clear() return self end
-    function sb:flush() spritebatch:flush() return self end
-    function sb:setBufferSize(size)
-      Verify(size,"bufferSize","number")
-      if not (size >= 1) then return error("Buffersize should be 1 or bigger, provided: "..size) end
-      spritebatch:setBufferSize(size)
-      return self
-    end
-    function sb:getBufferSize() return spritebatch:getBufferSize() end
-    function sb:getCount() return spritebatch:getCount() end
-    function sb:add(quad,x,y,r,sx,sy,ox,oy,kx,ky)
-      x,y,r,sx,sy,ox,oy,kx,ky = x or 0, y or 0, r or 0, sx or 1, sy or sx or 1, ox or 0, oy or 0, kx or 0, ky or 0
-      if type(quad) ~= "userdata" then return error("Quad should be provided, ("..type(quad)..") is not accepted.") end
-      Verify(x,"x","number") Verify(y,"y","number")
-      Verify(r,"r","number")
-      Verify(sx,"sx","number") Verify(sy,"sy","number")
-      Verify(ox,"ox","number") Verify(ox,"ox","number")
-      Verify(kx,"kx","number") Verify(kx,"kx","number")
-      return spritebatch:add(quad,x,y,r,sx,sy,ox,oy,kx,ky)
-    end
-    function sb:set(id,quad,x,y,r,sx,sy,ox,oy,kx,ky)
-      x,y,r,sx,sy,ox,oy,kx,ky = x or 0, y or 0, r or 0, sx or 1, sy or sx or 1, ox or 0, oy or 0, kx or 0, ky or 0
-      if type(quad) ~= "userdata" then return error("Quad should be provided, ("..type(quad)..") is not accepted.") end
-      Verify(id,"id","number")
-      Verify(x,"x","number") Verify(y,"y","number")
-      Verify(r,"r","number")
-      Verify(sx,"sx","number") Verify(sy,"sy","number")
-      Verify(ox,"ox","number") Verify(ox,"ox","number")
-      Verify(kx,"kx","number") Verify(kx,"kx","number")
-      local ok, err = pcall(spritebatch.set,spritebatch,id,quad,x,y,r,sx,sy,ox,oy,kx,ky)
-      if not ok then return error(err) end
-      return self
-    end
-    function sb:draw(x,y,r,sx,sy,quad) UnbindVRAM()
-      x, y, r, sx, sy = x or 0, y or 0, r or 0, sx or 1, sy or 1
-      GPU.pushColor()
-      love.graphics.setShader(RenderVars.ImageShader)
-      love.graphics.setColor(1,1,1,1)
-      if quad then
-        love.graphics.draw(spritebatch,quad,math.floor(x+ofs.quad[1]),math.floor(y+ofs.quad[2]),r,sx,sy)
-      else
-        love.graphics.draw(spritebatch,math.floor(x+ofs.image[1]),math.floor(y+ofs.image[2]),r,sx,sy)
-      end
-      love.graphics.setShader(RenderVars.DrawShader)
-      GPU.popColor()
-      RenderVars.ShouldDraw = true
-      return self
-    end
-    
-    return sb
-  end
-  
-  function i:type() return "GPU.image" end
-  function i:typeOf(t) if t == "GPU" or t == "image" or t == "GPU.image" or t == "LK12" then return true end end
-  
+  local i = setmetatable({}, imageMetatable)
+  weakImage[i], weakSourceData[i] = Image, SourceData
+
   return i
+end
+
+function GPU.quad(x,y,w,h,sw,sh)
+  local ok, err = pcall(love.graphics.newQuad,x,y,w,h,sw,sh)
+  if ok then
+    return err
+  else
+    return error(err)
+  end
 end
--- Peripherals/GPU/modules/imagedata.lua
+++ Peripherals/GPU/modules/imagedata.lua
@@ -8,7 +8,6 @@ local lg = love.graphics
 
 local SharedVars = GPUVars.Shared
 local PaletteVars = GPUVars.Palette
-local ImageDataVars = GPUVars.ImageData
 
 --==Varss Constants==--
 local _ImageTransparent = PaletteVars.ImageTransparent
@@ -21,8 +20,9 @@ local colorTo255 = SharedVars.colorTo255
 local mathFloor = math.floor
 local strFormat = string.format
 
---==Vars Variables==--
-ImageDataVars.PasteImage = false --A walkthrough to avoide exporting the image to png and reloading it.
+--==Variables==--
+local weakMetatable = { __mode = "k" } --Weak keys
+local weakImageData = setmetatable({}, weakMetatable)
 
 --==Helper Functions==--
 
@@ -44,6 +44,117 @@ local function _ImportImage(_,_, r,g,b,a)
 end
 
 --==GPU Imagedata API==--
+
+--Shared values between imagedata objects
+local sharedImageDataObject = {}
+local imagedataMetadata = { __index = sharedImageDataObject, __metatable = false }
+do
+  function sharedImageDataObject:size()
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    return imageData:getDimensions() end
+  function sharedImageDataObject:getPixel(x,y)
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    if not x then return error("Must provide X") end
+    if not y then return error("Must provide Y") end
+    x,y = mathFloor(x), mathFloor(y)
+    if x < 0 or x > self:width()-1 or y < 0 or y > self:height()-1 then
+      return false, "Pixel position out from the image region"
+    end
+    local r = imageData:getPixel(x,y)
+    return mathFloor(r*255)
+  end
+  function sharedImageDataObject:setPixel(x,y,c)
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    if type(c) ~= "number" then return error("Color must be a number, provided "..type(c)) end
+    if not x then return error("Must provide X") end
+    if not y then return error("Must provide Y") end
+    x,y = mathFloor(x), mathFloor(y)
+    if x < 0 or x > self:width()-1 or y < 0 or y > self:height()-1 then
+      return false, "Pixel position out from the image region"
+    end
+    c = mathFloor(c) if c < 0 or c > 15 then return error("Color out of range ("..c..") expected [0,15]") end
+    imageData:setPixel(x,y,c/255,0,0,1)
+    return self
+  end
+  function sharedImageDataObject:map(mf)
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    imageData:mapPixel(
+    function(x,y,r,g,b,a)
+      local c = mf(x,y,mathFloor(r*255))
+      if c and type(c) ~= "number" then return error("Color must be a number, provided "..type(c)) elseif c then c = mathFloor(c) end
+      if c and (c < 0 or c > 15) then return error("Color out of range ("..c..") expected [0,15]") end
+      if c then return c/255,0,0,1 else return r,g,b,a end
+    end)
+    return self
+  end
+  function sharedImageDataObject:height()
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    return imageData:getHeight() end
+  function sharedImageDataObject:width()
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    return imageData:getWidth() end
+  
+  function sharedImageDataObject:paste(imgData,dx,dy,sx,sy,sw,sh)
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    if type(imgData) ~= "table" then return error("ImageData must be an imagedata, got '"..type(imageData).."'") end
+    local PasteImage = weakImageData[imgData]; if not PasteImage then return error("Invalid ImageData Object!") end
+    imageData:paste(PasteImage,dx or 0,dy or 0,sx or 0,sy or 0,sw or PasteImage:getWidth(), sh or PasteImage:getHeight())
+    return self
+  end
+  
+  function sharedImageDataObject:quad(x,y,qw,qh)
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    return lg.newQuad(x,y,qw or self:width(),qh or self:height(),self:width(),self:height()) end
+  function sharedImageDataObject:image()
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    return GPU.image(imageData) end
+  
+  function sharedImageDataObject:export()
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    local expData = love.image.newImageData(self:width(),self:height())
+    expData:mapPixel(function(x,y) return _ExportImage(x,y, imageData:getPixel(x,y)) end)
+    return expData:encode("png"):getString()
+  end
+  
+  function sharedImageDataObject:exportOpaque()
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    local expData = love.image.newImageData(self:width(),self:height())
+    expData:mapPixel(function(x,y) return _ExportImageOpaque(x,y, imageData:getPixel(x,y)) end)
+    return expData:encode("png"):getString()
+  end
+  
+  function sharedImageDataObject:enlarge(scale)
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    scale = mathFloor(scale or 1)
+    if scale <= 0 then scale = 1 end --Protection
+    if scale == 1 then return self end
+    local newData = GPU.imagedata(self:width()*scale,self:height()*scale)
+    self:map(function(x,y,c)
+      for iy=0, scale-1 do for ix=0, scale-1 do
+        newData:setPixel(x*scale + ix,y*scale + iy,c)
+      end end
+    end)
+    return newData
+  end
+  
+  function sharedImageDataObject:encode() --Export to .lk12 format
+    local imageData = weakImageData[self]; if not imageData then return error("Use : instead of . when calling imagedata object methods!") end
+    local data = {strFormat("LK12;GPUIMG;%dx%d;",self:width(),self:height())}
+    local datalen = 2
+    self:map(function(x,_,c)
+      if x == 0 then
+        data[datalen] = "\n"
+        datalen = datalen + 1
+      end
+      data[datalen] = strFormat("%X",c)
+      datalen = datalen + 1
+    end)
+    return table.concat(data)
+  end
+  
+  function sharedImageDataObject:type() return "GPU.imageData" end
+  function sharedImageDataObject:typeOf(t) if t == "GPU" or t == "imageData" or t == "GPU.imageData" or t == "LK12" then return true end end
+end
 
 function GPU.imagedata(w,h)
   local imageData
@@ -76,97 +187,7 @@ function GPU.imagedata(w,h)
     return error("Invalid arguments")
   end
   
-  local id = {}
-  
-  function id:size() return imageData:getDimensions() end
-  function id:getPixel(x,y)
-    if not x then return error("Must provide X") end
-    if not y then return error("Must provide Y") end
-    x,y = mathFloor(x), mathFloor(y)
-    if x < 0 or x > self:width()-1 or y < 0 or y > self:height()-1 then
-      return false, "Pixel position out from the image region"
-    end
-    local r = imageData:getPixel(x,y)
-    return mathFloor(r*255)
-  end
-  function id:setPixel(x,y,c)
-    if type(c) ~= "number" then return error("Color must be a number, provided "..type(c)) end
-    if not x then return error("Must provide X") end
-    if not y then return error("Must provide Y") end
-    x,y = mathFloor(x), mathFloor(y)
-    if x < 0 or x > self:width()-1 or y < 0 or y > self:height()-1 then
-      return false, "Pixel position out from the image region"
-    end
-    c = mathFloor(c) if c < 0 or c > 15 then return error("Color out of range ("..c..") expected [0,15]") end
-    imageData:setPixel(x,y,c/255,0,0,1)
-    return self
-  end
-  function id:map(mf)
-    imageData:mapPixel(
-    function(x,y,r,g,b,a)
-      local c = mf(x,y,mathFloor(r*255))
-      if c and type(c) ~= "number" then return error("Color must be a number, provided "..type(c)) elseif c then c = mathFloor(c) end
-      if c and (c < 0 or c > 15) then return error("Color out of range ("..c..") expected [0,15]") end
-      if c then return c/255,0,0,1 else return r,g,b,a end
-    end)
-    return self
-  end
-  function id:height() return imageData:getHeight() end
-  function id:width() return imageData:getWidth() end
-  function id:___pushimgdata() ImageDataVars.PasteImage = imageData end --An internal function used when pasting images.
-  
-  function id:paste(imgData,dx,dy,sx,sy,sw,sh)
-    if type(imgData) ~= "table" then return error("ImageData must be a table, got '"..type(imageData).."'") end
-    if not (imgData.typeOf and imgData.typeOf("GPU.imageData")) then return error("Invalid ImageData Object") end
-    ImageDataVars.PasteImage = false; imgData:___pushimgdata(); if not ImageDataVars.PasteImage then return error("Fake ImageData Object") end
-    imageData:paste(ImageDataVars.PasteImage,dx or 0,dy or 0,sx or 0,sy or 0,sw or ImageDataVars.PasteImage:getWidth(), sh or ImageDataVars.PasteImage:getHeight())
-    return self
-  end
-  
-  function id:quad(x,y,qw,qh) return lg.newQuad(x,y,qw or self:width(),qh or self:height(),self:width(),self:height()) end
-  function id:image() return GPU.image(imageData) end
-  
-  function id:export()
-    local expData = love.image.newImageData(self:width(),self:height())
-    expData:mapPixel(function(x,y) return _ExportImage(x,y, imageData:getPixel(x,y)) end)
-    return expData:encode("png"):getString()
-  end
-  
-  function id:exportOpaque()
-    local expData = love.image.newImageData(self:width(),self:height())
-    expData:mapPixel(function(x,y) return _ExportImageOpaque(x,y, imageData:getPixel(x,y)) end)
-    return expData:encode("png"):getString()
-  end
-  
-  function id:enlarge(scale)
-    scale = mathFloor(scale or 1)
-    if scale <= 0 then scale = 1 end --Protection
-    if scale == 1 then return self end
-    local newData = GPU.imagedata(self:width()*scale,self:height()*scale)
-    self:map(function(x,y,c)
-      for iy=0, scale-1 do for ix=0, scale-1 do
-        newData:setPixel(x*scale + ix,y*scale + iy,c)
-      end end
-    end)
-    return newData
-  end
-  
-  function id:encode() --Export to liko12 format
-    local data = {strFormat("LK12;GPUIMG;%dx%d;",self:width(),self:height())}
-    local datalen = 2
-    self:map(function(x,_,c)
-      if x == 0 then
-        data[datalen] = "\n"
-        datalen = datalen + 1
-      end
-      data[datalen] = strFormat("%X",c)
-      datalen = datalen + 1
-    end)
-    return table.concat(data)
-  end
-  
-  function id.type() return "GPU.imageData" end
-  function id.typeOf(t) if t == "GPU" or t == "imageData" or t == "GPU.imageData" or t == "LK12" then return true end end
-  
+  local id = setmetatable({}, imagedataMetadata)
+  weakImageData[id] = imageData
   return id
 end
