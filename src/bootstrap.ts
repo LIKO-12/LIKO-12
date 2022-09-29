bce45fa2940aa33ef29b6d4ae6cc804213269ae5
Rami Sabbagh <rami.sab07@gmail.com>
2022-09-29 15:15:23 +0300

    Add temporary testing script

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,7 +5,36 @@
 import options from 'core/options';
 import loveEvents from 'core/love-events';
 import Machine from 'core/machine';
+import Events from 'modules/events';
+
+const rawProgram = `
+print('hello from Lua');
+
+print(coroutine.running());
+
+for eventName, a,b,c,d,e,f in events.pull do
+    print(eventName, a,b,c,d,e,f);
+end
+`;
 
 loveEvents.on('load', () => {
-    new Machine(options.modules, options.options);
+    const machine = new Machine(options.modules, options.options);
+    const events = machine.resolveModule<Events>('events');
+    if (!events) throw 'WHERE EVENTS';
+
+    const [program, compileError] = loadstring(rawProgram, 'program');
+    if (!program) throw compileError;
+
+    machine.load(program);
+    events.pushEvent('ping', 1);
+    events.pushEvent('ping', 2);
+    machine.resume();
+
+    print('yielded', 'dead:', machine.isDead());
+    events.pushEvent('ping', 3);
+    print('-p1');
+    events.pushEvent('ping', 4);
+    print('-p2');
+    events.pushEvent('ping', 5);
+    print('done');
 });
