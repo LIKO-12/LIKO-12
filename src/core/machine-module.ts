4f18f890a04428714db0948bcbdf5ee7c8cc1a35
Rami Sabbagh <rami.sab07@gmail.com>
2022-09-29 12:46:31 +0300

    Introducing the machine system

---
+++ src/core/machine-module.ts
@@ -0,0 +1,24 @@
+import Machine from "core/machine";
+
+/**
+ * Base class for all machine modules.
+ */
+export default class MachineModule {
+    static readonly MACHINE_MODULE_SYMBOL = Symbol('for identifying MachineModule subclasses');
+
+    /**
+     * @param _options options table from `options.json`. defaults to `{}`.
+     */
+    constructor(_machine: Machine, _options: Record<string, any>) {}
+
+    /**
+     * Construct an instance of a usable API to be exposed to the machine environment as a global.
+     * Named as the module name.
+     * 
+     * Or return `undefined` if the module has no API.
+     * @param machine The same machine during construction.
+     */
+    createAPI(_machine: Machine): Record<string, any> | undefined {
+        return undefined;
+    };
+}
---
+++ src/core/machine.ts
@@ -0,0 +1,176 @@
+import EventsEmitter from "core/events-emitter";
+import EnvironmentBox from "./environment-box";
+import MachineModule from "./machine-module";
+
+export default class Machine {
+    readonly events = new EventsEmitter();
+
+    private _environmentBox!: EnvironmentBox; // Initialized by the constructor's call to `resetEnvironment`.
+
+    private readonly _modules: Record<string, MachineModule> = {};
+    private _initializingModules = false;
+
+    /**
+     * Machine program thread (when running)
+     */
+    private _thread?: LuaThread;
+
+    constructor(modulesNames: string[], modulesOptions: Record<string, any>) {
+        this._loadModules(modulesNames, modulesOptions);
+        this.resetEnvironment();
+    }
+
+    /**
+     * @throws when a program is already loaded in the machine.
+     */
+    load(program: (...args: any[]) => any) {
+        if (this._thread) throw new Error('a program is already loaded');
+
+        this._environmentBox.apply(program);
+        const thread = coroutine.create(program);
+        this._environmentBox.protectThread(thread);
+        this._thread = thread;
+
+        return this;
+    }
+
+    /**
+     * @throws if no program is loaded into the machine.
+     */
+    resume(...args: any[]) {
+        if (!this._thread) throw new Error('no program is loaded to be resumed');
+
+        this.events.emit('resumed');
+        assert(coroutine.resume(this._thread, ...args));
+        this.events.emit('suspended');
+
+        // TODO: Execute a BSOD program with the same environment. This would allow doing crash dumps and saving user data.
+    }
+
+    isSuspended() {
+        if (!this._thread) return false;
+        return coroutine.status(this._thread) === 'suspended';
+    }
+
+    isRunning() {
+        if (!this._thread) return false;
+        const status = coroutine.status(this._thread);
+        return status === 'running' || status === 'normal';
+    }
+
+    isDead() {
+        if (!this._thread) return true;
+        return coroutine.status(this._thread) === 'dead';
+    }
+
+    getStatus(): 'running' | 'suspended' | 'dead' {
+        if (!this._thread) return 'dead';
+        const status = coroutine.status(this._thread);
+        return status === 'normal' ? 'running' : status;
+    }
+
+    /**
+     * @throws if no program is loaded into the machine.
+     */
+    unload() {
+        if (!this._thread) throw new Error('no program is loaded');
+        this._environmentBox.unprotectThread(this._thread);
+        this._thread = undefined;
+
+        return this;
+    }
+
+    /**
+     * @throws if a program is loaded into the machine.
+     */
+    resetEnvironment() {
+        if (this._thread) throw new Error('forbidden while a program is loaded');
+
+        this._environmentBox = new EnvironmentBox();
+        this._environmentBox.protectEnvironment(_G);
+
+        this._exposeModulesAPIs();
+        return this;
+    }
+
+    /**
+     * Resolve a module, for interoperability between machine modules.
+     * 
+     * When done during module initialization, the initialization is paused until the dependency is loaded.
+     * And thus it's considered a required dependency for the module.
+     * 
+     * But when done post-initialization, nil is returned if a module was not loaded.
+     * Thus considered an optional late dependency.
+     * 
+     * The pausing during initialization is achieved by initializing each module within a thread (coroutine).
+     */
+    resolveModule(moduleName: string): MachineModule | undefined {
+        if (this._initializingModules) while (!this._modules[moduleName]) coroutine.yield(moduleName);
+        return this._modules[moduleName];
+    }
+
+    private _loadModules(modulesNames: string[], modulesOptions: Record<string, any>) {
+        const pending: Record<string, LuaThread> = {};
+
+        /*
+        This is an approach for resolving modules dependencies using Lua threads (coroutines).
+        Where the modules should use `Machine.resolveModule` for resolving their required dependencies.
+        When a dependency is requested, the initialization thread yields to be resumed at the next iteration.
+        In the hope that the module it requested got loaded, otherwise it retries at the next iteration.
+
+        With (maximum iterations count = modules count) it's guaranteed that if a dependency is resolvable
+        it would have been already resolved. Any remaining dependencies would be due to:
+        - either not loaded module (not configured to be loaded or doesn't exist).
+        - or deadlocked dependency (two modules depending on each other).
+        The error message in this case should clarify the situation.
+
+        1. Loads each module's code.
+        2. Creates a thread for initializing an instance of each module.
+        3. Each thread is executed in the next section below for initializing the dependencies.
+        */
+        for (const moduleName of modulesNames) {
+            const moduleOptions = modulesOptions[moduleName] ?? {};
+
+            const Module: typeof MachineModule = require(`../modules/${moduleName}`);
+            if (Module.MACHINE_MODULE_SYMBOL !== MachineModule.MACHINE_MODULE_SYMBOL)
+                throw new Error('invalid module. should be a subclass of core/MachineModule');
+
+            const thread = coroutine.create(() => this._modules[moduleName] = new Module(this, moduleOptions));
+            pending[moduleName] = thread;
+        }
+
+        const unresolved: string[] = [];
+
+        this._initializingModules = true; // For Machine.resolveModule to yield when resolving an unloaded dependency.
+
+        // Notice the 1 extra iteration for looking up what's the stuck dependencies and forming the error message.
+        for (let i=modulesNames.length; i >= 0; i--) {
+            for (const moduleName in pending) {
+                const thread = pending[moduleName];
+                if (!thread) continue;
+
+                const [ok, requestedModule] = coroutine.resume(thread);
+                assert(ok, requestedModule);
+                
+                if (coroutine.status(thread) === 'dead') delete pending[moduleName];
+
+                if (i === 0 && requestedModule)
+                    unresolved.push(`(${requestedModule} <- ${moduleName})`);
+            }
+        }
+
+        this._initializingModules = false;
+
+        if (unresolved.length !== 0)
+            throw new Error(`failed to resolve modules dependencies: ${unresolved.join(', ')}`);
+    }
+
+    private _exposeModulesAPIs() {
+        const apis: Record<string, Record<string, any> | undefined> = {};
+
+        for (const moduleName in this._modules)
+            apis[moduleName] = this._modules[moduleName].createAPI(this);
+
+        this._environmentBox.expose(apis);
+    }
+}
