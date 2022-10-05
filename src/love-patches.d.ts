c58b95fea652e6223e393429d31cd9549f26d17a
Rami Sabbagh <rami.sab07@gmail.com>
2022-10-06 00:53:14 +0300

    Add a TODO note

--- src/love-patches.d.ts
+++ src/love-patches.d.ts
@@ -2,6 +2,8 @@
  * This file includes and missing declarations of LÖVE values.
  */
 
+// TODO: Contribute them back.
+
 /**
  * A global variable holding the command-line parameters passed to the executable.
  */
