cb12e7ed1911ca3664aaf70ad3f21d5a1b27da82
Rami Sabbagh <rami.sab07@gmail.com>
2022-10-07 10:36:31 +0300

    Improve the security of proxied objects

--- src/core/object-proxy.ts
+++ src/core/object-proxy.ts
@@ -1,37 +1,39 @@
+import { assertParameter } from "./utilities";
+
 const originalObjects: Record<any, any> = setmetatable({}, { __mode: 'k' });
-const patchedMethods: Record<any, any> = setmetatable({}, {
-    __index: function (this: any, method: any) {
-        const patched = (key: any, ...args: any[]) => {
-            const originalObject = originalObjects[key];
-            if (originalObject === undefined) error('call using : instead of .', 2);
-            // TODO: Find a better error message that's more clear (check love's one).
-            
-            const result = pcall(method, originalObject, ...args);
-            if (result[1] === originalObject) result[1] = key;
-            
-            if (result[0]) return $multi(...select(2, ...result));
-            error(result[1], 3);
-        };
+const patchedMethods: Record<any, any> = setmetatable({}, { __mode: 'k' });
 
-        (this as any)[method] = patched;
-        return patched;
-    },
+function proxyMethod(methodName: string, method: any) {
+    if (patchedMethods[method]) return patchedMethods[method];
 
-    __mode: 'k',
-});
+    const patched = (key: any, ...args: any[]) => {
+        const originalObject = originalObjects[key];
+        if (originalObject === undefined || originalObject[methodName] !== method)
+            error('bad object method call: invalid self parameter', 2);
+
+        const result = pcall(method, originalObject, ...args);
+        if (result[1] === originalObject) result[1] = key;
+
+        if (result[0]) return $multi(...select(2, ...result));
+        error(result[1], 3);
+    };
+
+    patchedMethods[method] = patched;
+    return patched;
+}
 
 const proxyMetatable: LuaMetatable<any> = {
-    __index: function(key) {
+    __index: function (key) {
         const originalObject = originalObjects[this];
         const value = originalObject[key];
 
         if (type(value) !== 'function') return undefined;
         if (type(key) === 'string' && key[0] === '_') return undefined;
 
-        return patchedMethods[value];
+        return proxyMethod(key, value);
     },
 
-    __tostring: function() {
+    __tostring: function () {
         const originalObject = originalObjects[this];
         return `userdata${string.sub(tostring(originalObject), 6, -1)}`;
     },
@@ -43,6 +45,8 @@ const proxyMetatable: LuaMetatable<any> = {
  * FIXME: Document object-proxy.
  */
 export function proxy<T>(object: T): T {
+    assertParameter(object, 'table', 1, 'proxy');
+
     const proxyObject: any = setmetatable({}, proxyMetatable);
     originalObjects[proxyObject] = object;
     return proxyObject as T;
