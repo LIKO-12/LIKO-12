6902be349906d0f08ef4d7504f75a13bffc7e078
Rami Sabbagh <rami.sab07@gmail.com>
2022-10-28 13:10:27 +0200

    formatting fix

--- packages/standard-modules-types/modules/keyboard.d.ts
+++ packages/standard-modules-types/modules/keyboard.d.ts
@@ -4,11 +4,11 @@
 declare module "@liko-12/standard-modules-types" {
     export interface KeyboardAPI {
         /**
-                     * Enable or disable text input events.
-                     * It is enabled by default on Windows, Mac, and Linux, and disabled by default on iOS and Android.
-                     * 
-                     * It would also show the on-screen keyboard for touch devices.
-                     */
+         * Enable or disable text input events.
+         * It is enabled by default on Windows, Mac, and Linux, and disabled by default on iOS and Android.
+         * 
+         * It would also show the on-screen keyboard for touch devices.
+         */
         setTextInput(this: void, enable: boolean): void;
 
         /**
