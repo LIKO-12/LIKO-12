a63a94106872ce42eae8ab3f1fc65194e60d8a05
Rami Sabbagh <rami.sab07@gmail.com>
2022-10-29 00:45:44 +0200

    Another use-case for LuaMap

--- packages/app/src/core/object-proxy.ts
+++ packages/app/src/core/object-proxy.ts
@@ -1,14 +1,14 @@
 import { assertParameter } from "./utilities";
 
-const originalObjects: Record<any, any> = setmetatable({}, { __mode: 'k' });
-const patchedMethods: Record<any, any> = setmetatable({}, { __mode: 'k' });
+const originalObjects = setmetatable(new LuaMap<AnyNotNil, LuaMap>(), { __mode: 'k' });
+const patchedMethods = setmetatable(new LuaMap<(...args: any[]) => any, (...args: any[]) => any>, { __mode: 'k' });
 
 function proxyMethod(methodName: string, method: any) {
-    if (patchedMethods[method]) return patchedMethods[method];
+    if (patchedMethods.has(method)) return patchedMethods.get(method);
 
     const patched = (key: any, ...args: any[]) => {
-        const originalObject = originalObjects[key];
-        if (originalObject === undefined || originalObject[methodName] !== method)
+        const originalObject = originalObjects.get(key);
+        if (originalObject === undefined || originalObject.get(methodName) !== method)
             error('bad object method call: invalid self parameter', 2);
 
         const result = pcall(method, originalObject, ...args);
@@ -18,14 +18,14 @@ function proxyMethod(methodName: string, method: any) {
         error(result[1], 3);
     };
 
-    patchedMethods[method] = patched;
+    patchedMethods.set(method, patched);
     return patched;
 }
 
 const proxyMetatable: LuaMetatable<any> = {
     __index: function (key) {
-        const originalObject = originalObjects[this];
-        const value = originalObject[key];
+        const originalObject = originalObjects.get(this);
+        const value = originalObject?.get(key);
 
         if (type(value) !== 'function') return undefined;
         if (type(key) === 'string' && key[0] === '_') return undefined;
@@ -34,7 +34,7 @@ const proxyMetatable: LuaMetatable<any> = {
     },
 
     __tostring: function () {
-        const originalObject = originalObjects[this];
+        const originalObject = originalObjects.get(this);
         return `userdata${string.sub(tostring(originalObject), 6, -1)}`;
     },
 
@@ -48,10 +48,10 @@ export function proxy<T>(object: T): T {
     assertParameter(object, 'table', 1, 'proxy');
 
     const proxyObject: any = setmetatable({}, proxyMetatable);
-    originalObjects[proxyObject] = object;
+    originalObjects.set(proxyObject, object as any);
     return proxyObject as T;
 }
 
 export function unproxy<T>(object: T): T {
-    return originalObjects[object] ?? object;
+    return originalObjects.get(object as any) ?? object as any;
 }
